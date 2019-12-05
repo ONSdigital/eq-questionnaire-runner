6876076397124e237c44bb8edc1b7701871e6195
Ian Wootten <hi@niftydigits.com>
2019-12-05 15:42:15 +0000

    Remove used of custom cached property

--- app/forms/date_form.py
+++ app/forms/date_form.py
@@ -2,6 +2,7 @@ import logging
 
 from enum import Enum
 
+from werkzeug.utils import cached_property
 from wtforms import Form, StringField, FormField
 
 logger = logging.getLogger(__name__)
@@ -35,27 +36,8 @@ class DateField(FormField):
         super().process(formdata, data)
 
 
-class CachedProperty:
-    """ A property that is only computed once per instance and then replaces
-        itself with an ordinary attribute. Deleting the attribute resets the
-        property.
-
-        Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
-        """
-
-    def __init__(self, func):
-        self.__doc__ = getattr(func, '__doc__')
-        self.func = func
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        value = obj.__dict__[self.func.__name__] = self.func(obj)
-        return value
-
-
 class DateForm(Form):
-    @CachedProperty
+    @cached_property
     def data(self):
         data = super().data
         try:
