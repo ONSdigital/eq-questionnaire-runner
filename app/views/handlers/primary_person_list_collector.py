35cd7f9a290b8b525c8c2a535d10c580adf92924
James Gardiner <james@jgardiner.co.uk>
2019-12-13 11:43:44 +0000

    Move call to update

--- app/views/handlers/primary_person_list_collector.py
+++ app/views/handlers/primary_person_list_collector.py
@@ -37,11 +37,15 @@ class PrimaryPersonListCollector(Question):
             self._primary_person_id = self.questionnaire_store_updater.add_primary_person(
                 list_name
             )
-
-        else:
-            self.questionnaire_store_updater.remove_primary_person(list_name)
-            super().handle_post()
-
-        if self.questionnaire_store_updater.is_dirty:
             self.evaluate_and_update_section_status_on_list_change(list_name)
             self.questionnaire_store_updater.save()
+        else:
+            self.questionnaire_store_updater.remove_primary_person(list_name)
+
+            # This method could determine the current section's status incorrectly, as
+            # the call to update the answer store takes place in
+            # `super().handle_post()`. The section status will eventually get
+            # determined correctly when the parent class' `update_section_status`
+            # method is called.
+            self.evaluate_and_update_section_status_on_list_change(list_name)
+            super().handle_post()
