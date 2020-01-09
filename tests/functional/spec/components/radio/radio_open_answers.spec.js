9562c3b48719ca96241bce2082761873a50839b3
petechd <piotr.chudziak@ons.gov.uk>
2020-01-09 08:53:53 +0000

    Reformat functional test

--- tests/functional/spec/components/radio/radio_open_answers.spec.js
+++ tests/functional/spec/components/radio/radio_open_answers.spec.js
@@ -1,30 +1,28 @@
-describe('Component: Radio', function() {
-  describe('Given I start a Radio survey with a write-in option', function() {
-    const RadioOpenTruePage = require('../../../generated_pages/radio_open/radio-open-true.page.js');
-    const RadioOpenFalsePage = require('../../../generated_pages/radio_open/radio-open-false.page.js');
-    const RadioOpenNonePage = require('../../../generated_pages/radio_open/radio-open-none.page.js');
+const RadioOpenTruePage = require('../../../generated_pages/radio_open/radio-open-true.page.js');
+const RadioOpenFalsePage = require('../../../generated_pages/radio_open/radio-open-false.page.js');
+const RadioOpenNonePage = require('../../../generated_pages/radio_open/radio-open-none.page.js');
 
-    beforeEach(function() {
-      browser.openQuestionnaire('test_radio_open.json');
-      $(RadioOpenTruePage.coffee()).click();
-    });
+describe('Given I start a Radio survey with a write-in option', function() {
+  beforeEach(function() {
+    browser.openQuestionnaire('test_radio_open.json');
+    $(RadioOpenTruePage.coffee()).click();
+   });
 
-    it('When I view a write-in radio and the open option is set to true, detail answer label should be displayed', function() {
-      expect($(RadioOpenTruePage.otherDetail()).isDisplayed()).to.be.true;
-    });
+  it('When I view a write-in radio and the open option is set to true, detail answer label should be displayed', function() {
+    expect($(RadioOpenTruePage.otherDetail()).isDisplayed()).to.be.true;
+  });
 
-    it('When I view a write-in radio and the open option is set to false, detail answer label should not be displayed', function() {
-      $(RadioOpenTruePage.submit()).click();
-      $(RadioOpenFalsePage.iceCream()).click();
-      expect($(RadioOpenFalsePage.otherDetail()).isDisplayed()).to.be.false;
-    });
+  it('When I view a write-in radio and the open option is set to false, detail answer label should not be displayed', function() {
+    $(RadioOpenTruePage.submit()).click();
+    $(RadioOpenFalsePage.iceCream()).click();
+    expect($(RadioOpenFalsePage.otherDetail()).isDisplayed()).to.be.false;
+  });
 
-    it('When I view a write-in radio and the open option is not set, detail answer label should not be displayed', function() {
-      $(RadioOpenFalsePage.submit()).click();
-      $(RadioOpenFalsePage.iceCream()).click();
-      $(RadioOpenFalsePage.submit()).click();
-      $(RadioOpenNonePage.blue()).click();
-      expect($(RadioOpenNonePage.otherDetail()).isDisplayed()).to.be.false;
-    });
+  it('When I view a write-in radio and the open option is not set, detail answer label should not be displayed', function() {
+    $(RadioOpenFalsePage.submit()).click();
+    $(RadioOpenFalsePage.iceCream()).click();
+    $(RadioOpenFalsePage.submit()).click();
+    $(RadioOpenNonePage.blue()).click();
+    expect($(RadioOpenNonePage.otherDetail()).isDisplayed()).to.be.false;
   });
 });
