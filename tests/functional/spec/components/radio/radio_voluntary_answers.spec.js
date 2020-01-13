3488551e8509298eb8a868f2373abe46cce47a95
petechd <piotr.chudziak@ons.gov.uk>
2020-01-13 08:50:58 +0000

    Fix indentation and syntax

--- tests/functional/spec/components/radio/radio_voluntary_answers.spec.js
+++ tests/functional/spec/components/radio/radio_voluntary_answers.spec.js
@@ -8,32 +8,32 @@ describe('Component: Radio', function() {
     });
 
     it('When I select a voluntary radio option, Then the clear button should be displayed', function() {
-       $(RadioVoluntaryTruePage.coffee()).click();
-       expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.be.true;
+      $(RadioVoluntaryTruePage.coffee()).click();
+      expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.equal(true);
      });
 
-     it('When I select a voluntary radio option and click the clear button, Then the radio option should not be selected and the clear button should not be displayed', function() {
-       $(RadioVoluntaryTruePage.coffee()).click();
-       $(RadioVoluntaryTruePage.clearSelectionButton()).click();
-       expect($(RadioVoluntaryTruePage.coffee()).isSelected()).to.be.false;
-       expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.be.false;
+    it('When I select a voluntary radio option and click the clear button, Then the radio option should not be selected and the clear button should not be displayed', function() {
+      $(RadioVoluntaryTruePage.coffee()).click();
+      $(RadioVoluntaryTruePage.clearSelectionButton()).click();
+      expect($(RadioVoluntaryTruePage.coffee()).isSelected()).to.equal(false);
+      expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.equal(false);
      });
 
-     it('When I clear a previously saved voluntary radio option and submit, Then when returning to the page the radio option is no longer selected', function() {
-       $(RadioVoluntaryTruePage.coffee()).click();
-       $(RadioVoluntaryTruePage.submit()).click();
-       $(RadioVoluntaryTruePage.previous()).click();
-       $(RadioVoluntaryTruePage.clearSelectionButton()).click();
-       $(RadioVoluntaryTruePage.submit()).click();
-       $(RadioVoluntaryTruePage.previous()).click();
-       expect($(RadioVoluntaryTruePage.coffee()).isSelected()).to.be.false;
-       expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.be.false;
+    it('When I clear a previously saved voluntary radio option and submit, Then when returning to the page the radio option is no longer selected', function() {
+      $(RadioVoluntaryTruePage.coffee()).click();
+      $(RadioVoluntaryTruePage.submit()).click();
+      $(RadioVoluntaryTruePage.previous()).click();
+      $(RadioVoluntaryTruePage.clearSelectionButton()).click();
+      $(RadioVoluntaryTruePage.submit()).click();
+      $(RadioVoluntaryTruePage.previous()).click();
+      expect($(RadioVoluntaryTruePage.coffee()).isSelected()).to.equal(false);
+      expect($(RadioVoluntaryTruePage.clearSelectionButton()).isDisplayed()).to.equal(false);
      });
 
-     it('When I select a non-voluntary radio option, Then the clear button should not be displayed on the page', function() {
-       $(RadioVoluntaryTruePage.submit()).click();
-       $(RadioVoluntaryFalsePage.iceCream()).click();
-       expect($(RadioVoluntaryFalsePage.clearSelectionButton()).isDisplayed()).to.be.false;
-     });
+    it('When I select a non-voluntary radio option, Then the clear button should not be displayed on the page', function() {
+      $(RadioVoluntaryTruePage.submit()).click();
+      $(RadioVoluntaryFalsePage.iceCream()).click();
+      expect($(RadioVoluntaryFalsePage.clearSelectionButton()).isDisplayed()).to.equal(false);
+    });
   });
 });
