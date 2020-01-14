e4b8b7834ab3e04849cb7c73a2a7011e62475e43
petechd <piotr.chudziak@ons.gov.uk>
2020-01-14 11:17:45 +0000

    Fix functional test

--- tests/functional/spec/components/radio/radio_visible_answers.spec.js
+++ tests/functional/spec/components/radio/radio_visible_answers.spec.js
@@ -4,7 +4,7 @@ const RadioVisibleNonePage = require('../../../generated_pages/radio_detail_answ
 
 describe('Given I start a Radio survey with a write-in option', function() {
   beforeEach(function() {
-    browser.visibleQuestionnaire('test_radio_detail_answer_visible.json');
+    browser.openQuestionnaire('test_radio_detail_answer_visible.json');
   });
 
   it('When I view a write-in radio and the visible option is set to true, Then the detail answer label should be displayed', function() {
