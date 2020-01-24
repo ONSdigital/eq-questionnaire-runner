661a28b894bd182d270483d4abba4474490ee504
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-01-24 10:45:05 +0000

    Revert validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=add_list_section_summary_type
+branch=v3
 docker pull onsdigital/eq-schema-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-schema-validator:$branch"
