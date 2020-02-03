2e057d8b29c4ccfc3f26d47f979af869486b45d0
Ian Wootten <hi@niftydigits.com>
2020-02-03 13:05:49 +0000

    Quote full string

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$SECRETS_S3_BUCKET" ]; then
     echo "Load Secrets from S3 Bucket [$SECRETS_S3_BUCKET]"
-    aws s3 sync s3://"$SECRETS_S3_BUCKET"/ /secrets
+    aws s3 sync "s3://$SECRETS_S3_BUCKET/" /secrets
 fi
 
 GUNICORN_OPTIONS = "-w $GUNICORN_WORKERS --keep-alive $GUNICORN_KEEP_ALIVE --worker-class gevent -b 0.0.0.0:5000 application:application"
