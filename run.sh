499f08e8237ab5a17c5542d4cbc6bd26b518b164
Ian Wootten <hi@niftydigits.com>
2020-02-03 11:35:46 +0000

    Rename docker script

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 if [ -n "$SECRETS_S3_BUCKET" ]; then
     echo "Load Secrets from S3 Bucket [$SECRETS_S3_BUCKET]"
-    aws s3 sync s3://$SECRETS_S3_BUCKET/ /secrets
+    aws s3 sync s3://"$SECRETS_S3_BUCKET"/ /secrets
 fi
 
 GUNICORN_OPTIONS = "-w $GUNICORN_WORKERS --keep-alive $GUNICORN_KEEP_ALIVE --worker-class gevent -b 0.0.0.0:5000 application:application"
 
 if [ "$EQ_NEW_RELIC_ENABLED" == "True" ]; then
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn $GUNICORN_OPTIONS
+    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn "$GUNICORN_OPTIONS"
 else
-    gunicorn $GUNICORN_OPTIONS
+    gunicorn "$GUNICORN_OPTIONS"
 fi
