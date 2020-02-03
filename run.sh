f0e7010a06dfc3c9ffe300df5b5966349cfc196b
Ian Wootten <hi@niftydigits.com>
2020-02-03 15:44:34 +0000

    Correct doublequoting

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ if [ -n "$SECRETS_S3_BUCKET" ]; then
     aws s3 sync "s3://$SECRETS_S3_BUCKET/" /secrets
 fi
 
-GUNICORN_OPTIONS = "-w $GUNICORN_WORKERS --keep-alive $GUNICORN_KEEP_ALIVE --worker-class gevent -b 0.0.0.0:5000 application:application"
+GUNICORN_OPTIONS="-w $GUNICORN_WORKERS --keep-alive $GUNICORN_KEEP_ALIVE --worker-class gevent -b 0.0.0.0:5000 application:application"
 
 if [ "$EQ_NEW_RELIC_ENABLED" == "True" ]; then
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn "$GUNICORN_OPTIONS"
+    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn $GUNICORN_OPTIONS
 else
-    gunicorn "$GUNICORN_OPTIONS"
+    gunicorn $GUNICORN_OPTIONS
 fi
