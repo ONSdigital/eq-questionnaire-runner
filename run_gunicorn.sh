aee74cde4111db7132887c92d0ce813767e1d0a4
Ian Wootten <hi@niftydigits.com>
2020-02-04 14:28:03 +0000

    Use single equals for string comparison

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -5,7 +5,7 @@ if [ -n "$SECRETS_S3_BUCKET" ]; then
     aws s3 sync "s3://$SECRETS_S3_BUCKET/" /secrets
 fi
 
-if [ "$EQ_NEW_RELIC_ENABLED" == "True" ]; then
+if [ "$EQ_NEW_RELIC_ENABLED" = "True" ]; then
     NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn application:application
 else
     gunicorn application:application
