ce42c0760f821fbe07287fcc533f079e7fbdb128
Ian Wootten <hi@niftydigits.com>
2020-02-05 13:05:31 +0000

    Correct branch name

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=add-lookup-to-text-field
+branch=add-lookup-to-textfield
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
