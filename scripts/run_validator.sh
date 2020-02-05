851ec2cdd52c9aa2649a1a16a1f38aac8a1192e2
Ian Wootten <hi@niftydigits.com>
2020-02-05 11:13:55 +0000

    Use correct validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=latest
+branch=add-lookup-to-text-field
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
