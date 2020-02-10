96632345a4c180645d444f283d3c5515e0d03878
Ian Wootten <hi@niftydigits.com>
2020-02-10 15:15:29 +0000

    Given, when, then

--- tests/functional/spec/typeahead.js
+++ tests/functional/spec/typeahead.js
@@ -1,7 +1,7 @@
 const TypeaheadPage = require('../generated_pages/textfield_typeahead/country-block.page.js');
 
 describe('Typeahead', function() {
-  it('Given text entered into a textfield typeahead, it will show suggestions', function() {
+  it('Given I open a typeahead, when I have entered text, then it will show suggestions', function() {
     browser.openQuestionnaire('test_textfield_typeahead.json');
     $(TypeaheadPage.country()).setValue("Uni");
     $("#country-answer-listbox li").waitForDisplayed();
