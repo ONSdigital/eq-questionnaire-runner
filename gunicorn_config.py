f83690826a0ec7dbf58f867e8732bab95af7c6b4
pricem14pc <mark.price@ons.gov.uk>
2020-02-12 15:03:01 +0000

    Updating gunicorn_config property from keep_alive to keepalive

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,6 +4,6 @@ import os
 
 workers = os.getenv("GUNICORN_WORKERS")
 worker_class = "gevent"
-keep_alive = os.getenv("GUNICORN_KEEP_ALIVE")
+keepalive = os.getenv("GUNICORN_KEEP_ALIVE")
 bind = "0.0.0.0:5000"
 gunicorn.SERVER_SOFTWARE = "None"
