45c53f0b36d0345ea1b37151e930dfafe052c6af
Ian Wootten <hi@niftydigits.com>
2020-02-12 10:33:08 +0000

    Be consistent with naming

--- tests/functional/spec/typeahead.js
+++ tests/functional/spec/typeahead.js
@@ -1,9 +1,9 @@
-const TypeaheadPage = require('../generated_pages/textfield_typeahead/country-block.page.js');
+const SuggestionsPage = require('../generated_pages/textfield_suggestions/country-block.page.js');
 
-describe('Typeahead', function() {
+describe('Suggestions', function() {
   it('Given text entered into a textfield typeahead, it will show suggestions', function() {
-    browser.openQuestionnaire('test_textfield_typeahead.json');
-    $(TypeaheadPage.country()).setValue("Uni");
+    browser.openQuestionnaire('test_textfield_lookup.json');
+    $(SuggestionsPage.country()).setValue("Uni");
     $("#country-answer-listbox li").waitForDisplayed();
     expect($$('.js-typeahead-listbox li').length).to.not.equal(0);
   });
