79ba6f89b6f439de6632c8efbfbbd112c64ddc85
Ian Wootten <hi@niftydigits.com>
2020-02-12 10:49:29 +0000

    Remove doublequotes

--- tests/functional/spec/textfield_suggestions.js
+++ tests/functional/spec/textfield_suggestions.js
@@ -3,8 +3,8 @@ const SuggestionsPage = require('../generated_pages/textfield_suggestions/countr
 describe('Suggestions', function() {
   it('Given I open a textfield with a suggestions url, when I have entered text, then it will show suggestions', function() {
     browser.openQuestionnaire('test_textfield_suggestions.json');
-    $(SuggestionsPage.country()).setValue("Uni");
-    $("#country-answer-listbox li").waitForDisplayed();
+    $(SuggestionsPage.country()).setValue('Uni');
+    $('#country-answer-listbox li').waitForDisplayed();
     expect($$('.js-typeahead-listbox li').length).to.not.equal(0);
   });
 });
