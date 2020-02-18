1f47e4162673b33373f4d576bf35db7524696828
petechd <piotr.chudziak@ons.gov.uk>
2020-02-18 11:27:02 +0000

    Revert validator branch change

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=age_confirmation_playback
+branch=latest
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
