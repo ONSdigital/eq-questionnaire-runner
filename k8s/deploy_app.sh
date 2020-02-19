75fbb557c9a699b278e7cee2a79d2193f2c06e87
Andrew Maddaford <ajmaddaford@gmail.com>
2020-02-19 11:16:55 +0000

    Add newrelic to k8s deployment (#56)

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -10,16 +10,19 @@ helm tiller run \
     helm upgrade --install \
     survey-runner \
     k8s/helm \
-    --set submissionBucket=${SUBMISSION_BUCKET_NAME} \
-    --set googleTagManagerId=${GOOGLE_TAG_MANAGER_ID} \
-    --set googleTagManagerAuth=${GOOGLE_TAG_MANAGER_AUTH} \
-    --set googleTagManagerPreview=${GOOGLE_TAG_MANAGER_PREVIEW} \
-    --set image.repository=${DOCKER_REGISTRY}/eq-questionnaire-runner \
-    --set image.tag=${IMAGE_TAG} \
-    --set cookieSettingsUrl=${COOKIE_SETTINGS_URL} \
-    --set resources.requests.cpu=${REQUESTED_CPU_PER_POD} \
-    --set rollingUpdate.maxUnavailable=${ROLLING_UPDATE_MAX_UNAVAILABLE} \
-    --set rollingUpdate.maxSurge=${ROLLING_UPDATE_MAX_SURGE} \
-    --set autoscaler.minReplicas=${MIN_REPLICAS} \
-    --set autoscaler.maxReplicas=${MAX_REPLICAS} \
-    --set autoscaler.targetCPUUtilizationPercentage=${TARGET_CPU_UTILIZATION_PERCENTAGE}
+    --set-string submissionBucket="${SUBMISSION_BUCKET_NAME}" \
+    --set-string googleTagManagerId="${GOOGLE_TAG_MANAGER_ID}" \
+    --set-string googleTagManagerAuth="${GOOGLE_TAG_MANAGER_AUTH}" \
+    --set-string googleTagManagerPreview="${GOOGLE_TAG_MANAGER_PREVIEW}" \
+    --set-string image.repository="${DOCKER_REGISTRY}/eq-questionnaire-runner" \
+    --set-string image.tag="${IMAGE_TAG}" \
+    --set-string cookieSettingsUrl="${COOKIE_SETTINGS_URL}" \
+    --set-string resources.requests.cpu="${REQUESTED_CPU_PER_POD}" \
+    --set-string rollingUpdate.maxUnavailable="${ROLLING_UPDATE_MAX_UNAVAILABLE}" \
+    --set-string rollingUpdate.maxSurge="${ROLLING_UPDATE_MAX_SURGE}" \
+    --set-string autoscaler.minReplicas="${MIN_REPLICAS}" \
+    --set-string autoscaler.maxReplicas="${MAX_REPLICAS}" \
+    --set-string autoscaler.targetCPUUtilizationPercentage="${TARGET_CPU_UTILIZATION_PERCENTAGE}" \
+    --set-string newRelic.enabled="${EQ_NEW_RELIC_ENABLED}" \
+    --set-string newRelic.licenseKey="${NEW_RELIC_LICENSE_KEY}" \
+    --set-string newRelic.appName="${NEW_RELIC_APP_NAME}"
