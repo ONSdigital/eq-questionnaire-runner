7efe4846e9a217b0706b165ba4888702f6af9d5c
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-02-21 11:13:57 +0000

    Validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=latest
+branch=add-field-length-validation
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
