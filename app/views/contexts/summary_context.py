456053a14066148bb1c18be6b9ed5240e0aa824c
James Gardiner <james@jgardiner.co.uk>
2020-02-21 16:30:07 +0000

    Start adding custom summary context

--- app/views/contexts/summary_context.py
+++ app/views/contexts/summary_context.py
@@ -1,5 +1,9 @@
+from flask import url_for
+
 from app.questionnaire.location import Location
+from app.questionnaire.questionnaire_schema import QuestionnaireSchema
 from app.views.contexts.context import Context
+from app.views.contexts.list_collector_context import ListCollectorContext
 from app.views.contexts.summary.group import Group
 
 
@@ -67,8 +71,14 @@ class SummaryContext(Context):
         return title
 
     def section_summary(self, current_location):
-        block = self._schema.get_block(current_location.block_id)
+        section = self._schema.get_section(current_location.block_id)
+        section_summary = self._schema.get_section_summary(section.section_id)
+        if section_summary:
+            return self.custom_section_summary(
+                current_location, section_summary, section
+            )
 
+        block = self._schema.get_block(current_location.block_id)
         return {
             "summary": {
                 "groups": self.build_groups_for_location(current_location),
@@ -78,3 +88,74 @@ class SummaryContext(Context):
                 "title": self.get_title_for_location(current_location),
             }
         }
+
+    def custom_section_summary(self, current_location, section_summary, section):
+        block = self._schema.get_block(current_location.block_id)
+
+        summaries = [
+            self.get_list_summary(summary, current_location, section)
+            for summary in section_summary
+            if summary["type"] == "List"
+        ]
+
+        return {"summary": {"title": block["title"], "list_summaries": summaries}}
+
+    def get_list_summary(self, summary, current_location, section):
+        visible_list_collector_blocks = self._schema.get_visible_list_blocks_for_section(
+            section
+        )
+        routing_path = self._router.routing_path(
+            section["id"], current_location.list_item_id
+        )
+        list_summaries = []
+
+        for list_collector_block in visible_list_collector_blocks:
+            add_link = url_for(
+                "questionnaire.block",
+                list_name=list_collector_block["for_list"],
+                block_id=list_collector_block["add_block"]["id"],
+                return_to=current_location.block_id,
+            )
+
+            if list_collector_block["id"] not in routing_path:
+                driving_question_block = QuestionnaireSchema.get_driving_question_for_list(
+                    section, list_collector_block["for_list"]
+                )
+
+                if driving_question_block:
+                    add_link = url_for(
+                        "questionnaire.block", block_id=driving_question_block["id"]
+                    )
+
+            rendered_summary = self._placeholder_renderer.render(
+                list_collector_block["summary"], current_location.list_item_id
+            )
+
+            list_context = ListCollectorContext(
+                self._language,
+                self._schema,
+                self._answer_store,
+                self._list_store,
+                self._progress_store,
+                self._metadata,
+            )
+
+            list_items = list_context.build_list_items_summary_context(
+                list_collector_block, current_location.block_id
+            )
+
+            list_summary = {
+                "title": rendered_summary["title"],
+                "add_link": add_link,
+                "add_link_text": rendered_summary["add_link_text"],
+                "empty_list_text": rendered_summary["empty_list_text"],
+                "list_name": list_collector_block["for_list"],
+            }
+
+            if list_items:
+                list_summary["list"] = {"list_items": list_items, "editable": True}
+
+            list_summaries.append(list_summary)
+
+        return list_summaries
+
