3fe76b57716345a7af4750e4c6372df276b70510
James Gardiner <james@jgardiner.co.uk>
2020-02-26 09:50:58 +0000

    Update test

--- tests/app/views/contexts/test_summary_context.py
+++ tests/app/views/contexts/test_summary_context.py
@@ -455,6 +455,7 @@ def test_context_for_driving_question_summary_empty_list():
                     "add_link": "/questionnaire/anyone-usually-live-at/",
                     "add_link_text": "Add someone to this household",
                     "empty_list_text": "There are no householders",
+                    "list": {"editable": True, "list_items": []},
                     "title": "Household members",
                     "list_name": "people",
                 }
