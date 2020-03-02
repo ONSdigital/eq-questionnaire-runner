f0942b6bc1dfbedf8e43d4e02e6e6cee1ed9df91
James Gardiner <james@jgardiner.co.uk>
2020-03-02 12:33:21 +0000

    Point at validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-branch=latest
+branch=custom-section-summaries
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
