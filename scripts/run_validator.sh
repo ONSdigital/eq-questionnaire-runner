179d2cbdb46de2aceb6c7b5b8c018200b9059055
petechd <piotr.chudziak@ons.gov.uk>
2020-03-09 08:38:07 +0000

    Add validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=latest
+branch=new-transforms-definitions
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
