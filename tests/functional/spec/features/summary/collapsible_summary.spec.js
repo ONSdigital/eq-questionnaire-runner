ba09d877954b219de1640c199c82953e50aeba14
Andrew Maddaford <ajmaddaford@gmail.com>
2020-03-10 15:32:46 +0000

    Removed un-used page from functional test

--- tests/functional/spec/features/summary/collapsible_summary.spec.js
+++ tests/functional/spec/features/summary/collapsible_summary.spec.js
@@ -1,6 +1,5 @@
 const InsuranceAddressPage = require('../../../generated_pages/section_summary/insurance-address.page.js');
 const InsuranceTypePage = require('../../../generated_pages/section_summary/insurance-type.page.js');
-const AddressDurationPage = require('../../../generated_pages/section_summary/address-duration.page.js');
 const PropertyDetailsSummaryPage = require('../../../generated_pages/section_summary/property-details-summary.page.js');
 const HouseType = require('../../../generated_pages/section_summary/house-type.page.js');
 const HouseholdDetailsSummaryPage = require('../../../generated_pages/section_summary/household-details-summary.page.js');
