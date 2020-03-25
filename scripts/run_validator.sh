32c49e589264b69e65de546eda1537948ad9aca0
James Gardiner <james@jgardiner.co.uk>
2020-03-25 15:37:13 +0000

    branch -> tag

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-branch=custom-section-summaries
-docker pull onsdigital/eq-questionnaire-validator:$branch
-docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
+tag=latest
+docker pull onsdigital/eq-questionnaire-validator:$tag
+docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
