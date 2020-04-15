59919c3af4a8d738e832c1e467f94776b90908d0
Piotr Chudziak <piotr.chudziak@ons.gov.uk>
2020-04-15 07:15:28 +0100

    Add changes

---
+++ scripts/schemas_check.py
@@ -0,0 +1,29 @@
+import requests
+import logging
+import re
+
+from requests.exceptions import RequestException
+
+logger = logging.getLogger(__name__)
+
+try:
+    response = requests.get(
+        "https://api.github.com/repos/ONSdigital/eq-questionnaire-schemas/releases"
+    )
+    if response.status_code == 200:
+        latest_tag = response.json()[0]["tag_name"]
+        f = open("Makefile", "r")
+        file = f.readlines()
+        for i, line in enumerate(file):
+            if "onsdigital/eq-questionnaire-schemas" in line:
+                ref = re.compile(r"v\d.\d.\d")
+                mo = ref.search(file[i])
+                version = mo.group()
+                break
+        if latest_tag != version:
+            logger.error(
+                f"eq-questionnaire-schemas is out of date (current version: {latest_tag}), update makefile"
+            )
+
+except RequestException:
+    logger.error("Can't check eq-questionnaire-schemas release version")
