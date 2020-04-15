8ea5896c60c0a9f7774824534e777695bca17215
Piotr Chudziak <piotr.chudziak@ons.gov.uk>
2020-04-15 11:06:58 +0100

    Add review fixes and demo check

--- scripts/schemas_check.py
+++ scripts/schemas_check.py
@@ -1,10 +1,20 @@
 import logging
-import re
 import requests
 
 from requests.exceptions import RequestException
 
 logger = logging.getLogger(__name__)
+
+
+def get_schema_version():
+    file = open("Makefile", "r")
+    contents = file.readlines()
+    for line in contents:
+        if "onsdigital/eq-questionnaire-schemas" not in line:
+            continue
+        version = line.split(' ')[-1].strip()
+        return version
+
 
 try:
     response = requests.get(
@@ -12,16 +22,7 @@ try:
     )
     if response.status_code == 200:
         latest_tag = response.json()[0]["tag_name"]
-        f = open("Makefile", "r")
-        file = f.readlines()
-        for i, line in enumerate(file):
-            if "onsdigital/eq-questionnaire-schemas" in line:
-                ref = re.compile(r"v\d.\d.\d")
-                mo = ref.search(file[i])
-                version = mo.group()
-                break
-        if latest_tag != version:
+        if latest_tag != get_schema_version():
             logger.error("eq-questionnaire-schemas is out of date, update makefile")
-
 except RequestException:
     logger.error("Can't check eq-questionnaire-schemas release version")
