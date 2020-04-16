3165955f65632c8736f735057306aa02bdfa5595
James Gardiner <james@jgardiner.co.uk>
2020-04-16 16:20:05 +0100

    Check for list items and primary person

--- app/views/handlers/question.py
+++ app/views/handlers/question.py
@@ -32,13 +32,21 @@ class Question(BlockHandler):
         )
 
     def _get_list_add_question_url(self, params):
-        list_name = params["list_name"]
         block_id = params["block_id"]
+        list_name = params["list_name"]
+        list_items = self._questionnaire_store.list_store[list_name].items
         section_id = self._schema.get_section_id_for_block_id(block_id)
 
-        return Location(
-            section_id=section_id, block_id=block_id, list_name=list_name
-        ).url(previous=self.current_location.block_id)
+        # deals with scenarios where a list is populated by only a
+        # primary person - so still need to go to the list add question
+        if (
+            len(list_items) == 1
+            and list_items[0]
+            == self._questionnaire_store.list_store[list_name].primary_person
+        ) or not list_items:
+            return Location(
+                section_id=section_id, block_id=block_id, list_name=list_name
+            ).url(previous=self.current_location.block_id)
 
     def _get_answer_action(self):
         answers = self.rendered_block["question"]["answers"]
