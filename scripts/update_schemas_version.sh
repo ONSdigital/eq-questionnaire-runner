2e5aa1aa10db33204ceebd23aec337536b554d19
petechd <piotr.chudziak@ons.gov.uk>
2020-04-21 12:00:17 +0100

    Move comment

--- scripts/update_schemas_version.sh
+++ scripts/update_schemas_version.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 
 set -e
 
+# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
