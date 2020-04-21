411ed7f2acb23e176f1bfa60d559f7b422a2dd6f
petechd <piotr.chudziak@ons.gov.uk>
2020-04-21 11:20:36 +0100

    Add a shebang line

--- scripts/update_schemas_version.sh
+++ scripts/update_schemas_version.sh
@@ -1,4 +1,4 @@
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+#!/bin/bash
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
