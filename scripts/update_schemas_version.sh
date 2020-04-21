ea81ab03401f243c9f310e2032043e7e58db9a65
petechd <piotr.chudziak@ons.gov.uk>
2020-04-21 11:16:17 +0100

    Test changes

---
+++ scripts/update_schemas_version.sh
@@ -0,0 +1,8 @@
+# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+get_latest_release "onsdigital/eq-questionnaire-schemas" > .schemas-version
