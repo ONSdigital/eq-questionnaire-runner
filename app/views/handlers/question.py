edd487f2dfc3048a8a3ea52a59073f748e3ece60
James Gardiner <james@jgardiner.co.uk>
2020-04-23 12:25:41 +0100

    Add checkbox list collector driving questions (#104)
    * Add checkbox list collector driving questions

--- app/views/handlers/question.py
+++ app/views/handlers/question.py
@@ -32,16 +32,22 @@ class Question(BlockHandler):
         )
 
     def _get_list_add_question_url(self, params):
+        block_id = params["block_id"]
         list_name = params["list_name"]
-        is_list_empty = not self._questionnaire_store.list_store[list_name].items
+        list_items = self._questionnaire_store.list_store[list_name].items
+        section_id = self._schema.get_section_id_for_block_id(block_id)
 
-        if is_list_empty:
-            block_id = params["block_id"]
-            section_id = self._schema.get_section_id_for_block_id(block_id)
-
+        if self._is_list_just_primary(list_items, list_name) or not list_items:
             return Location(
                 section_id=section_id, block_id=block_id, list_name=list_name
             ).url(previous=self.current_location.block_id)
+
+    def _is_list_just_primary(self, list_items, list_name):
+        return (
+            len(list_items) == 1
+            and list_items[0]
+            == self._questionnaire_store.list_store[list_name].primary_person
+        )
 
     def _get_answer_action(self):
         answers = self.rendered_block["question"]["answers"]
