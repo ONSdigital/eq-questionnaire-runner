550877e9755793b4c16b900a137a27e151980243
Mebin Abraham <mebin95+work@gmail.com>
2020-05-14 17:00:16 +0100

    Point to validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=latest
+tag=conditional-summaries-section
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
