3cfdd9940d40bf0e234a52563b4c46d2700914fd
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-05-28 08:52:00 +0100

    Use consistent spelling for serialize/deserialize (#128)

--- tests/app/data_model/test_progress_store.py
+++ tests/app/data_model/test_progress_store.py
@@ -28,9 +28,9 @@ def test_serialisation():
         list_item_id="abc123",
     )
 
-    serialised = store.serialise()
+    serialized = store.serialize()
 
-    assert serialised == [
+    assert serialized == [
         Progress.from_dict(
             {
                 "section_id": "s1",
@@ -99,7 +99,7 @@ def test_clear():
 
     store.clear()
 
-    assert store.serialise() == []
+    assert store.serialize() == []
     assert store.is_dirty
 
 
