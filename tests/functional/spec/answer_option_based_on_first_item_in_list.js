efb61f4feb01e65eb1c48aa78c59931a6642ac7c
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-05-29 10:23:10 +0100

    Refactor test

--- tests/functional/spec/answer_option_based_on_first_item_in_list.js
+++ tests/functional/spec/answer_option_based_on_first_item_in_list.js
@@ -1,19 +1,19 @@
-const ListCollectorPage = require("../generated_pages/answer_based_on_first_item_in_list/list-collector.page.js");
-const ListCollectorAddPage = require("../generated_pages/answer_based_on_first_item_in_list/list-collector-add.page.js");
-const ListStatusInterstitial = require("../generated_pages/answer_based_on_first_item_in_list/list-status.page.js");
-const FavouriteDrinkQuestion = require("../generated_pages/answer_based_on_first_item_in_list/favourite-drink.page.js");
-const ListStatusQuestion = require("../generated_pages/answer_based_on_first_item_in_list/list-status-2.page.js");
-const SummaryPage = require("../generated_pages/answer_based_on_first_item_in_list/personal-details-section-summary.page.js");
+const ListCollectorPage = require("../generated_pages/placeholder_based_on_first_item_in_list/list-collector.page.js");
+const ListCollectorAddPage = require("../generated_pages/placeholder_based_on_first_item_in_list/list-collector-add.page.js");
+const ListStatusInterstitial = require("../generated_pages/placeholder_based_on_first_item_in_list/list-status.page.js");
+const FavouriteDrinkQuestion = require("../generated_pages/placeholder_based_on_first_item_in_list/favourite-drink.page.js");
+const ListStatusQuestion = require("../generated_pages/placeholder_based_on_first_item_in_list/list-status-2.page.js");
+const SummaryPage = require("../generated_pages/placeholder_based_on_first_item_in_list/personal-details-section-summary.page.js");
 
 const HubPage = require("../base_pages/hub.page.js");
 
-describe('Placeholder Based on First Item in List"', function() {
-  beforeEach(function() {
-    browser.openQuestionnaire("test_placeholder_based_on_first_item_in_list.json");
-  });
+describe("Component: Definition", function() {
+  describe("Load the Survey", function() {
+    beforeEach(function() {
+      browser.openQuestionnaire("test_placeholder_based_on_first_item_in_list.json");
+    });
 
-  describe("Given I am the first person in the list", function() {
-    it("When I get to the question page, Then I should see the default answer option", function() {
+    it('Given I am the first person in the list, When I get to the question page, Then I should see the default answer option', function() {
       // Given
       $(HubPage.submit()).click();
       $(ListCollectorPage.yes()).click();
@@ -23,9 +23,9 @@ describe('Placeholder Based on First Item in List"', function() {
       $(ListCollectorAddPage.submit()).click();
       $(ListCollectorPage.no()).click();
       $(ListCollectorPage.submit()).click();
+      $(HubPage.submit()).click();
 
       // When
-      $(HubPage.submit()).click();
       $(ListStatusInterstitial.submit()).click();
       $(FavouriteDrinkQuestion.answer()).setValue("Orange Juice");
       $(FavouriteDrinkQuestion.submit()).click();
@@ -33,10 +33,8 @@ describe('Placeholder Based on First Item in List"', function() {
       // Then
       expect($(ListStatusQuestion.listStatus2TeaLabel()).getText()).to.contain("Tea");
     });
-  });
 
-  describe("Given I am not the first person in the list", function() {
-    it("When I get to the question page, Then I should see the correct answer option", function() {
+    it('Given I am not the first person in the list, When I get to the question page, Then I should see the correct answer option', function() {
       // Given
       $(HubPage.submit()).click();
       $(ListCollectorPage.yes()).click();
@@ -51,9 +49,9 @@ describe('Placeholder Based on First Item in List"', function() {
       $(ListCollectorAddPage.submit()).click();
       $(ListCollectorPage.no()).click();
       $(ListCollectorPage.submit()).click();
+      $(HubPage.submit()).click();
 
       // When
-      $(HubPage.submit()).click();
       $(ListStatusInterstitial.submit()).click();
       $(FavouriteDrinkQuestion.answer()).setValue("Orange Juice");
       $(FavouriteDrinkQuestion.submit()).click();
