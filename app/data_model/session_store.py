f8ed82e540692d7ca8d75207a49b4f3b33d4d8a8
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-06-01 11:29:07 +0100

    Use Redis to store eQ Session data and standardise data storage classes (#125)
    Co-authored-by: James Gardiner <james@jgardiner.co.uk>
    
    - Introduces a new abstract base class to standardise our Storage backend classes.
    - Cleaned up some duplication in our Storage classes.
    - Use Redis to store eq-sessions instead of Datastore.
    - Use consistent naming for the expires at attribute.
    - Removed the used_at key from JTI as this is not needed. We simply try to insert the key, and if it already exists, then we know it has been used recently. This also means that we now do not store a value for the JTI claim in Redis, hence we save some memory.
    - In Redis, the key, and the value both contained the key field, therefore for EQSession, the eq_session_id has been dropped from the value. The key field is added back when fetching the value.

--- app/data_model/session_store.py
+++ app/data_model/session_store.py
@@ -39,7 +39,7 @@ class SessionStore:
         if self._eq_session and self._eq_session.expires_at:
             self._eq_session.expires_at = expires_at
 
-    def create(self, eq_session_id, user_id, session_data, expires_at=None):
+    def create(self, eq_session_id, user_id, session_data, expires_at):
         """
         Create a new eq_session and associate it with the user_id specified
         """
@@ -47,7 +47,9 @@ class SessionStore:
         self.user_id = user_id
         self.session_data = session_data
         self._eq_session = EQSession(
-            self.eq_session_id, self.user_id, expires_at=expires_at
+            eq_session_id=self.eq_session_id,
+            user_id=self.user_id,
+            expires_at=expires_at,
         )
 
         return self
@@ -61,7 +63,7 @@ class SessionStore:
                 self.user_id, self.user_ik, self.pepper
             ).encrypt_data(vars(self.session_data))
 
-            current_app.eq["storage"].put(self._eq_session)
+            current_app.eq["ephemeral_storage"].put(self._eq_session, overwrite=True)
 
         return self
 
@@ -70,7 +72,7 @@ class SessionStore:
         deletes user session from database
         """
         if self._eq_session:
-            current_app.eq["storage"].delete(self._eq_session)
+            current_app.eq["ephemeral_storage"].delete(self._eq_session)
 
             self._eq_session = None
             self.eq_session_id = None
@@ -82,7 +84,7 @@ class SessionStore:
             "finding eq_session_id in database", eq_session_id=self.eq_session_id
         )
 
-        self._eq_session = current_app.eq["storage"].get_by_key(
+        self._eq_session = current_app.eq["ephemeral_storage"].get(
             EQSession, self.eq_session_id
         )
 
--- tests/app/storage/test_redis.py
+++ tests/app/storage/test_redis.py
@@ -1,41 +1,161 @@
-from datetime import datetime, timedelta
+import json
 import uuid
+from datetime import datetime, timedelta
+from unittest import mock
 
 import fakeredis
+from dateutil.tz import tzutc
 
+from app.data_model.app_models import UsedJtiClaim, EQSession
 from app.storage.errors import ItemAlreadyExistsError
-from app.storage.redis import RedisStorage
-from app.data_model.app_models import UsedJtiClaim
+from app.storage.redis import Redis
+from app.storage.storage import StorageModel
 from tests.app.app_context_test_case import AppContextTestCase
 
+EXPIRES_AT = datetime.now(tz=tzutc()).replace(microsecond=0) + timedelta(minutes=1)
 
-class TestDatastore(AppContextTestCase):
+
+class TestRedis(AppContextTestCase):
     def setUp(self):
         super().setUp()
 
         self.mock_client = fakeredis.FakeStrictRedis()
 
-        self.redis = RedisStorage(self.mock_client)
+        self.redis = Redis(self.mock_client)
 
-    def test_put_jti(self):
+    def test_put_jti_stores_empty_value(self):
         used_at = datetime.now()
-        expires = used_at + timedelta(seconds=60)
+        expires_at = used_at + timedelta(seconds=60)
 
-        jti = UsedJtiClaim(str(uuid.uuid4()), used_at, expires)
+        jti = UsedJtiClaim(str(uuid.uuid4()), expires_at)
 
-        self.redis.put_jti(jti)
+        self.redis.put(jti, overwrite=False)
 
-        set_data = self.mock_client.get(jti.jti_claim)
+        stored_data = self.mock_client.get(jti.jti_claim)
 
-        self.assertEqual(int(jti.used_at.timestamp()), int(set_data))
+        self.assertEqual(b"", stored_data)
 
     def test_duplicate_put_jti_fails(self):
         used_at = datetime.now()
-        expires = used_at + timedelta(seconds=60)
+        expires_at = used_at + timedelta(seconds=60)
 
-        jti = UsedJtiClaim(str(uuid.uuid4()), used_at, expires)
+        jti = UsedJtiClaim(str(uuid.uuid4()), expires_at)
 
-        self.redis.put_jti(jti)
+        self.redis.put(jti, overwrite=False)
 
         with self.assertRaises(ItemAlreadyExistsError):
-            self.redis.put_jti(jti)
+            self.redis.put(jti, overwrite=False)
+
+    def test_put_session(self):
+        # given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+        stored_data = self.redis.get(EQSession, eq_session.eq_session_id)
+        self.assertIsNone(stored_data)
+
+        # when
+        self.redis.put(eq_session)
+
+        # Then
+        stored_data = self.redis.get(EQSession, eq_session.eq_session_id)
+        self.assertIsNotNone(stored_data)
+
+    def test_get_session(self):
+        # Given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+        stored_data = self.redis.get(EQSession, eq_session.eq_session_id)
+        self.assertIsNone(stored_data)
+        self.redis.put(eq_session)
+
+        # When
+        stored_data = self.redis.get(EQSession, eq_session.eq_session_id)
+
+        # Then
+        for k, v in eq_session.__dict__.items():
+            parsed_value = getattr(stored_data, k)
+            if isinstance(v, datetime):
+                self.assertGreaterEqual(v, parsed_value)
+            else:
+                self.assertEqual(v, parsed_value)
+
+    def test_delete_session(self):
+        # Given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+        self.redis.put(eq_session)
+        session = self.redis.get(EQSession, "sessionid")
+        self.assertEqual(session.eq_session_id, eq_session.eq_session_id)
+
+        # When
+        self.redis.delete(eq_session)
+
+        # Then
+        self.assertIsNone(self.redis.get(EQSession, "sessionid"))
+
+    def test_redis_does_not_store_key_field_in_value(self):
+        # Given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+        stored_data = self.redis.get(EQSession, eq_session.eq_session_id)
+        self.assertIsNone(stored_data)
+        self.redis.put(eq_session)
+
+        # When
+        stored_data = self.mock_client.get(eq_session.eq_session_id)
+
+        storage_model = StorageModel(model_type=EQSession)
+
+        assert storage_model.key_field not in json.loads(stored_data)
+
+    def test_get_redis_expiry_when_expiry_set(self):
+        # Given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+        # When
+        self.redis.put(eq_session)
+
+        # Then
+        expires_in = self.mock_client.ttl(eq_session.eq_session_id)
+        assert expires_in > 0
+
+    def test_get_redis_expiry_when_expiry_not_set(self):
+        # Given
+        eq_session = EQSession(
+            eq_session_id="sessionid",
+            user_id="someuser",
+            session_data="somedata",
+            expires_at=EXPIRES_AT,
+        )
+
+        # When
+        with mock.patch(
+            "app.storage.storage.StorageModel.expiry_field",
+            new_callable=mock.PropertyMock,
+        ) as mock_expiry_field:
+            mock_expiry_field.return_value = None
+            self.redis.put(eq_session)
+
+        # Then
+        expires_in = self.mock_client.ttl(eq_session.eq_session_id)
+        assert expires_in == -1
