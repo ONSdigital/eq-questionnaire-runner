ebf066be25d45a816b128f4fdd824adc4a8bce8b
Mebin Abraham <mebin95+work@gmail.com>
2020-06-02 16:40:21 +0100

    Fix lint

--- tests/functional/spec/timeout.spec.js
+++ tests/functional/spec/timeout.spec.js
@@ -5,7 +5,7 @@ describe("Timeout", () => {
     browser.openQuestionnaire("test_timeout.json");
   });
 
-  it("Given I am completing an electronic questionnaire, when I have been inactive for the timeout period and attempt to submit data, then I will be redirected to a page confirming my session has timed out", function() {
+  it("Given I am completing an electronic questionnaire, when I have been inactive for the timeout period and attempt to submit data, then I will be redirected to a page confirming my session has timed out", () => {
     browser.pause(6000);
     $(TimeoutBlockPage.submit()).click();
 
