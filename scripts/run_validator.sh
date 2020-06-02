f7870e1d722a760e7c000694c0b9729f9b7c3b62
Andrew Maddaford <ajmaddaford@gmail.com>
2020-06-02 16:41:15 +0100

    Revert validator tag change

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=add-question-warning
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
