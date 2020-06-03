85d239c893f050ca953511398905d673516166b0
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-06-03 08:13:35 +0100

    Revert validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=update-list-item-selector
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
