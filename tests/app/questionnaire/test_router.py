aff74b89a9f269595cd78f8d0b8484fc9d25d3fc
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-06-30 13:57:32 +0100

    Fix broken routing tests

--- tests/app/questionnaire/test_router.py
+++ tests/app/questionnaire/test_router.py
@@ -483,7 +483,7 @@ class TestRouter(AppContextTestCase):  # pylint: disable=too-many-public-methods
 
         expected_path = [
             RoutingPath(
-                ["mandatory-checkbox", "non-mandatory-checkbox", "summary"],
+                ["mandatory-checkbox", "non-mandatory-checkbox", "single-checkbox", "summary"],
                 section_id="default-section",
             )
         ]
