bc4f04d4207fcda538aba60c100a82c5e9562629
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2020-07-03 14:30:01 +0100

    fix numbering on data-qa attributes

--- tests/functional/spec/question_definitions.spec.js
+++ tests/functional/spec/question_definitions.spec.js
@@ -7,31 +7,50 @@ describe("Component: Definition", () => {
     });
 
     it('When I click the title link, then the description and "Hide this" button should be visible', () => {
-      expect($(DefinitionPage.definitionContent(2)).isDisplayed()).to.be.false;
-      expect($(DefinitionPage.definitionButton(2)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionContent(1)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionButton(1)).isDisplayed()).to.be.false;
 
       // When
-      $(DefinitionPage.definitionTitle(2)).click();
+      $(DefinitionPage.definitionTitle(1)).click();
 
       // Then
-      $(DefinitionPage.definitionContent(2)).waitForDisplayed({ timeout: 300 });
-      $(DefinitionPage.definitionButton(2)).waitForDisplayed({ timeout: 300 });
+      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300 });
+      $(DefinitionPage.definitionButton(1)).waitForDisplayed({ timeout: 300 });
     });
 
     it('When I click the title link twice, then the description and "Hide this" button should not be visible', () => {
-      expect($(DefinitionPage.definitionContent(2)).isDisplayed()).to.be.false;
-      expect($(DefinitionPage.definitionButton(2)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionContent(1)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionButton(1)).isDisplayed()).to.be.false;
 
       // When
-      $(DefinitionPage.definitionTitle(2)).click();
-      $(DefinitionPage.definitionTitle(2)).click();
+      $(DefinitionPage.definitionTitle(1)).click();
+      $(DefinitionPage.definitionTitle(1)).click();
 
       // Then
-      $(DefinitionPage.definitionContent(2)).waitForDisplayed({ timeout: 300, reverse: true });
-      $(DefinitionPage.definitionButton(2)).waitForDisplayed({ timeout: 300, reverse: true });
+      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300, reverse: true });
+      $(DefinitionPage.definitionButton(1)).waitForDisplayed({ timeout: 300, reverse: true });
     });
 
     it('When I click the title link then click "Hide this" button, then the description and button should not be visible', () => {
+      expect($(DefinitionPage.definitionContent(1)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionButton(1)).isDisplayed()).to.be.false;
+
+      // When
+      $(DefinitionPage.definitionTitle(1)).click();
+
+      // Then
+      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300 });
+      $(DefinitionPage.definitionButton(1)).waitForDisplayed({ timeout: 300 });
+
+      // When
+      $(DefinitionPage.definitionButton(1)).click();
+
+      // Then
+      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300, reverse: true });
+      $(DefinitionPage.definitionButton(1)).waitForDisplayed({ timeout: 300, reverse: true });
+    });
+
+    it('When I click the second definition\'s title link then the description and "Hide this" button for the second definition should be visible', () => {
       expect($(DefinitionPage.definitionContent(2)).isDisplayed()).to.be.false;
       expect($(DefinitionPage.definitionButton(2)).isDisplayed()).to.be.false;
 
@@ -41,25 +60,6 @@ describe("Component: Definition", () => {
       // Then
       $(DefinitionPage.definitionContent(2)).waitForDisplayed({ timeout: 300 });
       $(DefinitionPage.definitionButton(2)).waitForDisplayed({ timeout: 300 });
-
-      // When
-      $(DefinitionPage.definitionButton(2)).click();
-
-      // Then
-      $(DefinitionPage.definitionContent(2)).waitForDisplayed({ timeout: 300, reverse: true });
-      $(DefinitionPage.definitionButton(2)).waitForDisplayed({ timeout: 300, reverse: true });
-    });
-
-    it('When I click the second definition\'s title link then the description and "Hide this" button for the second definition should be visible', () => {
-      expect($(DefinitionPage.definitionContent(3)).isDisplayed()).to.be.false;
-      expect($(DefinitionPage.definitionButton(3)).isDisplayed()).to.be.false;
-
-      // When
-      $(DefinitionPage.definitionTitle(3)).click();
-
-      // Then
-      $(DefinitionPage.definitionContent(3)).waitForDisplayed({ timeout: 300 });
-      $(DefinitionPage.definitionButton(3)).waitForDisplayed({ timeout: 300 });
     });
   });
 });
