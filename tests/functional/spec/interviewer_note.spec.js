fbde88d0ed83a8f55b8f447ae3fd62512520cfbe
petechd <piotr.chudziak@ons.gov.uk>
2020-07-13 15:17:22 +0100

    Add review changes

--- tests/functional/spec/interviewer_note.spec.js
+++ tests/functional/spec/interviewer_note.spec.js
@@ -1,27 +1,27 @@
-import InitialInterstitialPage from "../generated_pages/interviewer_note/initial-interstitial-block.page.js";
-import FavouriteTeamPage from "../generated_pages/interviewer_note/favourite-team-block.page.js";
 import ConfirmPage from "../generated_pages/interviewer_note/confirm-block.page.js";
+import FavouriteTeamPage from "../generated_pages/interviewer_note/favourite-team-block.page.js";
 import FinalInterstitialPage from "../generated_pages/interviewer_note/final-interstitial-block.page.js";
+import InitialInterstitialPage from "../generated_pages/interviewer_note/initial-interstitial-block.page.js";
 
 describe("Interviewer Note", () => {
-  beforeEach(() => {
+  before(() => {
     browser.openQuestionnaire("test_interviewer_note.json");
   });
 
-  it("Given I start a survey, When I view interstitial page and the interviewer_note is set to true then I should be able to see interviewer note", () => {
+  it("When I view interstitial page and the interviewer_note is set to true then I should be able to see interviewer note", () => {
     expect($(InitialInterstitialPage.questionText()).getText()).to.contain("Interviewer Note");
   });
-  it("Given I start a survey, When I view question page and the interviewer_note is set to true then I should be able to see interviewer note", () => {
+  it("When I view question page and the interviewer_note is set to true then I should be able to see interviewer note", () => {
     $(InitialInterstitialPage.submit()).click();
     expect($(FavouriteTeamPage.questionText()).getText()).to.contain("Interviewer Note");
   });
-  it("Given I start a survey, When I view question page and the interviewer_note is set to false then I should not be able to see interviewer note", () => {
+  it("When I view question page and the interviewer_note is set to false then I should not be able to see interviewer note", () => {
     $(InitialInterstitialPage.submit()).click();
     $(FavouriteTeamPage.favouriteTeam()).setValue("TNS");
     $(FavouriteTeamPage.submit()).click();
     expect($(ConfirmPage.questionText()).getText()).to.not.contain("Interviewer Note");
   });
-  it("Given I start a survey, When I view interstitial page and the interviewer_note is not set then I should not be able to see interviewer note", () => {
+  it("When I view interstitial page and the interviewer_note is not set then I should not be able to see interviewer note", () => {
     $(InitialInterstitialPage.submit()).click();
     $(FavouriteTeamPage.favouriteTeam()).setValue("TNS");
     $(FavouriteTeamPage.submit()).click();
