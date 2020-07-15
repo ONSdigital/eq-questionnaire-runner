9e69e8d9aa94e7e65afe919870bec868166f9268
James Gardiner <james@jgardiner.co.uk>
2020-07-15 10:25:17 +0100

    Revert validator branch to master

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=individual-response
+tag=master
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
