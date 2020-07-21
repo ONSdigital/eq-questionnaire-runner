8976b1160a09ccec9da2c3196e620c9cd2aabb5c
Ian Wootten <hi@niftydigits.com>
2020-07-21 10:20:29 +0100

    Update validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=make-description-list
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
