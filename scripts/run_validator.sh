24d56678c412cbddc0bd822e9b91798347e1005b
Ian Wootten <hi@niftydigits.com>
2020-08-04 17:18:10 +0100

    Update valdiator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=latest
+tag=spike-using-listcollector-actions
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
