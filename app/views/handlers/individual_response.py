d4edaa491bb4887fa12981696f8f7342338e06af
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-08-26 15:34:59 +0100

    PR comment

--- app/views/handlers/individual_response.py
+++ app/views/handlers/individual_response.py
@@ -143,7 +143,7 @@ class IndividualResponseHandler:
                 "questionnaire.block",
                 list_name=self._list_name,
                 list_item_id=self._list_item_id,
-                block_id="remove-person",
+                block_id=self._request_args.get("journey"),
             )
 
         elif self._list_item_id:
