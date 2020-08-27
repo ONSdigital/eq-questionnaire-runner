9340169e30aaa7b2c685701e84164d585e96f552
Mebin Abraham <mebin95+work@gmail.com>
2020-08-27 16:53:33 +0100

    Point to address validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=latest
+tag=add-support-for-address
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
