63ed79aba97bbcb8a37d0bdf0f749774aac1c58d
James Gardiner <james@jgardiner.co.uk>
2020-09-09 14:55:38 +0100

    Revert validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=block-and-non-block-page-titles
+tag=master
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
