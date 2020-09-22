ad7987a1cc76ffe860615b0fc271307e57e89d2b
LJBabbage <lewis.babbage@ons.gov.uk>
2020-09-22 10:56:01 +0100

    Revert validator

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=add-repeating-page-title
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
