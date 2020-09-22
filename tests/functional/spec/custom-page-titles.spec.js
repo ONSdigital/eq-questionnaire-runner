d04d01f603217ff58da58b220689bb37a371a052
LJBabbage <lewis.babbage@ons.gov.uk>
2020-09-22 11:13:31 +0100

    Add section repeating page title (#290)
    * Add section repeating page title

--- tests/functional/spec/custom-page-titles.spec.js
+++ tests/functional/spec/custom-page-titles.spec.js
@@ -1,16 +1,17 @@
 import HubPage from "../base_pages/hub.page.js";
+import DateOfBirthPage from "../generated_pages/custom_page_titles/date-of-birth.page.js";
 import IndividualInterstitialPage from "../generated_pages/custom_page_titles/individual-interstitial.page.js";
 import ListCollectorAddPage from "../generated_pages/custom_page_titles/list-collector-add.page.js";
 import ListCollectorEditPage from "../generated_pages/custom_page_titles/list-collector-edit.page.js";
 import ListCollectorPage from "../generated_pages/custom_page_titles/list-collector.page.js";
+import ProxyPage from "../generated_pages/custom_page_titles/proxy.page.js";
 import RelationshipsPage from "../generated_pages/custom_page_titles/relationships.page.js";
-import SectionSummaryPage from "../generated_pages/custom_page_titles/section-summary.page.js";
 
 describe("Feature: Custom Page Titles", () => {
   const schema = "test_custom_page_titles.json";
 
   describe("Given I am completing the test_custom_page_titles survey,", () => {
-    beforeEach("load the survey", () => {
+    before("load the survey", () => {
       browser.openQuestionnaire(schema);
     });
 
@@ -20,8 +21,7 @@ describe("Feature: Custom Page Titles", () => {
       expect(expectedPageTitle).to.equal("Custom page title - Census 2021");
     });
 
-    it("When I navigate to the add person pages, Then I should see the custom page titles", () => {
-      $(HubPage.submit()).click();
+    it("When I navigate to the add person page, Then I should see the custom page title", () => {
       $(ListCollectorPage.yes()).click();
       $(ListCollectorPage.submit()).click();
       let expectedPageTitle = browser.getTitle();
@@ -37,14 +37,6 @@ describe("Feature: Custom Page Titles", () => {
     });
 
     it("When I navigate to relationship collector pages, Then I should see the custom page titles", () => {
-      $(HubPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
-      $(ListCollectorAddPage.firstName()).setValue("Marcus");
-      $(ListCollectorAddPage.lastName()).setValue("Twin");
-      $(ListCollectorAddPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
       $(ListCollectorAddPage.firstName()).setValue("Samuel");
       $(ListCollectorAddPage.lastName()).setValue("Clemens");
       $(ListCollectorAddPage.submit()).click();
@@ -75,57 +67,26 @@ describe("Feature: Custom Page Titles", () => {
       expect(expectedPageTitle).to.equal("Custom section summary page title - Census 2021");
     });
 
-    it("When I navigate to individual section pages, Then I should see the custom page titles", () => {
-      $(HubPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
-      $(ListCollectorAddPage.firstName()).setValue("Marcus");
-      $(ListCollectorAddPage.lastName()).setValue("Twin");
-      $(ListCollectorAddPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
-      $(ListCollectorAddPage.firstName()).setValue("Samuel");
-      $(ListCollectorAddPage.lastName()).setValue("Clemens");
-      $(ListCollectorAddPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
-      $(ListCollectorAddPage.firstName()).setValue("Olivia");
-      $(ListCollectorAddPage.lastName()).setValue("Clemens");
-      $(ListCollectorAddPage.submit()).click();
-      $(ListCollectorPage.no()).click();
-      $(ListCollectorPage.submit()).click();
-      $(RelationshipsPage.husbandOrWife()).click();
-      $(RelationshipsPage.submit()).click();
-      $(RelationshipsPage.sonOrDaughter()).click();
-      $(RelationshipsPage.submit()).click();
-      $(RelationshipsPage.submit()).click();
-      $(RelationshipsPage.sonOrDaughter()).click();
-      $(RelationshipsPage.submit()).click();
-      $(SectionSummaryPage.submit()).click();
-      $(HubPage.submit()).click();
-      let expectedPageTitle = browser.getTitle();
-      expect(expectedPageTitle).to.equal("Person 1 individual interstitial - Census 2021");
-
-      $(IndividualInterstitialPage.submit()).click();
-      expectedPageTitle = browser.getTitle();
-      expect(expectedPageTitle).to.equal("Person 1 proxy question - Census 2021");
-    });
-
     it("When I navigate to list edit and remove pages Then I should see the custom page titles", () => {
-      $(HubPage.submit()).click();
-      $(ListCollectorPage.yes()).click();
-      $(ListCollectorPage.submit()).click();
-      $(ListCollectorAddPage.firstName()).setValue("Marcus");
-      $(ListCollectorAddPage.lastName()).setValue("Twin");
-      $(ListCollectorAddPage.submit()).click();
       $(ListCollectorPage.listEditLink(1)).click();
       let expectedPageTitle = browser.getTitle();
       expect(expectedPageTitle).to.equal("Edit person 1 - Census 2021");
-
       $(ListCollectorEditPage.previous()).click();
       $(ListCollectorPage.listRemoveLink(1)).click();
       expectedPageTitle = browser.getTitle();
       expect(expectedPageTitle).to.equal("Remove person 1 - Census 2021");
+    });
+
+    it("When I navigate to a repeating section which has custom page title, Then all page titles in the section should have the correct prefix", () => {
+      browser.url(HubPage.url());
+      $(HubPage.submit()).click();
+      expect(browser.getTitle()).to.equal("Individual interstitial: Person 1 - Census 2021");
+      $(IndividualInterstitialPage.submit()).click();
+      expect(browser.getTitle()).to.equal("Proxy question: Person 1 - Census 2021");
+      $(ProxyPage.submit()).click();
+      expect(browser.getTitle()).to.equal("What is your date of birth?: Person 1 - Census 2021");
+      $(DateOfBirthPage.submit()).click();
+      expect(browser.getTitle()).to.equal("Summary: Person 1 - Census 2021");
     });
   });
 });
