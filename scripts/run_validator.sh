0643d84de07d9a4ebcc2bf7fb05ed9fbcb20c236
LJBabbage <lewis.babbage@ons.gov.uk>
2020-09-23 08:24:16 +0100

    Update validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=latest
+tag=add-selector-to-placeholder-address-type
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
