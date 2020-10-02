bf9194587e03ed7cdd60fdd1d6330a829aae4358
LJBabbage <lewis.babbage@ons.gov.uk>
2020-10-02 09:05:15 +0100

    Add individualResponseLimit to k8 deploy app

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -24,7 +24,8 @@ helm upgrade --install \
     --set-string autoscaler.targetCPUUtilizationPercentage="${TARGET_CPU_UTILIZATION_PERCENTAGE}" \
     --set-string newRelic.enabled="${EQ_NEW_RELIC_ENABLED}" \
     --set-string newRelic.licenseKey="${NEW_RELIC_LICENSE_KEY}" \
-    --set-string newRelic.appName="${NEW_RELIC_APP_NAME}"
+    --set-string newRelic.appName="${NEW_RELIC_APP_NAME}" \
+    --set-string individualResponseLimit="${EQ_INDIVIDUAL_RESPONSE_LIMIT}"
 
 kubectl rollout restart deployment.v1.apps/runner
 kubectl rollout status deployment.v1.apps/runner
