5b7e5291d0027df1185e9beb7500e1490e4c1d09
James Gardiner <james@jgardiner.co.uk>
2020-10-09 15:27:51 +0100

    Use HTTP_KEEP_ALIVE env var

--- run_app.sh
+++ run_app.sh
@@ -7,9 +7,9 @@ if [ "$WEB_SERVER_TYPE" = "gunicorn-async" ]; then
 elif [ "$WEB_SERVER_TYPE" = "gunicorn-threads" ]; then
     run_command="gunicorn application:application --worker-class gthread"
 elif [ "$WEB_SERVER_TYPE" = "uwsgi" ]; then
-    run_command="uwsgi uwsgi.ini --workers ${WEB_SERVER_WORKERS}"
+    run_command="uwsgi uwsgi.ini --workers ${WEB_SERVER_WORKERS} --http-keepalive ${HTTP_KEEP_ALIVE}"
 elif [ "$WEB_SERVER_TYPE" = "uwsgi-threads" ]; then
-    run_command="uwsgi uwsgi.ini --workers ${WEB_SERVER_WORKERS} --enable-threads --threads ${WEB_SERVER_THREADS}"
+    run_command="uwsgi uwsgi.ini --workers ${WEB_SERVER_WORKERS} --enable-threads --threads ${WEB_SERVER_THREADS} --http-keepalive ${HTTP_KEEP_ALIVE}"
 elif [ "$WEB_SERVER_TYPE" = "uwsgi-async" ]; then
     run_command="uwsgi uwsgi.ini --module patched:application --workers ${WEB_SERVER_WORKERS} --gevent ${WEB_SERVER_UWSGI_ASYNC_CORES}"
 fi
