60460f8834853897ddb9ce620cfd6b9bfa98d982
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2020-11-06 13:59:41 +0000

    rename tests and test file

--- tests/integration/components/test_cookies_banner_content.py
+++ tests/integration/components/test_cookies_banner_content.py
@@ -1,10 +1,10 @@
 from tests.integration.integration_test_case import IntegrationTestCase
 
 
-class TestCookiesBanner(IntegrationTestCase):
+class TestCookiesBannerContent(IntegrationTestCase):
     def setUp(self):
         super().setUp()
         self.launchSurvey("test_dropdown_mandatory")
 
-    def test_cookies_banner_contents(self):
+    def test_cookies_banner_content(self):
         self.assertInBody("Tell us whether you accept cookies")
