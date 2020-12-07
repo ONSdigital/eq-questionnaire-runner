a77e2de0772049c588f5c1582c133fe2ec2bf72c
LJBabbage <lewis.babbage@ons.gov.uk>
2020-12-07 16:08:01 +0000

    Update app/views/handlers/list_remove_question.py
    Co-authored-by: James Gardiner <james@jgardiner.co.uk>

--- app/views/handlers/list_remove_question.py
+++ app/views/handlers/list_remove_question.py
@@ -32,8 +32,7 @@ class ListRemoveQuestion(ListAction):
         return super().handle_post()
 
     def individual_response_enabled(self) -> bool:
-        if self.parent_block["for_list"] == self._schema.get_individual_response_list():
-            return True
+		return self.parent_block["for_list"] == self._schema.get_individual_response_list()
         return False
 
     def get_context(self):
