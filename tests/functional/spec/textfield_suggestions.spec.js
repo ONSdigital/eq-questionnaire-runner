d609d8bbd61db00f6b125b3f3ef23d3e9e05f8df
petechd <piotr.chudziak@ons.gov.uk>
2021-01-07 09:34:56 +0000

    More fixes to description

--- tests/functional/spec/textfield_suggestions.spec.js
+++ tests/functional/spec/textfield_suggestions.spec.js
@@ -11,7 +11,7 @@ describe("Suggestions", () => {
 });
 
 describe("Suggestions", () => {
-  it("Given I open a textfield with a suggestions url, when I have entered text and picked suggestion from a list, then after typing text it will show new suggestions", () => {
+  it("Given I open a textfield with a suggestions url, when I have entered text and picked suggestion from a list, then after typing more text it will show new suggestions", () => {
     browser.openQuestionnaire("test_textfield_suggestions.json");
     $(SuggestionsPage.country()).setValue("United States of America");
     $(SuggestionsPage.submit()).click();
