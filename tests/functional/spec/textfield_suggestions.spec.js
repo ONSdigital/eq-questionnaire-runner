68af9a25fed73a2eb8e3a1dd6c20b5d5ecbdc7e4
petechd <piotr.chudziak@ons.gov.uk>
2021-01-08 10:35:32 +0000

    Fix functional test

--- tests/functional/spec/textfield_suggestions.spec.js
+++ tests/functional/spec/textfield_suggestions.spec.js
@@ -1,5 +1,5 @@
 import SuggestionsPage from "../generated_pages/textfield_suggestions/country-block.page.js";
-import MultipleSuggestionsPage from "../generated_pages/textfield_suggestions/country-block-multiple-selections.page.js";
+import MultipleSuggestionsPage from "../generated_pages/textfield_suggestions/multiple-country-block.page.js";
 
 describe("Suggestions", () => {
   it("Given I open a textfield with a suggestions url, when I have entered text, then it will show suggestions", () => {
@@ -15,16 +15,16 @@ describe("Suggestions", () => {
     browser.openQuestionnaire("test_textfield_suggestions.json");
     $(SuggestionsPage.country()).setValue("United States of America");
     $(SuggestionsPage.submit()).click();
-    $(MultipleSuggestionsPage.countryMultipleSelections()).click();
+    $(MultipleSuggestionsPage.multipleCountry()).click();
     browser.keys("Ita");
-    $("#country-multiple-selections-answer-listbox li").waitForDisplayed();
+    $("#multiple-country-answer-listbox li").waitForDisplayed();
     expect($$(".js-autosuggest-listbox li").length).to.equal(2);
-    $("#country-multiple-selections-answer-listbox__option--0").click();
-    $(MultipleSuggestionsPage.countryMultipleSelections()).click();
+    $("#multiple-country-answer-listbox__option--0").click();
+    $(MultipleSuggestionsPage.multipleCountry()).click();
     // Browser needs to pause before typing starts, otherwise this sequence is executed almost instantly
     browser.pause(500);
-    browser.keys(" United States");
-    $("#country-multiple-selections-answer-listbox li").waitForDisplayed();
+    browser.keys(" United");
+    $("#multiple-country-answer-listbox li").waitForDisplayed();
     expect($$(".js-autosuggest-listbox li").length).to.equal(3);
   });
 });
