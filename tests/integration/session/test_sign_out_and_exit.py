7ddfa71d84000aae7b8067e3f22c9e4eab59c290
Mebin Abraham <mebin95+work@gmail.com>
2021-03-15 13:29:51 +0000

    Add additional tests

--- tests/integration/session/test_sign_out_and_exit.py
+++ tests/integration/session/test_sign_out_and_exit.py
@@ -8,24 +8,33 @@ from tests.integration.integration_test_case import IntegrationTestCase
 SIGN_OUT_URL_PATH = "/sign-out"
 SIGNED_OUT_URL_PATH = "/signed-out"
 ACCOUNT_SERVICE_LOG_OUT_URL = "http://localhost/logout"
+LOG_OUT_URL_PATH = "/logout"
 
 
 class TestSaveAndSignOut(IntegrationTestCase):
+    # Test the behaviour when using a Hub/No Hub
+
     def test_no_session_cookie_redirects_to_census_home_page(self):
-        self.get(SIGN_OUT_URL_PATH, follow_redirects=False)
-        self.assertInRedirect(CENSUS_EN_BASE_URL)
+        for schema in ["test_textfield", "test_hub_and_spoke"]:
+            with self.subTest(schema=schema):
+                self.get(SIGN_OUT_URL_PATH, follow_redirects=False)
+                self.assertInRedirect(CENSUS_EN_BASE_URL)
 
     def test_no_account_service_log_out_url_redirects_to_signed_out_page(self):
-        self.launchSurvey("test_textfield")
-        self.get(SIGN_OUT_URL_PATH)
-        self.assertInUrl(SIGNED_OUT_URL_PATH)
+        for schema in ["test_textfield", "test_hub_and_spoke"]:
+            with self.subTest(schema=schema):
+                self.launchSurvey(schema)
+                self.get(SIGN_OUT_URL_PATH)
+                self.assertInUrl(SIGNED_OUT_URL_PATH)
 
     def test_redirects_to_account_service_log_out_url_when_present(self):
-        self.launchSurvey(
-            "test_textfield", account_service_log_out_url=ACCOUNT_SERVICE_LOG_OUT_URL
-        )
-        self.get(SIGN_OUT_URL_PATH)
-        self.assertInUrl("/logout")
+        for schema in ["test_textfield", "test_hub_and_spoke"]:
+            with self.subTest(schema=schema):
+                self.launchSurvey(
+                    schema, account_service_log_out_url=ACCOUNT_SERVICE_LOG_OUT_URL
+                )
+                self.get(SIGN_OUT_URL_PATH)
+                self.assertInUrl(LOG_OUT_URL_PATH)
 
 
 class TestExitPostSubmissionTestCase(IntegrationTestCase):
@@ -33,6 +42,7 @@ class TestExitPostSubmissionTestCase(IntegrationTestCase):
         self.launchSurvey(schema, **kwargs)
         self.post()
         self.post()
+        self.assertInUrl("/thank-you")
 
 
 class TestExitPostSubmissionWithFinalSummaryDefaultTheme(
@@ -49,7 +59,7 @@ class TestExitPostSubmissionWithFinalSummaryDefaultTheme(
             account_service_log_out_url=ACCOUNT_SERVICE_LOG_OUT_URL,
         )
         self.get(SIGN_OUT_URL_PATH)
-        self.assertInUrl("/logout")
+        self.assertInUrl(LOG_OUT_URL_PATH)
 
 
 class TestExitPostSubmissionWithFinalSummaryCensusTheme(TestExitPostSubmissionTestCase):
@@ -88,7 +98,30 @@ class TestExitPostSubmissionWithFinalSummaryCensusTheme(TestExitPostSubmissionTe
         self.assertInRedirect(CENSUS_NIR_BASE_URL)
 
 
-class TestExitPostSubmissionWitHub(TestExitPostSubmissionTestCase):
+class TestExitPostSubmissionWitHubDefaultTheme(IntegrationTestCase):
+    def _launch_and_submit_questionnaire(self, schema, **kwargs):
+        self.launchSurvey(schema, **kwargs)
+        self.post({"household-relationships-answer": "No"})
+        self.post()
+        self.assertInUrl("/thank-you")
+
+    def test_no_account_service_log_out_url_redirects_to_signed_out_page(self):
+        self._launch_and_submit_questionnaire(
+            schema="test_hub_section_required_and_enabled"
+        )
+        self.get(SIGN_OUT_URL_PATH, follow_redirects=False)
+        self.assertInUrl(SIGN_OUT_URL_PATH)
+
+    def test_redirects_to_account_service_log_out_url_when_present(self):
+        self._launch_and_submit_questionnaire(
+            schema="test_hub_section_required_and_enabled",
+            account_service_log_out_url=ACCOUNT_SERVICE_LOG_OUT_URL,
+        )
+        self.get(SIGN_OUT_URL_PATH)
+        self.assertInUrl(LOG_OUT_URL_PATH)
+
+
+class TestExitPostSubmissionWitHubCensusTheme(TestExitPostSubmissionTestCase):
     def test_no_account_service_log_out_url_redirects_to_census_home_page(self):
         self._launch_and_submit_questionnaire(schema="test_thank_you_census_household")
         self.get(SIGN_OUT_URL_PATH, follow_redirects=False)
