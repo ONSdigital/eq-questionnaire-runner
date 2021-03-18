ed8533398a4182e12a10f7fa598e0ece7f26ab87
James Gardiner <james@jgardiner.co.uk>
2021-03-18 12:38:14 +0000

    Update log message and kwargs

--- app/storage/redis.py
+++ app/storage/redis.py
@@ -6,13 +6,17 @@ from redis.exceptions import ConnectionError as RedisConnectionError
 from structlog import get_logger
 
 from app.storage.errors import ItemAlreadyExistsError
-from .storage import StorageHandler, StorageModel
 
+from .storage import StorageHandler, StorageModel
 
 logger = get_logger()
 
 
 class Redis(StorageHandler):
+    @property
+    def command_failed_log_message(self):
+        return "retrying redis command"
+
     def put(self, model, overwrite=True):
         storage_model = StorageModel(model_type=type(model))
         serialized_item = storage_model.serialize(model)
@@ -36,7 +40,7 @@ class Redis(StorageHandler):
                 name=key_value, value=value, ex=expires_in, nx=not overwrite
             )
         except RedisConnectionError:
-            logger.info("retrying redis set command")
+            logger.info(self.command_failed_log_message, command="set")
             record_created = self.client.set(
                 name=key_value, value=value, ex=expires_in, nx=not overwrite
             )
@@ -49,7 +53,7 @@ class Redis(StorageHandler):
         try:
             item = self.client.get(key_value)
         except RedisConnectionError:
-            logger.info("retrying redis get command")
+            logger.info(self.command_failed_log_message, command="get")
             item = self.client.get(key_value)
 
         if item:
@@ -65,5 +69,5 @@ class Redis(StorageHandler):
         try:
             return self.client.delete(key_value)
         except RedisConnectionError:
-            logger.info("retrying redis delete command")
+            logger.info(self.command_failed_log_message, command="delete")
             return self.client.delete(key_value)
