6d1360342dbeb45d2b3d2458afc41c5f6b55d631
petechd <piotr.chudziak@ons.gov.uk>
2021-03-23 08:35:06 +0000

    Add test and schema

--- tests/integration/questionnaire/test_questionnaire_radio_voluntary.py
+++ tests/integration/questionnaire/test_questionnaire_radio_voluntary.py
@@ -12,3 +12,23 @@ class TestQuestionnaireRadioVoluntary(IntegrationTestCase):
         self.post(action="clear_radios")
         self.post()
         self.assertNotInBody("Coffee")
+
+
+class TestQuestionnaireRepeatingSectionRadioVoluntary(IntegrationTestCase):
+
+    BASE_URL = "/questionnaire/"
+
+    def test_radio_voluntary_on_repeating_section(self):
+        self.launchSurvey("test_repeating_section_with_radio_voluntary")
+        self.post()
+        self.post({"you-live-here": "Yes"})
+        self.post({"first-name": "James", "last-name": "May"})
+        self.post({"anyone-else": "No"})
+        self.post()
+        self.post({"radio-voluntary-true-answer": "Coffee"})
+        self.previous()
+        self.post(action="clear_radios")
+        self.post()
+        self.post({"radio-voluntary-false-answer": "Hamburger"})
+        self.assertInUrl("personal-details-section")
+        self.assertNotInBody("Coffee")
