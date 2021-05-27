b9aa329391329e709e4e29887803a3a5893756fc
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2021-05-27 14:23:49 +0100

    Update scripts/run_validator.sh

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=introduce-questionnaire-flow
+tag=v2.0.0
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
