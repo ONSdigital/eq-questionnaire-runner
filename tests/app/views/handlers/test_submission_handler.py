0bae393f25bd2caa178b8c4a10ce7dd9fbb9eeee
LJBabbage <lewis.babbage@ons.gov.uk>
2021-07-23 11:38:35 +0100

    Add view_submitted_response to submission handler (#628)
    * Add view_submitted_response to submission handler

--- tests/app/views/handlers/test_submission_handler.py
+++ tests/app/views/handlers/test_submission_handler.py
@@ -27,26 +27,74 @@ class TestSubmissionPayload(AppContextTestCase):
             ru_ref="ru_ref",
             case_id="0123456789000000",
         )
-        self.session_store = SessionStore("user_ik", "pepper", "eq_session_id")
         self.expires_at = datetime.now(tzutc()) + timedelta(seconds=5)
-
-    def test_submission_language_code_in_payload(self):
-        session_store = self.session_store.create(
+        self.session_store = SessionStore("user_ik", "pepper", "eq_session_id").create(
             "eq_session_id", "user_id", self.session_data, self.expires_at
         )
-        storage = Mock()
-        storage.get_user_data = Mock(return_value=("{}", 1))
 
+    @patch("app.views.handlers.submission.convert_answers", Mock(return_value={}))
+    def test_submission_language_code_in_payload(self):
         with patch(
             "app.views.handlers.submission.get_session_store",
-            return_value=session_store,
+            return_value=self.session_store,
         ):
+
+            submission_handler = SubmissionHandler(
+                QuestionnaireSchema({}), self.questionnaire_store_mock(), {}
+            )
+            assert submission_handler.get_payload()["submission_language_code"] == "cy"
+
+    @patch(
+        "app.views.handlers.submission.SubmissionHandler.get_payload",
+        Mock(return_value={}),
+    )
+    def test_questionnaire_store_delete_called(self):
+        questionnaire_store = self.questionnaire_store_mock()
+        questionnaire_store.delete = Mock()
+
+        with self.app_request_context():
             with patch(
-                "app.views.handlers.submission.convert_answers", return_value={}
+                "app.views.handlers.submission.get_session_store",
+                return_value=self.session_store,
             ):
+
                 submission_handler = SubmissionHandler(
-                    QuestionnaireSchema({}), QuestionnaireStore(storage), {}
+                    QuestionnaireSchema({}), questionnaire_store, full_routing_path=[]
                 )
-                assert (
-                    submission_handler.get_payload()["submission_language_code"] == "cy"
+                submission_handler.submit_questionnaire()
+
+                assert questionnaire_store.delete.called
+
+    @patch(
+        "app.views.handlers.submission.SubmissionHandler.get_payload",
+        Mock(return_value={}),
+    )
+    def test_view_submitted_response_true_questionnaire_store_delete_not_called(self):
+        questionnaire_store = self.questionnaire_store_mock()
+        questionnaire_store.delete = Mock()
+
+        with self.app_request_context():
+
+            with patch(
+                "app.views.handlers.submission.get_session_store",
+                return_value=self.session_store,
+            ):
+
+                submission_handler = SubmissionHandler(
+                    QuestionnaireSchema(
+                        {"submission": {"view_submitted_response": "True"}}
+                    ),
+                    questionnaire_store,
+                    full_routing_path=[],
                 )
+                submission_handler.submit_questionnaire()
+
+                assert not questionnaire_store.delete.called
+
+    @staticmethod
+    def questionnaire_store_mock():
+        storage = Mock()
+        storage.get_user_data = Mock(return_value=("{}", 1))
+        questionnaire_store = QuestionnaireStore(storage)
+        questionnaire_store.metadata = {"tx_id": "tx_id", "case_id": "case_id"}
+        return questionnaire_store
