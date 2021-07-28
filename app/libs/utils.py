318596d16340c7b7bbad684a934949241d191387
Mebin Abraham <mebin95+work@gmail.com>
2021-07-28 10:53:40 +0100

    Make escape_value method specific to answer values to improve type hints

--- app/libs/utils.py
+++ app/libs/utils.py
@@ -1,10 +1,3 @@
-from typing import Optional, Union
-
-from markupsafe import Markup, escape
-
-from app.data_models.answer import AnswerValueTypes
-
-
 def convert_tx_id(tx_id: str) -> str:
     """
     Converts the guid tx_id to string of 16 characters with a dash between every 4 characters
@@ -12,20 +5,3 @@ def convert_tx_id(tx_id: str) -> str:
     :return: String in the form of xxxx-xxxx-xxxx-xxxx
     """
     return (tx_id[:4] + "-" + tx_id[4:])[:19]
-
-
-def escape_value(
-    value: Optional[AnswerValueTypes],
-) -> Union[None, Markup, AnswerValueTypes]:
-    if isinstance(value, list):
-        return [
-            escape(item) if item and isinstance(item, str) else item for item in value
-        ]
-
-    if isinstance(value, dict):
-        return {
-            key: escape(val) if isinstance(val, str) else val
-            for key, val in value.items()
-        }
-
-    return escape(value) if isinstance(value, str) else value
