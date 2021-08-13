cc73505f37d1588269ce1881f1ecb05c54214281
LJBabbage <lewis.babbage@ons.gov.uk>
2021-08-13 11:03:42 +0100

    Improve test_submitted_response.py and correct errors

--- tests/integration/routes/test_submitted_response.py
+++ tests/integration/routes/test_submitted_response.py
@@ -13,21 +13,27 @@ class TestSubmissionResponse(IntegrationTestCase):
     def test_enabled(self):
         # Given I launch and complete a questionnaire that has view-submitted-response enabled
         self._launch_and_complete_questionnaire()
-        self.post()
-        self.post()
 
-        # When I try to ge the view-submission page
+        # When I try to get the view-response page
         self.get("/submitted/view-response")
 
+        # Then the page is displayed correctly
         self.assertEqualPageTitle("Submitted Response - Test View Submitted Response")
+        self.assertInBody(
+            "Your answers were submitted for <span>Integration Testing</span>"
+        )
+        self.assertInBody("Submitted on:")
+        self.assertInBody("Submission reference:")
+        self.assertInBody("What is your name?")
+        self.assertInBody("John Smith")
 
     def test_not_enabled(self):
-        # Given I launch a questionnaire that does not have view-submitted-response enabled
+        # Given I launch and complete a questionnaire that does not have view-submitted-response enabled
         self.launchSurvey("test_confirmation_email")
-        self.post({"name-answer": "John Smith"})
+        self.post()
         self.post()
 
-        # When I try to ge the view-submission page
+        # When I try to get the view-response page
         self.get("/submitted/view-response")
 
         # Then I get shown a 404 error
@@ -40,7 +46,7 @@ class TestSubmissionResponse(IntegrationTestCase):
         self._launch_and_complete_questionnaire()
         time.sleep(2)
 
-        # When I try to ge the view-submission page
+        # When I try to get the view-response page
         self.get("/submitted/view-response")
 
         # Then I get shown a 404 error
