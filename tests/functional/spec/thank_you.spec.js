68877a6c3939b65f8c7910e478642d7bcb8c20d1
petechd <piotr.chudziak@ons.gov.uk>
2021-08-13 13:38:05 +0100

    Remove test schema and refactor test

--- tests/functional/spec/thank_you.spec.js
+++ tests/functional/spec/thank_you.spec.js
@@ -1,6 +1,6 @@
 import { SubmitPage } from "../base_pages/submit.page.js";
 import HubPage from "../base_pages/hub.page";
-
+import CheckboxPage from "../generated_pages/title/single-title-block.page";
 import ThankYouPage from "../base_pages/thank-you.page";
 
 describe("Thank You Social", () => {
@@ -24,14 +24,15 @@ describe("Thank You Social", () => {
 describe("Thank You Business", () => {
   describe("Given I launch a business theme schema", () => {
     beforeEach(() => {
-      browser.openQuestionnaire("test_theme_business.json");
+      browser.openQuestionnaire("test_title.json");
     });
 
-    it("When I navigate to the thank you page, Then I should see business theme content", () => {
+    it("When I navigate to the thank you page, Then I should see default theme content", () => {
+      $(CheckboxPage.good()).click();
       $(SubmitPage.submit()).click();
       $(HubPage.submit()).click();
       expect(browser.getUrl()).to.contain(ThankYouPage.pageName);
-      expect($(ThankYouPage.title()).getHTML()).to.contain("Thank you for submitting the Test Business Survey");
+      expect($(ThankYouPage.title()).getHTML()).to.contain("Thank you for submitting the Question Title Test");
       expect($(ThankYouPage.guidance()).getHTML()).to.contain("Your answers have been submitted for");
       expect($(ThankYouPage.metadata()).getHTML()).to.contain("Submitted on:");
       expect($(ThankYouPage.metadata()).getHTML()).to.contain("Submission reference:");
