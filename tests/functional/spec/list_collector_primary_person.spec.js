f749485142ceba27704adbea52aeed37323d9c16
petechd <piotr.chudziak@ons.gov.uk>
2021-08-17 08:43:22 +0100

    Refactor tests

--- tests/functional/spec/list_collector_primary_person.spec.js
+++ tests/functional/spec/list_collector_primary_person.spec.js
@@ -95,7 +95,7 @@ describe("Primary Person List Collector Survey", () => {
 
     it("When the user submits, then they are allowed to submit the survey", () => {
       $(SubmitPage.submit()).click();
-      expect($(ThankYouPage.heading()).getText()).to.contain("Thank you for completing the Test PrimaryPersonListCollector");
+      expect(browser.getUrl()).to.contain(ThankYouPage.pageName);
     });
   });
 
