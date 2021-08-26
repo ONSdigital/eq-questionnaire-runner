76f505b98ae35d3baaf9fc0fda71c39423294fa5
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2021-08-26 14:15:26 +0100

    Update scripts/run_validator.sh
    Co-authored-by: petechd <53475968+petechd@users.noreply.github.com>

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=rationalise-submission-property
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
