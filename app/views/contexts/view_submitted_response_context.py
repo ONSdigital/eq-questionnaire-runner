4076080ef8164a9e210184f99dc4a63c74cf2729
LJBabbage <lewis.babbage@ons.gov.uk>
2021-08-26 10:19:59 +0100

    Remove store_metadata var

--- app/views/contexts/view_submitted_response_context.py
+++ app/views/contexts/view_submitted_response_context.py
@@ -18,30 +18,30 @@ def build_view_submitted_response_context(
     survey_type: str,
 ) -> dict[str, Union[str, datetime, dict]]:
 
-    store_metadata = questionnaire_store.metadata
-
     summary_context = SummaryContext(
         language=language,
         schema=schema,
         answer_store=questionnaire_store.answer_store,
         list_store=questionnaire_store.list_store,
         progress_store=questionnaire_store.progress_store,
-        metadata=store_metadata,
+        metadata=questionnaire_store.metadata,
     )
 
     if survey_type == "social":
         submitted_text = lazy_gettext("Answers submitted.")
-    elif trad_as := store_metadata.get("trad_as"):
+    elif trad_as := questionnaire_store.metadata.get("trad_as"):
         submitted_text = lazy_gettext(
             "Answers submitted for <span>{ru_name}</span> ({trad_as})."
-        ).format(ru_name=store_metadata["ru_name"], trad_as=trad_as)
+        ).format(ru_name=questionnaire_store.metadata["ru_name"], trad_as=trad_as)
     else:
         submitted_text = lazy_gettext(
             "Answers submitted for <span>{ru_name}</span>."
-        ).format(ru_name=store_metadata["ru_name"])
+        ).format(ru_name=questionnaire_store.metadata["ru_name"])
 
     metadata = build_submission_metadata_context(
-        survey_type, questionnaire_store.submitted_at, store_metadata["tx_id"]
+        survey_type,
+        questionnaire_store.submitted_at,
+        questionnaire_store.metadata["tx_id"],
     )
     context = {
         "hide_sign_out_button": True,
