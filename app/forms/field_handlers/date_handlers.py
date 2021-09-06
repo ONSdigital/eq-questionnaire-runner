c12c30181d149b5a2abf13a141828beaa548d00f
LJBabbage <lewis.babbage@ons.gov.uk>
2021-09-06 14:04:19 +0100

    Revert extra lines and optional

--- app/forms/field_handlers/date_handlers.py
+++ app/forms/field_handlers/date_handlers.py
@@ -76,6 +76,7 @@ class DateHandler(FieldHandler):
         :return: date value
         """
         value = self.get_schema_value(self.answer_schema[key])
+
         if value == "now":
             value = datetime.now(timezone.utc).strftime("%Y-%m-%d")
 
@@ -110,6 +111,7 @@ class DateHandler(FieldHandler):
         :return: attributes
         """
         date_value = None
+
         if key in self.answer_schema:
             date_value = self.get_referenced_date(key)
 
