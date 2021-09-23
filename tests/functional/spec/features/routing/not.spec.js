f70cc6cb6fc2178456145ecc558d3550325a13f9
Rhys Berrow <rhys.berrow@ons.gov.uk>
2021-09-23 08:21:05 +0100

    Add interstitial page

--- tests/functional/spec/features/routing/not.spec.js
+++ tests/functional/spec/features/routing/not.spec.js
@@ -1,6 +1,6 @@
 import CountryCheckboxPage from "../../../generated_pages/new_routing_not/country-checkbox.page";
-import CountryInterstitialPage from "../../../generated_pages/new_routing_not/country-interstitial-india.page";
-import SubmitPage from "../../../generated_pages/new_routing_not/submit.page";
+import CountryInterstitialPage from "../../../generated_pages/new_routing_not/country-interstitial-not-india.page";
+import IndiaInterstitialPage from "../../../generated_pages/new_routing_not/country-interstitial-india.page";
 
 describe("Feature: Routing - Not Operator", () => {
   describe("Equals", () => {
@@ -9,16 +9,16 @@ describe("Feature: Routing - Not Operator", () => {
         browser.openQuestionnaire("test_new_routing_not.json");
       });
 
-      it("When I do not select India, Then I should be routed to the interstitial page", () => {
+      it("When I do not select India, Then I should be routed to the not India interstitial page", () => {
         $(CountryCheckboxPage.azerbaijan()).click();
         $(CountryCheckboxPage.submit()).click();
         expect(browser.getUrl()).to.contain(CountryInterstitialPage.pageName);
       });
 
-      it("When I do select India, Then I should be routed to the submit page", () => {
+      it("When I select India, Then I should be routed to the India interstitial page", () => {
         $(CountryCheckboxPage.india()).click();
         $(CountryCheckboxPage.submit()).click();
-        expect(browser.getUrl()).to.contain(SubmitPage.pageName);
+        expect(browser.getUrl()).to.contain(IndiaInterstitialPage.pageName);
       });
     });
   });
