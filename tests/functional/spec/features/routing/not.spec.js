877ca2e025e56f6d59cee2e868cf4d611e695036
Rhys Berrow <47635349+berroar@users.noreply.github.com>
2021-09-23 11:47:56 +0100

    Add new routing and or and not operators (#666)

---
+++ tests/functional/spec/features/routing/not.spec.js
@@ -0,0 +1,25 @@
+import CountryCheckboxPage from "../../../generated_pages/new_routing_not/country-checkbox.page";
+import CountryInterstitialPage from "../../../generated_pages/new_routing_not/country-interstitial-not-india.page";
+import IndiaInterstitialPage from "../../../generated_pages/new_routing_not/country-interstitial-india.page";
+
+describe("Feature: Routing - Not Operator", () => {
+  describe("Equals", () => {
+    describe("Given I start the not operator routing survey", () => {
+      beforeEach(() => {
+        browser.openQuestionnaire("test_new_routing_not.json");
+      });
+
+      it("When I do not select India, Then I should be routed to the not India interstitial page", () => {
+        $(CountryCheckboxPage.azerbaijan()).click();
+        $(CountryCheckboxPage.submit()).click();
+        expect(browser.getUrl()).to.contain(CountryInterstitialPage.pageName);
+      });
+
+      it("When I select India, Then I should be routed to the India interstitial page", () => {
+        $(CountryCheckboxPage.india()).click();
+        $(CountryCheckboxPage.submit()).click();
+        expect(browser.getUrl()).to.contain(IndiaInterstitialPage.pageName);
+      });
+    });
+  });
+});
