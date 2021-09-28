b6e00b0cbc45ac5d7689b1557dd2783216ed3ad7
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2021-09-28 12:17:43 +0100

    add variable type to function

--- app/questionnaire/placeholder_transforms.py
+++ app/questionnaire/placeholder_transforms.py
@@ -246,6 +246,6 @@ class PlaceholderTransforms:
         return f'<a href="{href}">{link_text}</a>'
 
     @staticmethod
-    def list_item_count(list_to_count) -> int:
+    def list_item_count(list_to_count: Sequence[str]) -> int:
         if list_to_count is not None:
             return len(list_to_count) if list_to_count else 0
