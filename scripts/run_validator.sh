81dfaf09f260da122b58d616251b1e12643ce89b
James Nunn <jamesnunn123@gmail.com>
2021-10-01 15:53:40 +0100

    fix validator branch name

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=generate_date_range-and-format_date_range
+tag=generate_date_range-and-format_date_range_pair
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
