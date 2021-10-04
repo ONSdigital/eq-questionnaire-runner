05a798d1dadfe8fa0d844cecbdf7acc9183cced1
Mebin Abraham <mebin95+work@gmail.com>
2021-10-04 13:46:23 +0100

    Add guidance to test schema

--- tests/functional/spec/features/routing/answered_unanswered.spec.js
+++ tests/functional/spec/features/routing/answered_unanswered.spec.js
@@ -7,8 +7,8 @@ import QuestionTwoAnswered from "../../../generated_pages/new_routing_answered_u
 import QuestionTwoUnanswered from "../../../generated_pages/new_routing_answered_unanswered/unanswered-question-2.page";
 
 import QuestionThree from "../../../generated_pages/new_routing_answered_unanswered/block-3.page";
-import QuestionThreeAnsweredOrNotOne from "../../../generated_pages/new_routing_answered_unanswered/answered-question-3.page";
-import QuestionThreeUnansweredOrAnswerOne from "../../../generated_pages/new_routing_answered_unanswered/unanswered-or-not-say-question-3.page";
+import QuestionThreeAnsweredOrNotZero from "../../../generated_pages/new_routing_answered_unanswered/answered-question-3.page";
+import QuestionThreeUnansweredOrAnswerZero from "../../../generated_pages/new_routing_answered_unanswered/unanswered-or-zero-question-3.page";
 
 describe("Test routing question answered/unanswered", () => {
   describe("Given I am on the first question", () => {
@@ -16,7 +16,7 @@ describe("Test routing question answered/unanswered", () => {
       browser.openQuestionnaire("test_new_routing_answered_unanswered.json");
     });
 
-    it("When I select any answer and and submit, Then I should see a page saying I have answered the first question", () => {
+    it("When I select any answer and submit, Then I should see a page saying I have answered the first question", () => {
       $(QuestionOne.ham()).click();
       $(QuestionOne.submit()).click();
       expect($(QuestionOneAnswered.heading()).getText()).to.contain("You answered the first question!");
@@ -74,23 +74,23 @@ describe("Test routing question answered/unanswered", () => {
       $(QuestionTwoUnanswered.submit()).click();
     });
 
-    it("When I do not answer the question or answer `1` and submit, Then I should see a page saying I did not answer the question or that I chose `1`", () => {
+    it("When I do not answer the question or answer `0` and submit, Then I should see a page saying I did not answer the question or that I chose `0`", () => {
       $(QuestionThree.submit()).click();
-      expect($(QuestionThreeUnansweredOrAnswerOne.heading()).getText()).to.contain("You did not answer the question or chose 1 slice");
-      expect(browser.getUrl()).to.contain(QuestionThreeUnansweredOrAnswerOne.pageName);
+      expect($(QuestionThreeUnansweredOrAnswerZero.heading()).getText()).to.contain("You did not answer the question or chose 0 slices");
+      expect(browser.getUrl()).to.contain(QuestionThreeUnansweredOrAnswerZero.pageName);
 
-      $(QuestionThreeUnansweredOrAnswerOne.previous()).click();
-      $(QuestionThree.answer3()).setValue("1");
+      $(QuestionThreeUnansweredOrAnswerZero.previous()).click();
+      $(QuestionThree.answer3()).setValue("0");
       $(QuestionThree.submit()).click();
-      expect($(QuestionThreeUnansweredOrAnswerOne.heading()).getText()).to.contain("You did not answer the question or chose 1 slice");
-      expect(browser.getUrl()).to.contain(QuestionThreeUnansweredOrAnswerOne.pageName);
+      expect($(QuestionThreeUnansweredOrAnswerZero.heading()).getText()).to.contain("You did not answer the question or chose 0 slices");
+      expect(browser.getUrl()).to.contain(QuestionThreeUnansweredOrAnswerZero.pageName);
     });
 
-    it("When I enter an answer great than 1 and submit, Then I should see a page saying I chose more than 1", () => {
+    it("When I enter an answer greater than 0 and submit, Then I should see a page saying I chose at least one", () => {
       $(QuestionThree.answer3()).setValue("2");
       $(QuestionThree.submit()).click();
-      expect($(QuestionTwoAnswered.heading()).getText()).to.contain("You chose more than 1 slice");
-      expect(browser.getUrl()).to.contain(QuestionThreeAnsweredOrNotOne.pageName);
+      expect($(QuestionTwoAnswered.heading()).getText()).to.contain("You chose at least 1 slice");
+      expect(browser.getUrl()).to.contain(QuestionThreeAnsweredOrNotZero.pageName);
     });
   });
 });
