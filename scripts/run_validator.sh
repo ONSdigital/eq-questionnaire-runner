2694747c6f2bad2df36740b8430e473f44ee62a0
rmccar <42928680+rmccar@users.noreply.github.com>
2021-10-07 14:11:19 +0100

    Update scripts/run_validator.sh

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=routing-on-checkbox-count
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
