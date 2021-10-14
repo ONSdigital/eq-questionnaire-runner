d6898f36df1a0c33fca47f7097fdb56a0fe8e00f
Mebin Abraham <mebin95+work@gmail.com>
2021-10-14 12:30:49 +0100

    Use pytest in favour of unittest

--- tests/app/views/handlers/test_view_submitted_response_pdf.py
+++ tests/app/views/handlers/test_view_submitted_response_pdf.py
@@ -1,48 +1,35 @@
 from datetime import datetime, timedelta, timezone
 from unittest.mock import Mock
 
-from werkzeug.exceptions import BadRequestKeyError
+import pytest
 
 from app.data_models import QuestionnaireStore
 from app.questionnaire.questionnaire_schema import QuestionnaireSchema
 from app.views.handlers.view_submitted_response import ViewSubmittedResponseExpired
 from app.views.handlers.view_submitted_response_pdf import ViewSubmittedResponsePDF
-from tests.app.app_context_test_case import AppContextTestCase
+from tests.app.views.handlers.conftest import set_storage_data
 
 
-class TestViewSubmittedResponsePDF(AppContextTestCase):
-    def setUp(self):
-        super().setUp()
+def test_pdf_not_downloadable(app, storage, schema, language):
+    submitted_at = datetime.now(timezone.utc) - timedelta(minutes=46)
+    set_storage_data(storage, submitted_at=submitted_at)
 
-        self.schema = QuestionnaireSchema({"post_submission": {"view_response": True}})
-        self.language = "en"
+    questionnaire_store = QuestionnaireStore(storage)
 
-    def set_submitted_at(self, submitted_at):
-        self.storage = Mock()
-        self.storage.get_user_data = Mock(return_value=("{}", 1, submitted_at))
-
-    def test_pdf_not_downloadable(self):
-        submitted_at = datetime.now(timezone.utc) - timedelta(minutes=46)
-        self.set_submitted_at(submitted_at)
-        self.questionnaire_store = QuestionnaireStore(self.storage)
-
-        with self.assertRaises(ViewSubmittedResponseExpired):
-            ViewSubmittedResponsePDF(
-                self.schema,
-                self.questionnaire_store,
-                self.language,
-            )
-
-    def test_filename_schema_name(self):
-        submitted_at = datetime.now(timezone.utc)
-        self.set_submitted_at(submitted_at)
-        self.questionnaire_store = QuestionnaireStore(self.storage)
-
-        self.questionnaire_store.set_metadata(
-            {"schema_name": "test_view_submitted_response"}
-        )
-        pdf = ViewSubmittedResponsePDF(
-            self.schema, self.questionnaire_store, self.language
+    with pytest.raises(ViewSubmittedResponseExpired):
+        ViewSubmittedResponsePDF(
+            schema,
+            questionnaire_store,
+            language,
         )
 
-        self.assertEqual(pdf.filename, "test_view_submitted_response.pdf")
+
+def test_filename_uses_schema_name(app, storage, schema, language):
+    submitted_at = datetime.now(timezone.utc)
+    set_storage_data(storage, submitted_at=submitted_at)
+
+    questionnaire_store = QuestionnaireStore(storage)
+    questionnaire_store.set_metadata({"schema_name": "test_view_submitted_response"})
+    pdf = ViewSubmittedResponsePDF(schema, questionnaire_store, language)
+
+    assert pdf.filename == "test_view_submitted_response.pdf"
