b9777576db27a6cb30d6e63a9d317565002d3389
petechd <piotr.chudziak@ons.gov.uk>
2021-10-19 13:31:42 +0100

    Fix functional test

--- tests/functional/spec/exit.spec.js
+++ tests/functional/spec/exit.spec.js
@@ -12,7 +12,7 @@ describe("Post submission exit", () => {
     $(HubPage.submit()).click();
     browser.deleteAllCookies();
     $(CensusThankYouPage.exit()).click();
-    expect(browser.getUrl()).to.equal("https://surveys.ons.gov.uk/sign-in/");
+    expect(browser.getUrl()).to.equal("http://localhost:5000/signed-out");
   });
 
   it("Given I click the exit button from the thank you page, When I am redirected, Then I should be redirected to the correct log out url", () => {
