5df9523f90dff4ccfa06666f47a1a0f19fdff4dc
LJBabbage <lewis.babbage@ons.gov.uk>
2021-11-22 13:25:39 +0000

    Refactor operations.py into class (#720)
    * Refactor operations.py into class

--- app/questionnaire/routing/operations.py
+++ app/questionnaire/routing/operations.py
@@ -11,82 +11,90 @@ ComparableValue = TypeVar("ComparableValue", str, int, float, Decimal, date)
 NonArrayPrimitiveTypes = Union[str, int, float, Decimal, None]
 
 
-@casefold
-def evaluate_equal(lhs: ValueTypes, rhs: ValueTypes) -> bool:
-    return lhs == rhs
-
-
-@casefold
-def evaluate_not_equal(lhs: ValueTypes, rhs: ValueTypes) -> bool:
-    return lhs != rhs
-
-
-def evaluate_greater_than(lhs: ComparableValue, rhs: ComparableValue) -> bool:
-    return lhs > rhs
-
-
-def evaluate_greater_than_or_equal(lhs: ComparableValue, rhs: ComparableValue) -> bool:
-    return lhs >= rhs
-
-
-def evaluate_less_than(lhs: ComparableValue, rhs: ComparableValue) -> bool:
-    return lhs < rhs
-
-
-def evaluate_less_than_or_equal(lhs: ComparableValue, rhs: ComparableValue) -> bool:
-    return lhs <= rhs
-
-
-def evaluate_not(value: bool) -> bool:
-    return not value
-
-
-def evaluate_and(values: Iterable[bool]) -> bool:
-    return all(iter(values))
-
-
-def evaluate_or(values: Iterable[bool]) -> bool:
-    return any(iter(values))
-
-
-def evaluate_count(values: Optional[Sized]) -> int:
-    return len(values or [])
-
-
-@casefold
-def evaluate_in(lhs: NonArrayPrimitiveTypes, rhs: Sequence) -> bool:
+class Operations:
     """
-    The NoneType check for rhs is done inline because this supports operations such as `None in [None, "Yes"]`
-    which is the short form of `value == None or value == "Yes"`.
+    A class to group the operations
     """
-    return rhs is not None and lhs in rhs
 
+    @staticmethod
+    @casefold
+    def evaluate_equal(lhs: ValueTypes, rhs: ValueTypes) -> bool:
+        return lhs == rhs
 
-@casefold
-def evaluate_all_in(lhs: Sequence, rhs: Sequence) -> bool:
-    return all(x in rhs for x in lhs)
+    @staticmethod
+    @casefold
+    def evaluate_not_equal(lhs: ValueTypes, rhs: ValueTypes) -> bool:
+        return lhs != rhs
 
+    @staticmethod
+    def evaluate_greater_than(lhs: ComparableValue, rhs: ComparableValue) -> bool:
+        return lhs > rhs
 
-@casefold
-def evaluate_any_in(lhs: Sequence, rhs: Sequence) -> bool:
-    return any(x in rhs for x in lhs)
+    @staticmethod
+    def evaluate_greater_than_or_equal(
+        lhs: ComparableValue, rhs: ComparableValue
+    ) -> bool:
+        return lhs >= rhs
 
+    @staticmethod
+    def evaluate_less_than(lhs: ComparableValue, rhs: ComparableValue) -> bool:
+        return lhs < rhs
 
-def resolve_date_from_string(
-    date_string: Optional[str], offset: Optional[dict[str, int]] = None
-) -> Optional[date]:
-    datetime_value = (
-        datetime.now(timezone.utc)
-        if date_string == "now"
-        else convert_to_datetime(date_string)
-    )
-    value_as_date = datetime_value.date() if datetime_value else None
+    @staticmethod
+    def evaluate_less_than_or_equal(lhs: ComparableValue, rhs: ComparableValue) -> bool:
+        return lhs <= rhs
 
-    if offset and value_as_date:
-        value_as_date += relativedelta(
-            days=offset.get("days", 0),
-            months=offset.get("months", 0),
-            years=offset.get("years", 0),
+    @staticmethod
+    def evaluate_not(value: bool) -> bool:
+        return not value
+
+    @staticmethod
+    def evaluate_and(values: Iterable[bool]) -> bool:
+        return all(iter(values))
+
+    @staticmethod
+    def evaluate_or(values: Iterable[bool]) -> bool:
+        return any(iter(values))
+
+    @staticmethod
+    def evaluate_count(values: Optional[Sized]) -> int:
+        return len(values or [])
+
+    @staticmethod
+    @casefold
+    def evaluate_in(lhs: NonArrayPrimitiveTypes, rhs: Sequence) -> bool:
+        """
+        The NoneType check for rhs is done inline because this supports operations such as `None in [None, "Yes"]`
+        which is the short form of `value == None or value == "Yes"`.
+        """
+        return rhs is not None and lhs in rhs
+
+    @staticmethod
+    @casefold
+    def evaluate_all_in(lhs: Sequence, rhs: Sequence) -> bool:
+        return all(x in rhs for x in lhs)
+
+    @staticmethod
+    @casefold
+    def evaluate_any_in(lhs: Sequence, rhs: Sequence) -> bool:
+        return any(x in rhs for x in lhs)
+
+    @staticmethod
+    def resolve_date_from_string(
+        date_string: Optional[str], offset: Optional[dict[str, int]] = None
+    ) -> Optional[date]:
+        datetime_value = (
+            datetime.now(timezone.utc)
+            if date_string == "now"
+            else convert_to_datetime(date_string)
         )
+        value_as_date = datetime_value.date() if datetime_value else None
 
-    return value_as_date
+        if offset and value_as_date:
+            value_as_date += relativedelta(
+                days=offset.get("days", 0),
+                months=offset.get("months", 0),
+                years=offset.get("years", 0),
+            )
+
+        return value_as_date
