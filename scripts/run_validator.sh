8be829a7e78e414dd1be78abe6fd58a53cfaef2f
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2021-12-01 14:30:43 +0000

    Remove extra space
    Signed-off-by: Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 tag=use-AJV-validator
-TAG=${tag} docker-compose -f docker-compose-schema-validator.yml  up -d
+TAG=${tag} docker-compose -f docker-compose-schema-validator.yml up -d
