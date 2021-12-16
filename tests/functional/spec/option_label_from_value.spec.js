26fd7007e360f5d5dae0d7b6ed57b1dd52b76679
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2021-12-16 10:08:32 +0000

    Moved the placeholder radiobox to setup and fix title
    Signed-off-by: Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>

--- tests/functional/spec/option_label_from_value.spec.js
+++ tests/functional/spec/option_label_from_value.spec.js
@@ -5,17 +5,17 @@ import RecoveryQuestionCheckboxBlockPage from "../generated_pages/option_label_f
 import MandatoryRadioPage from "../generated_pages/option_label_from_value/mandatory-radio.page";
 import RecoveryQuestionRadioBlockPage from "../generated_pages/option_label_from_value/recovery-question-radio-block.page";
 
-describe("Date checks", () => {
+describe("Option label value check", () => {
   beforeEach("Load the survey", () => {
     browser.openQuestionnaire("test_option_label_from_value.json");
-  });
-
-  it("Given multiple checkbox options are provided, when I select a single answer and go to the next page, then the placeholder contains the label text of the answer I selected", () => {
     $(MandatoryRadioLabel1Page.head()).click();
     $(MandatoryRadioLabel1Page.submit()).click();
 
     $(MandatoryRadioLabel2Page.label()).click();
     $(MandatoryRadioLabel2Page.submit()).click();
+  });
+
+  it("Given multiple checkbox options are provided, when I select a single answer and go to the next page, then the placeholder contains the label text of the answer I selected", () => {
     $(MandatoryCheckboxPage.answerHeadLabel()).click();
 
     $(MandatoryCheckboxPage.submit()).click();
@@ -24,36 +24,19 @@ describe("Date checks", () => {
   });
 
   it("Given multiple checkbox options are provided, when I select more than one answer and go to the next page, then I am asked to select only one answer from a list", () => {
-    $(MandatoryRadioLabel1Page.head()).click();
-    $(MandatoryRadioLabel1Page.submit()).click();
-
-    $(MandatoryRadioLabel2Page.label()).click();
-    $(MandatoryRadioLabel2Page.submit()).click();
-
     $(MandatoryCheckboxPage.answerHeadLabel()).click();
     $(MandatoryCheckboxPage.body()).click();
-
     $(MandatoryCheckboxPage.submit()).click();
-
     expect($(MandatoryRadioPage.questionText()).getText()).to.contain(
       "If you suffered any one injury from the options below, please select the most serious one"
     );
   });
 
   it("Given multiple checkbox options are provided and I subsequently select a single answer from the next displayed list, when I select one answer and go to the next page, then the placeholder contains the label text of the answer I selected", () => {
-    $(MandatoryRadioLabel1Page.head()).click();
-    $(MandatoryRadioLabel1Page.submit()).click();
-
-    $(MandatoryRadioLabel2Page.label()).click();
-    $(MandatoryRadioLabel2Page.submit()).click();
-
     $(MandatoryCheckboxPage.answerHeadLabel()).click();
     $(MandatoryCheckboxPage.body()).click();
-
     $(MandatoryCheckboxPage.submit()).click();
-
     $(MandatoryRadioPage.nose()).click();
-
     $(MandatoryRadioPage.submit()).click();
 
     expect($(RecoveryQuestionRadioBlockPage.questionText()).getText()).to.contain("Nose");
