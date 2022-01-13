cef7bd95ba57c2ace102963143a3addba571859a
petechd <piotr.chudziak@ons.gov.uk>
2022-01-13 13:51:57 +0000

    Fix functional test timeouts and repetition, question description and removes wdio conf change

--- tests/functional/spec/timeout_modal.spec.js
+++ tests/functional/spec/timeout_modal.spec.js
@@ -3,53 +3,43 @@ import TimeoutSubmitPage from "../generated_pages/timeout_modal/submit.page";
 import { TimeoutModalPage } from "../base_pages/timeout-modal.page.js";
 
 describe("Timeout Modal", () => {
-  before("Open Survey", () => {
+  beforeEach("Load the survey", () => {
     browser.openQuestionnaire("test_timeout_modal.json");
+    browser.setTimeout({ script: 70000 });
   });
 
-  it("Given I am completing the survey, when the session timeout is set to 2 minutes I will be able to see the timeout modal with the option to extend the session after 60 seconds", () => {
-    checkTimeoutModal();
-  });
-});
-
-describe("Timeout Modal", () => {
-  before("Open Survey", () => {
-    browser.openQuestionnaire("test_timeout_modal.json");
+  describe("Given I am completing the survey, when the session timeout is set to 2 minutes", () => {
+    it("I will be able to see the timeout modal with the option to extend the session after 60 seconds", () => {
+      checkTimeoutModal();
+    });
   });
 
-  it("Given I am completing the survey, when I click on “Continue survey” button of the timeout modal I will be able to extend the session", () => {
-    checkTimeoutModal();
-    $(TimeoutModalPage.submit()).click();
-    expect($(TimeoutModalPage.timer()).getText()).to.equal("");
+  describe("Given I am completing the survey, when I click on “Continue survey” button of the timeout modal", () => {
+    it("I will be able to extend the session", () => {
+      checkTimeoutModal();
+      $(TimeoutModalPage.submit()).click();
+      expect($(TimeoutModalPage.timer()).getText()).to.equal("");
+    });
   });
-});
-
-describe("Timeout Modal", () => {
-  before("Open Survey", () => {
-    browser.openQuestionnaire("test_timeout_modal.json");
+  describe("Given I am completing the survey, when I open a new window and then focus on the modal window ", () => {
+    it("I will extend the session", () => {
+      checkTimeoutModal();
+      browser.newWindow("");
+      browser.switchWindow("http://localhost:5000/questionnaire/timeout-modal-interstitial/");
+      expect($(TimeoutModalPage.timer()).getText()).to.equal("To protect your information, your progress will be saved and you will be signed out in 1 minute.");
+    });
   });
 
-  it("Given I am completing the survey, when I open a new window and then focus on the modal window I will extend the session", () => {
-    checkTimeoutModal();
-    browser.newWindow("");
-    browser.switchWindow("http://localhost:5000/questionnaire/timeout-modal-interstitial/");
+  describe("Given I am completing the survey, when I get to post submission page", () => {
+    it("I should be able see the timeout modal 60 seconds before session expiry", () => {
+      $(TimeoutInterstitialPage.submit()).click();
+      $(TimeoutSubmitPage.submit()).click();
+      checkTimeoutModal();
+    });
+  });
+
+  function checkTimeoutModal() {
+    $(TimeoutModalPage.timer()).waitForDisplayed({ timeout: 70000 });
     expect($(TimeoutModalPage.timer()).getText()).to.equal("To protect your information, your progress will be saved and you will be signed out in 1 minute.");
-  });
+  }
 });
-
-describe("Timeout Modal", () => {
-  before("Open Survey", () => {
-    browser.openQuestionnaire("test_timeout_modal.json");
-  });
-
-  it("Given I am completing the survey, when I get to post submission page I should be able see the timeout modal 60 seconds before session expiry", () => {
-    $(TimeoutInterstitialPage.submit()).click();
-    $(TimeoutSubmitPage.submit()).click();
-    checkTimeoutModal();
-  });
-});
-
-function checkTimeoutModal() {
-  $(TimeoutModalPage.timer()).waitForDisplayed({ timeout: 70000 });
-  expect($(TimeoutModalPage.timer()).getText()).to.equal("To protect your information, your progress will be saved and you will be signed out in 1 minute.");
-}
