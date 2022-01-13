3358cf92cac627936fa1d973b15b0aef4888f8d7
petechd <piotr.chudziak@ons.gov.uk>
2022-01-13 15:18:45 +0000

    Refactor existing functional tests and spec

--- tests/functional/spec/timeout_modal.spec.js
+++ tests/functional/spec/timeout_modal.spec.js
@@ -5,25 +5,27 @@ import { TimeoutModalPage } from "../base_pages/timeout-modal.page.js";
 describe("Timeout Modal", () => {
   beforeEach("Load the survey", () => {
     browser.openQuestionnaire("test_timeout_modal.json");
+    browser.setTimeout({ script: 70000 });
   });
 
-  describe("Given I am completing the survey, when the session timeout is set to 2 minutes", () => {
-    it("I will be able to see the timeout modal with the option to extend the session after 60 seconds", () => {
+  describe("Given I am completing the survey, when the session timeout is set to 125 seconds", () => {
+    it("It will make the timeout modal with the option to extend the session visible after 65 seconds", () => {
       checkTimeoutModal();
     });
   });
 
   describe("Given I am completing the survey, when I click on “Continue survey” button of the timeout modal", () => {
-    it("I will be able to extend the session", () => {
+    it("It will extend the session", () => {
       checkTimeoutModal();
       $(TimeoutModalPage.submit()).click();
       expect($(TimeoutModalPage.timer()).getText()).to.equal("");
     });
   });
-  describe("Given I am completing the survey, when I open a new window and then focus on the modal window ", () => {
-    it("I will extend the session", () => {
+  describe("Given I am completing the survey, when I open a new window and then focus on the modal window", () => {
+    it("It will extend the session", () => {
       checkTimeoutModal();
       browser.newWindow("");
+      browser.setTimeout({ script: 70000 });
       browser.switchWindow("http://localhost:5000/questionnaire/timeout-modal-interstitial/");
       expect($(TimeoutModalPage.timer()).getText()).to.equal(
         "To protect your information, your progress will be saved and you will be signed out in 1 minute."
@@ -32,7 +34,7 @@ describe("Timeout Modal", () => {
   });
 
   describe("Given I am completing the survey, when I get to post submission page", () => {
-    it("I should be able see the timeout modal 60 seconds before session expiry", () => {
+    it("It will make the timeout modal visible 60 seconds before session expiry", () => {
       $(TimeoutInterstitialPage.submit()).click();
       $(TimeoutSubmitPage.submit()).click();
       checkTimeoutModal();
