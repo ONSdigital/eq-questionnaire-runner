d5e5ee33b305da7572320b7a04eb630a1a1af0a6
LJBabbage <lewis.babbage@ons.gov.uk>
2022-01-18 14:58:10 +0000

    Update Authentication tests to pytest (#760)
    * Update Authentication tests to pytest

--- tests/app/authentication/test_expired_token_exception.py
+++ tests/app/authentication/test_expired_token_exception.py
@@ -1,15 +1,8 @@
-import unittest
-
 from app.authentication.no_questionnaire_state_exception import (
     NoQuestionnaireStateException,
 )
 
 
-class NoQuestionnaireStateExceptionTest(unittest.TestCase):
-    def test(self):
-        no_token = NoQuestionnaireStateException("test")
-        self.assertEqual("test", str(no_token))
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_no_questionnaire_state_exception():
+    no_token = NoQuestionnaireStateException("test")
+    assert "test" == str(no_token)
