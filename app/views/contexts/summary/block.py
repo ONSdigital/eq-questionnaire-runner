1dcec03d605cdbece3559f4b906bc03a1b717968
James Nunn <james.nunn@ext.ons.gov.uk>
2022-01-27 11:05:52 +0000

    use schema methods to get answers

--- app/views/contexts/summary/block.py
+++ app/views/contexts/summary/block.py
@@ -21,8 +21,9 @@ class Block:
         self.location = location
         self.title = block_schema.get("title")
         self.number = block_schema.get("number")
+        first_answer_id_for_block = schema.get_answer_ids_for_block(self.id)[0]
         self.link = self._build_link(
-            block_schema["id"], return_to, schema.get_answer_ids_for_block(self.id)[0]
+            block_schema["id"], return_to, first_answer_id_for_block
         )
         self.question = self.get_question(
             block_schema,
