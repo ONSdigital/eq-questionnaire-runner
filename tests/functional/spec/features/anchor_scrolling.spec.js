5d5965db3a55789e5e4581a88f092f3b9c2b567a
James Nunn <james.nunn@ext.ons.gov.uk>
2022-01-28 09:32:27 +0000

    Update functional tests

--- tests/functional/spec/features/anchor_scrolling.spec.js
+++ tests/functional/spec/features/anchor_scrolling.spec.js
@@ -8,7 +8,7 @@ import HouseholdDetailsSummaryPage from "../../generated_pages/section_summary/h
 import NumberOfPeoplePage from "../../generated_pages/section_summary/number-of-people.page.js";
 import SubmitPage from "../../generated_pages/section_summary/submit.page.js";
 
-describe("Section Summary", () => {
+describe("Summary Anchor Scrolling", () => {
   describe("Given I start a Test Section Summary survey", () => {
     beforeEach(() => {
       browser.openQuestionnaire("test_section_summary.json");
@@ -16,13 +16,13 @@ describe("Section Summary", () => {
       $(InsuranceTypePage.submit()).click();
     });
 
-    it("When I have provided an answer and click through to the next question, the Previous button url shouldn't yet contain any anchors or reference to return_to or return_to_answer_id", () => {
+    it("When I have provided an answer and click through to the next question, Then the Previous link url shouldn't contain any anchors or reference to return_to or return_to_answer_id", () => {
       expect($(InsuranceAddressPage.previous()).getAttribute("href")).not.to.contain("#");
       expect($(InsuranceAddressPage.previous()).getAttribute("href")).not.to.contain("return_to");
       expect($(InsuranceAddressPage.previous()).getAttribute("href")).not.to.contain("return_to_answer_id");
     });
 
-    it("When I reach the summary page, the Change button url should contain return_to, return_to_answer_id query params", () => {
+    it("When I reach the section summary page, Then the Change link url should contain return_to, return_to_answer_id query params", () => {
       $(InsuranceAddressPage.submit()).click();
       $(ListedPage.submit()).click();
       expect($(PropertyDetailsSummaryPage.insuranceAddressAnswerEdit()).getAttribute("href")).to.contain(
@@ -30,16 +30,22 @@ describe("Section Summary", () => {
       );
     });
 
-    it("When I edit an answer from the summary page, the Previous button url should contain an anchor referencing the answer id of the answer I am changing and when clicked, the browser url contains the same", () => {
+    it("When I edit an answer from the section summary page, Then the Previous link url should contain an anchor referencing the answer id of the answer I am changing", () => {
       $(InsuranceAddressPage.submit()).click();
       $(ListedPage.submit()).click();
       $(PropertyDetailsSummaryPage.insuranceAddressAnswerEdit()).click();
       expect($(InsuranceAddressPage.previous()).getAttribute("href")).to.contain("/questionnaire/sections/property-details-section/#insurance-address-answer");
+    });
+
+    it("When I edit an answer from the section summary page and click the Previous link, Then the browser url should contain an anchor referencing the answer id of the answer I am changing", () => {
+      $(InsuranceAddressPage.submit()).click();
+      $(ListedPage.submit()).click();
+      $(PropertyDetailsSummaryPage.insuranceAddressAnswerEdit()).click();
       $(InsuranceAddressPage.previous()).click();
       expect(browser.getUrl()).to.contain("/questionnaire/sections/property-details-section/#insurance-address-answer");
     });
 
-    it("When I edit an answer from the final summary page, the Change button url should contain return_to, return_to_answer_id query params and when clicked, the browser url contains the same", () => {
+    it("When I edit an answer from the final summary page, Then the Change link url should contain return_to, return_to_answer_id query params", () => {
       $(InsuranceAddressPage.submit()).click();
       $(ListedPage.submit()).click();
       $(InsuranceAddressPage.submit()).click();
@@ -54,6 +60,20 @@ describe("Section Summary", () => {
       expect($(SubmitPage.insuranceAddressAnswerEdit()).getAttribute("href")).to.contain(
         "/questionnaire/insurance-address/?return_to=final-summary&return_to_answer_id=insurance-address-answer#insurance-address-answer"
       );
+    });
+
+    it("When I edit an answer from the final summary page, Then the browser url contains return_to, return_to_answer_id query params", () => {
+      $(InsuranceAddressPage.submit()).click();
+      $(ListedPage.submit()).click();
+      $(InsuranceAddressPage.submit()).click();
+      $(ListedPage.submit()).click();
+      $(PropertyDetailsSummaryPage.submit()).click();
+      $(HouseType.submit()).click();
+      $(HouseholdDetailsSummaryPage.submit()).click();
+      $(NumberOfPeoplePage.answer()).setValue(3);
+      $(NumberOfPeoplePage.submit()).click();
+      $(HouseholdCountSectionSummaryPage.submit()).click();
+      $(SubmitPage.summaryShowAllButton()).click();
       $(SubmitPage.insuranceAddressAnswerEdit()).click();
       expect(browser.getUrl()).to.contain(
         "/questionnaire/insurance-address/?return_to=final-summary&return_to_answer_id=insurance-address-answer#insurance-address-answer"
