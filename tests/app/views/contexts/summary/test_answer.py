4a43478ea5cd4daf3d54f155a8e56a6500b6e4a3
James Nunn <james.nunn@ext.ons.gov.uk>
2022-02-02 10:09:10 +0000

    use kwargs, update test

--- tests/app/views/contexts/summary/test_answer.py
+++ tests/app/views/contexts/summary/test_answer.py
@@ -6,6 +6,7 @@ class TestAnswer(AppContextTestCase):
     def test_create_answer(self):
         answer_schema = {"id": "answer-id", "label": "Answer Label", "type": "date"}
         user_answer = "An answer"
+
         answer = Answer(
             answer_schema=answer_schema,
             answer_value=user_answer,
@@ -24,3 +25,21 @@ class TestAnswer(AppContextTestCase):
             answer.link,
             "http://test.localdomain/questionnaire/address-list/address-item-id/house-type/?return_to=section-summary&return_to_answer_id=answer-id#answer-id",
         )
+
+    def test_date_answer_type(self):
+        # Given
+        answer_schema = {"id": "answer-id", "label": "", "type": "date"}
+        user_answer = None
+
+        # When
+        answer = Answer(
+            answer_schema=answer_schema,
+            answer_value=user_answer,
+            block_id="house-type",
+            list_name="address-list",
+            list_item_id="address-item-id",
+            return_to="section-summary",
+        )
+
+        # Then
+        self.assertEqual(answer.type, "date")
