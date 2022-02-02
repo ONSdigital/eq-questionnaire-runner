457d5200c394446de8a3acce603ffbb4dcf848bc
LJBabbage <lewis.babbage@ons.gov.uk>
2022-02-02 14:18:56 +0000

    Split test upto min and max and add extra combos

--- tests/app/forms/validation/test_date_range_validator.py
+++ tests/app/forms/validation/test_date_range_validator.py
@@ -28,6 +28,7 @@ def test_invalid_date_range(
 
     with pytest.raises(ValidationError) as context:
         date_range_check(mock_form, mock_period_from, mock_period_to)
+
     assert error_messages["INVALID_DATE_RANGE"] == str(context.value)
 
 
@@ -81,43 +82,63 @@ def test_bespoke_message_playback(
     assert "Test 2 years, 1 month, 3 days" == str(context.value)
 
 
-def test_message_combinations(
-    get_date_range_check, mock_form, mock_period_from, mock_period_to
+@pytest.mark.parametrize(
+    "period_max, expected_result",
+    [
+        ({"years": 2, "months": 1, "days": 3}, "2 years, 1 month, 3 days"),
+        ({"months": 2, "days": 1}, "2 months, 1 day"),
+        ({"days": 3}, "3 days"),
+    ],
+)
+def test_date_range_period_max(
+    get_date_range_check,
+    mock_form,
+    mock_period_from,
+    mock_period_to,
+    period_max,
+    expected_result,
 ):
     mock_period_from.data = "2016-02-11"
     mock_period_to.data = "2018-03-19"
-    period_max = {"years": 2, "months": 1, "days": 3}
 
     date_range_check = get_date_range_check(period_max=period_max)
 
     with pytest.raises(ValidationError) as context:
         date_range_check(mock_form, mock_period_from, mock_period_to)
 
-    assert (
-        "Enter a reporting period less than or equal to 2 years, 1 month, 3 days"
-        == str(context.value)
-    )
-
-    period_max = {"months": 2, "days": 1}
-
-    date_range_check = get_date_range_check(period_max=period_max)
-
-    with pytest.raises(ValidationError) as context:
-        date_range_check(mock_form, mock_period_from, mock_period_to)
-
-    assert "Enter a reporting period less than or equal to 2 months, 1 day" == str(
+    assert f"Enter a reporting period less than or equal to {expected_result}" == str(
         context.value
     )
 
-    period_min = {"years": 3, "days": 2}
+
+@pytest.mark.parametrize(
+    "period_min, expected_result",
+    [
+        ({"years": 3, "months": 2}, "3 years, 2 months"),
+        ({"years": 3, "days": 2}, "3 years, 2 days"),
+        ({"years": 3}, "3 years"),
+        ({"months": 3}, "3 months"),
+    ],
+)
+def test_date_range_period_min(
+    get_date_range_check,
+    mock_form,
+    mock_period_from,
+    mock_period_to,
+    period_min,
+    expected_result,
+):
+    mock_period_from.data = "2016-02-11"
+    mock_period_to.data = "2016-03-19"
 
     date_range_check = get_date_range_check(period_min=period_min)
 
     with pytest.raises(ValidationError) as context:
         date_range_check(mock_form, mock_period_from, mock_period_to)
 
-    assert "Enter a reporting period greater than or equal to 3 years, 2 days" == str(
-        context.value
+    assert (
+        f"Enter a reporting period greater than or equal to {expected_result}"
+        == str(context.value)
     )
 
 
