5887c75dfd940e30a4f47e0ab50d90cd18937d17
petechd <piotr.chudziak@ons.gov.uk>
2022-02-03 14:27:42 +0000

    Remove submit page test

--- tests/functional/spec/theme_northernireland.spec.js
+++ tests/functional/spec/theme_northernireland.spec.js
@@ -1,5 +1,4 @@
 import RadioPage from "../generated_pages/theme_northernireland/radio.page";
-import SubmitPage from "../generated_pages/theme_northernireland/submit.page.js";
 
 describe("Theme Northern Ireland", () => {
   describe("Given I launch a Northern Ireland themed questionnaire", () => {
@@ -9,11 +8,6 @@ describe("Theme Northern Ireland", () => {
 
     it("When I navigate to the radio page, Then I should see Northern Ireland theme content", () => {
       expect(browser.getUrl()).to.contain(RadioPage.pageName);
-      expect($("#ni-finance-logo-alt").getHTML()).to.contain("Northern Ireland Department of Finance logo");
-    });
-    it("When I navigate to the submit page, Then I should see Northern Ireland theme content", () => {
-      $(SubmitPage.submit()).click();
-      expect(browser.getUrl()).to.contain(SubmitPage.pageName);
       expect($("#ni-finance-logo-alt").getHTML()).to.contain("Northern Ireland Department of Finance logo");
     });
   });
