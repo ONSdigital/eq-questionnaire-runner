f2d71d4790c91329296d8efc489e6af3e5c34360
James Nunn <james.nunn@ext.ons.gov.uk>
2022-02-07 14:05:50 +0000

    access <p> attribute

--- tests/functional/spec/question_description.spec.js
+++ tests/functional/spec/question_description.spec.js
@@ -3,6 +3,6 @@ import NameBlockPage from "../generated_pages/question_description/name-block.pa
 describe("Question description", () => {
   it("Given a question description has been set in the schema as an array, it is displayed correctly as a newline-separated string", () => {
     browser.openQuestionnaire("test_question_description.json");
-    expect($(NameBlockPage.questionTitle()).getText()).to.contain("Answer the question\nGo on");
+    expect($(NameBlockPage.questionTitle()).getHTML()).to.contain("<p>Answer the question</p><p>Go on</p>");
   });
 });
