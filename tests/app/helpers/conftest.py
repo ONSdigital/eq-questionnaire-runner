9f8c927cd23cd082e5989459e9671e9c515be19b
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2022-02-08 09:32:21 +0000

    Pytest helper framework module (#786)

--- tests/app/helpers/conftest.py
+++ tests/app/helpers/conftest.py
@@ -1,12 +1,194 @@
-from flask.app import Flask
 from pytest import fixture
 
-from app.setup import create_app
+from app.helpers.template_helpers import ContextHelper
 
 
 @fixture
-def app() -> Flask:
-    setting_overrides = {"LOGIN_DISABLED": True}
-    _app = create_app(setting_overrides=setting_overrides)
+def get_context_helper():
+    def _context_helper(
+        app, survey_config, is_post_submission=False, include_csrf_token=True
+    ):
+        with app.test_client():
+            return ContextHelper(
+                language="en",
+                is_post_submission=is_post_submission,
+                include_csrf_token=include_csrf_token,
+                survey_config=survey_config,
+            )
 
-    return _app
+    return _context_helper
+
+
+@fixture(name="footer_context")
+def footer():
+    return {
+        "lang": "en",
+        "crest": True,
+        "newTabWarning": "The following links open in a new tab",
+        "copyrightDeclaration": {
+            "copyright": "Crown copyright and database rights 2020 OS 100019153.",
+            "text": "Use of address data is subject to the terms and conditions.",
+        },
+    }
+
+
+@fixture
+def expected_footer_census_theme(footer_context):
+    census = {
+        "rows": [
+            {
+                "itemsList": [
+                    {
+                        "text": "Help",
+                        "url": "https://census.gov.uk/help/how-to-answer-questions/online-questions-help/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Contact us",
+                        "url": "https://census.gov.uk/contact-us/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Languages",
+                        "url": "https://census.gov.uk/help/languages-and-accessibility/languages/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "BSL and audio videos",
+                        "url": "https://census.gov.uk/help/languages-and-accessibility/accessibility/accessible-videos-with-bsl/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+        "legal": [
+            {
+                "itemsList": [
+                    {
+                        "text": "Cookies",
+                        "url": "https://census.gov.uk/cookies/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Accessibility statement",
+                        "url": "https://census.gov.uk/accessibility-statement/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Privacy and data protection",
+                        "url": "https://census.gov.uk/privacy-and-data-protection/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Terms and conditions",
+                        "url": "https://census.gov.uk/terms-and-conditions/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+    }
+    return {**footer_context, **census}
+
+
+@fixture
+def expected_footer_business_theme(footer_context):
+    business = {
+        "rows": [
+            {
+                "itemsList": [
+                    {
+                        "text": "What we do",
+                        "url": "https://www.ons.gov.uk/aboutus/whatwedo/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Contact us",
+                        "url": "https://surveys.ons.gov.uk/contact-us/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Accessibility",
+                        "url": "https://www.ons.gov.uk/help/accessibility/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+        "legal": [
+            {
+                "itemsList": [
+                    {
+                        "text": "Cookies",
+                        "url": "https://surveys.ons.gov.uk/cookies/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Privacy and data protection",
+                        "url": "https://surveys.ons.gov.uk/privacy-and-data-protection/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+    }
+    return {**footer_context, **business}
+
+
+@fixture
+def expected_footer_nisra_theme():
+    return {
+        "lang": "en",
+        "crest": True,
+        "newTabWarning": "The following links open in a new tab",
+        "copyrightDeclaration": {
+            "copyright": "Crown copyright and database rights 2021 NIMA MOU577.501.",
+            "text": "Use of address data is subject to the terms and conditions.",
+        },
+        "rows": [
+            {
+                "itemsList": [
+                    {
+                        "text": "Help",
+                        "url": "https://census.gov.uk/ni/help/help-with-the-questions/online-questions-help/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Contact us",
+                        "url": "https://census.gov.uk/ni/contact-us/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+        "legal": [
+            {
+                "itemsList": [
+                    {
+                        "text": "Cookies",
+                        "url": "https://census.gov.uk/ni/cookies/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Accessibility statement",
+                        "url": "https://census.gov.uk/ni/accessibility-statement/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Privacy and data protection",
+                        "url": "https://census.gov.uk/ni/privacy-and-data-protection/",
+                        "target": "_blank",
+                    },
+                    {
+                        "text": "Terms and conditions",
+                        "url": "https://census.gov.uk/ni/terms-and-conditions/",
+                        "target": "_blank",
+                    },
+                ]
+            }
+        ],
+        "poweredBy": {
+            "logo": "nisra-logo-black-en",
+            "alt": "NISRA - Northern Ireland Statistics and Research Agency",
+        },
+    }
