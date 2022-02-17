e5a8c8df394f77e7e9c5c617637836bee4d94dff
James Nunn <2095017+jamesnunn@users.noreply.github.com>
2022-02-17 09:10:33 +0000

    Migrate app/forms/validation to pytest (#787)

--- tests/app/forms/validation/test_mobile_number_validation.py
+++ tests/app/forms/validation/test_mobile_number_validation.py
@@ -1,86 +1,47 @@
-import unittest
-from unittest.mock import Mock, patch
-
+import pytest
 from wtforms.validators import ValidationError
 
 from app.forms import error_messages
 from app.forms.validators import MobileNumberCheck, sanitise_mobile_number
 
 
-# pylint: disable=no-member,no-self-use
-@patch("app.jinja_filters.flask_babel.get_locale", Mock(return_value="en_GB"))
-class TestPhoneNumberValidator(unittest.TestCase):
-    def test_sanitise_mobile_number(self):
-        assert sanitise_mobile_number("0.7.7.0.0.9.0.0.1.1.1.") == "7700900111"
-        assert sanitise_mobile_number("0447700\t900222") == "7700900222"
-        assert sanitise_mobile_number("07700-(900333)") == "7700900333"
-        assert sanitise_mobile_number("/0770/090/0444") == "7700900444"
-        assert sanitise_mobile_number("00447700 900555") == "7700900555"
-        assert sanitise_mobile_number("0447700 900555") == "7700900555"
-        assert sanitise_mobile_number("+447700 900666") == "7700900666"
-        assert sanitise_mobile_number("[07700] 900777") == "7700900777"
-        assert sanitise_mobile_number("(07700) {900888}") == "7700900888"
-        assert sanitise_mobile_number("+440447700900999") == "0447700900999"
-        assert sanitise_mobile_number(" 09[8./{}756gf}/{h]fgh") == "98756gfhfgh"
-        assert sanitise_mobile_number("7700900222") == "7700900222"
-        assert sanitise_mobile_number("07700900222") == "7700900222"
+@pytest.mark.parametrize(
+    "unsanitized_number,expected",
+    (
+        ("0.7.7.0.0.9.0.0.1.1.1.", "7700900111"),
+        ("0447700\t900222", "7700900222"),
+        ("07700-(900333)", "7700900333"),
+        ("/0770/090/0444", "7700900444"),
+        ("00447700 900555", "7700900555"),
+        ("0447700 900555", "7700900555"),
+        ("+447700 900666", "7700900666"),
+        ("[07700] 900777", "7700900777"),
+        ("(07700) {900888}", "7700900888"),
+        ("+440447700900999", "0447700900999"),
+        (" 09[8./{}756gf}/{h]fgh", "98756gfhfgh"),
+        ("7700900222", "7700900222"),
+        ("07700900222", "7700900222"),
+    ),
+)
+def test_sanitise_mobile_number(unsanitized_number, expected):
+    assert sanitise_mobile_number(unsanitized_number) == expected
 
-    def test_string_number_too_long(self):
-        validator = MobileNumberCheck()
 
-        mock_form = Mock()
-        mock_field = Mock()
-        mock_field.data = "7700900111789101112"
+@pytest.mark.parametrize(
+    "number",
+    (
+        "7700900111789101112",
+        "`-tykg07700900222",
+        "07700e90033",
+        "06700900222",
+        "൦൧൨൩൪൫൬൭൮൯",
+    ),
+)
+def test_invalid_numbers_raise_ValidationError(number, mock_form, mock_field):
+    validator = MobileNumberCheck()
+    mock_field.data = number
 
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, mock_field)
+    with pytest.raises(ValidationError) as exc:
+        validator(mock_form, mock_field)
 
-        self.assertEqual(error_messages["INVALID_MOBILE_NUMBER"], str(ite.exception))
-
-    def test_string_invalid_chars(self):
-        validator = MobileNumberCheck()
-
-        mock_form = Mock()
-        mock_field = Mock()
-        mock_field.data = "`-tykg07700900222"
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, mock_field)
-
-        self.assertEqual(error_messages["INVALID_MOBILE_NUMBER"], str(ite.exception))
-
-    def test_non_numeric_string_invalid(self):
-        validator = MobileNumberCheck()
-
-        mock_form = Mock()
-        mock_field = Mock()
-        mock_field.data = "07700e90033"
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, mock_field)
-
-        self.assertEqual(error_messages["INVALID_MOBILE_NUMBER"], str(ite.exception))
-
-    def test_prefix_service_type_invalid(self):
-        validator = MobileNumberCheck()
-
-        mock_form = Mock()
-        mock_field = Mock()
-        mock_field.data = "06700900222"
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, mock_field)
-
-        self.assertEqual(error_messages["INVALID_MOBILE_NUMBER"], str(ite.exception))
-
-    def test_non_utf8_string_invalid(self):
-        validator = MobileNumberCheck()
-
-        mock_form = Mock()
-        mock_field = Mock()
-        mock_field.data = "൦൧൨൩൪൫൬൭൮൯"
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, mock_field)
-
-        self.assertEqual(error_messages["INVALID_MOBILE_NUMBER"], str(ite.exception))
+    assert error_messages["INVALID_MOBILE_NUMBER"] == str(exc.value)
--- tests/app/forms/validation/test_sum_check_validator.py
+++ tests/app/forms/validation/test_sum_check_validator.py
@@ -1,172 +1,86 @@
-import unittest
-from unittest.mock import Mock, patch
-
+import pytest
 from wtforms.validators import ValidationError
 
 from app.forms import error_messages
 from app.forms.validators import SumCheck, format_playback_value
 
 
-@patch("app.jinja_filters.flask_babel.get_locale", Mock(return_value="en_GB"))
-class TestSumCheckValidator(unittest.TestCase):
-    """
-    Sum check validator evaluates a calculated total against a target total, given a condition
-    """
+@pytest.mark.parametrize(
+    "conditions,calculation_total,target_total,error_type",
+    (
+        (["equals"], 10, 11.5, "TOTAL_SUM_NOT_EQUALS"),
+        (["less than", "equals"], 20, 11.5, "TOTAL_SUM_NOT_LESS_THAN_OR_EQUALS"),
+        (["less than"], 11.99, 11.5, "TOTAL_SUM_NOT_LESS_THAN"),
+        (["greater than"], 11.99, 12.5, "TOTAL_SUM_NOT_GREATER_THAN"),
+        (
+            ["greater than", "equals"],
+            11.99,
+            12.5,
+            "TOTAL_SUM_NOT_GREATER_THAN_OR_EQUALS",
+        ),
+    ),
+)
+@pytest.mark.usefixtures("gb_locale")
+def test_sum_check_invalid_raises_ValidationError(
+    conditions, calculation_total, target_total, error_type, mock_form
+):
+    validator = SumCheck()
 
-    def test_equal_condition_check(self):
-        validator = SumCheck()
+    with pytest.raises(ValidationError) as exc:
+        validator(mock_form, conditions, calculation_total, target_total)
 
-        mock_form = Mock()
+    assert error_messages[error_type] % {
+        "total": format_playback_value(target_total)
+    } == str(exc.value)
 
-        conditions = ["equals"]
-        calculation_total = 10
-        target_total = 11.5
 
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
+@pytest.mark.usefixtures("gb_locale")
+def test_currency_playback(mock_form):
+    validator = SumCheck(currency="EUR")
 
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_EQUALS"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
+    conditions = ["equals"]
+    calculation_total = 10
+    target_total = 11.5
 
-    def test_less_or_equal_condition_check(self):
-        validator = SumCheck()
+    with pytest.raises(ValidationError) as exc:
+        validator(mock_form, conditions, calculation_total, target_total)
 
-        mock_form = Mock()
+    assert (
+        error_messages["TOTAL_SUM_NOT_EQUALS"]
+        % {
+            "total": format_playback_value(target_total, currency="EUR"),
+        }
+        == str(exc.value)
+    )
 
-        conditions = ["less than", "equals"]
-        calculation_total = 20
-        target_total = 11.5
 
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
+@pytest.mark.usefixtures("gb_locale")
+def test_bespoke_message_playback(mock_form):
+    message = {"TOTAL_SUM_NOT_EQUALS": "Test %(total)s"}
+    validator = SumCheck(messages=message)
 
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_LESS_THAN_OR_EQUALS"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
+    conditions = ["equals"]
+    calculation_total = 10
+    target_total = 11.5
 
-    def test_less_than_condition_check(self):
-        validator = SumCheck()
+    with pytest.raises(ValidationError) as exc:
+        validator(mock_form, conditions, calculation_total, target_total)
 
-        mock_form = Mock()
+    assert f"Test {target_total}" == str(exc.value)
 
-        conditions = ["less than"]
-        calculation_total = 11.99
-        target_total = 11.5
 
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
+@pytest.mark.usefixtures("gb_locale")
+def test_invalid_multiple_conditions(mock_form):
+    validator = SumCheck()
 
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_LESS_THAN"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
+    conditions = ["less than", "greater than"]
+    calculation_total = 10
+    target_total = 11.5
 
-    def test_greater_than_condition_check(self):
-        validator = SumCheck()
+    with pytest.raises(Exception) as exc:
+        validator(mock_form, conditions, calculation_total, target_total)
 
-        mock_form = Mock()
-
-        conditions = ["greater than"]
-        calculation_total = 11.99
-        target_total = 12.5
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_GREATER_THAN"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
-
-    def test_greater_or_equal_condition_check(self):
-        validator = SumCheck()
-
-        mock_form = Mock()
-
-        conditions = ["greater than", "equals"]
-        calculation_total = 11.99
-        target_total = 12.5
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_GREATER_THAN_OR_EQUALS"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
-
-    def test_currency_playback(self):
-        validator = SumCheck(currency="EUR")
-
-        mock_form = Mock()
-
-        conditions = ["equals"]
-        calculation_total = 10
-        target_total = 11.5
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_EQUALS"]
-            % {"total": format_playback_value(target_total, currency="EUR")},
-            str(ite.exception),
-        )
-
-    def test_bespoke_message_playback(self):
-        message = {"TOTAL_SUM_NOT_EQUALS": "Test %(total)s"}
-        validator = SumCheck(messages=message)
-
-        mock_form = Mock()
-
-        conditions = ["equals"]
-        calculation_total = 10
-        target_total = 11.5
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(f"Test {target_total}", str(ite.exception))
-
-    def test_valid_multiple_conditions(self):
-        validator = SumCheck()
-
-        mock_form = Mock()
-
-        conditions = ["less than", "equals"]
-        calculation_total = 13
-        target_total = 11.5
-
-        with self.assertRaises(ValidationError) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(
-            error_messages["TOTAL_SUM_NOT_LESS_THAN_OR_EQUALS"]
-            % {"total": format_playback_value(target_total)},
-            str(ite.exception),
-        )
-
-    def test_invalid_multiple_conditions(self):
-        validator = SumCheck()
-
-        mock_form = Mock()
-
-        conditions = ["less than", "greater than"]
-        calculation_total = 10
-        target_total = 11.5
-
-        with self.assertRaises(Exception) as ite:
-            validator(mock_form, conditions, calculation_total, target_total)
-
-        self.assertEqual(
-            "There are multiple conditions, but equals is not one of them. We only support <= and >=",
-            str(ite.exception),
-        )
+    assert (
+        "There are multiple conditions, but equals is not one of them. We only support <= and >="
+        == str(exc.value)
+    )
