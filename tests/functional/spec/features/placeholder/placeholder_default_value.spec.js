bd1e70753c4380949d970fd31f2140bd27598c26
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2022-02-23 16:52:11 +0000

    Add currency in schema and tests
    Signed-off-by: Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>

--- tests/functional/spec/features/placeholder/placeholder_default_value.spec.js
+++ tests/functional/spec/features/placeholder/placeholder_default_value.spec.js
@@ -1,4 +1,6 @@
 import EmployeesNumberBlockPage from "../../../generated_pages/placeholder_default_value/employees-number-block.page";
+import EmployeesTrainingBlockPage from "../../../generated_pages/placeholder_default_value/employees-training-block.page";
+import EmployeesNumberDefaultInterstitialPage from "../../../generated_pages/placeholder_default_value/employees-number-default-interstitial.page";
 
 describe("Placeholder default value check", () => {
   beforeEach("Load the survey", () => {
@@ -14,5 +16,18 @@ describe("Placeholder default value check", () => {
     $(EmployeesNumberBlockPage.employeesNo()).setValue("54");
     $(EmployeesNumberBlockPage.submit()).click();
     expect($("#main-content > p").getText()).to.contain("The total number of employees confirmed are 54");
+  });
+  it("Given a training budget question with default answer, When I do not enter any amount and click submit, Then the interstitial page shows default amount as 250.00", () => {
+    $(EmployeesNumberBlockPage.submit()).click();
+    $(EmployeesNumberDefaultInterstitialPage.submit()).click();
+    $(EmployeesTrainingBlockPage.submit()).click();
+    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £250.00 (default value)");
+  });
+  it("Given a training budget question with default answer, When I enter an amount and click submit, Then the interstitial page shows amount entered", () => {
+    $(EmployeesNumberBlockPage.submit()).click();
+    $(EmployeesNumberDefaultInterstitialPage.submit()).click();
+    $(EmployeesTrainingBlockPage.employeesAvgTraining()).setValue("200");
+    $(EmployeesTrainingBlockPage.submit()).click();
+    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £200.00");
   });
 });
