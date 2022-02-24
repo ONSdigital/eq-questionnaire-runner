75c8ab31cb15227d834e08ca8c3fb8a9d1b711d6
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2022-02-24 13:18:34 +0000

    simplify schema and update tests
    Signed-off-by: Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>

--- tests/functional/spec/features/placeholder/placeholder_default_value.spec.js
+++ tests/functional/spec/features/placeholder/placeholder_default_value.spec.js
@@ -1,6 +1,6 @@
 import EmployeesNumberBlockPage from "../../../generated_pages/placeholder_default_value/employees-number-block.page";
 import EmployeesTrainingBlockPage from "../../../generated_pages/placeholder_default_value/employees-training-block.page";
-import EmployeesNumberDefaultInterstitialPage from "../../../generated_pages/placeholder_default_value/employees-number-default-interstitial.page";
+import EmployeesNumberInterstitialPage from "../../../generated_pages/placeholder_default_value/employees-number-interstitial.page";
 
 describe("Placeholder default value check", () => {
   beforeEach("Load the survey", () => {
@@ -9,7 +9,7 @@ describe("Placeholder default value check", () => {
 
   it("Given a question with default answer, When I do not enter any number and click submit, Then the interstitial page shows default employees number as 0", () => {
     $(EmployeesNumberBlockPage.submit()).click();
-    expect($("#main-content > p").getText()).to.contain("The total number of employees confirmed are 0 (default value)");
+    expect($("#main-content > p").getText()).to.contain("The total number of employees confirmed are 0");
   });
 
   it("Given a question with default answer, When I enter a number of employee and click submit, Then the interstitial page shows me the employees number entered", () => {
@@ -19,15 +19,15 @@ describe("Placeholder default value check", () => {
   });
   it("Given a training budget question with default answer, When I do not enter any amount and click submit, Then the interstitial page shows default amount as 250.00", () => {
     $(EmployeesNumberBlockPage.submit()).click();
-    $(EmployeesNumberDefaultInterstitialPage.submit()).click();
+    $(EmployeesNumberInterstitialPage.submit()).click();
     $(EmployeesTrainingBlockPage.submit()).click();
-    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £250.00 (default value)");
+    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £250.00");
   });
   it("Given a training budget question with default answer, When I enter an amount and click submit, Then the interstitial page shows amount entered", () => {
     $(EmployeesNumberBlockPage.submit()).click();
-    $(EmployeesNumberDefaultInterstitialPage.submit()).click();
-    $(EmployeesTrainingBlockPage.employeesAvgTraining()).setValue("200");
+    $(EmployeesNumberInterstitialPage.submit()).click();
+    $(EmployeesTrainingBlockPage.employeesAvgTraining()).setValue("100");
     $(EmployeesTrainingBlockPage.submit()).click();
-    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £200.00");
+    expect($("#main-content > p").getText()).to.contain("The average training budget per employee is £100.00");
   });
 });
