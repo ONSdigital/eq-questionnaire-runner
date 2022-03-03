cc6712d9aef0797722f212cb403281e6dbd33741
petechd <piotr.chudziak@ons.gov.uk>
2022-03-03 12:53:33 +0000

    Add changes

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-tag=latest
+tag=introduction_guidance
 TAG=${tag} docker-compose -f docker-compose-schema-validator.yml up -d
--- tests/functional/spec/introduction.spec.js
+++ tests/functional/spec/introduction.spec.js
@@ -3,7 +3,7 @@ import IntroductionPage from "../generated_pages/introduction/introduction.page"
 describe("Introduction page", () => {
   const introductionSchema = "test_introduction.json";
 
-  it("Given I start a survey, When I view the introduction page then I should be able to see introduction information", () => {
+  it("Given I start a survey, When I view the introduction page, Then I should be able to see introduction information", () => {
     browser.openQuestionnaire(introductionSchema);
     expect($(IntroductionPage.useOfData()).getText()).to.contain("How we use your data");
     expect($(IntroductionPage.useOfInformation()).getText()).to.contain("What you need to do next");
@@ -11,6 +11,18 @@ describe("Introduction page", () => {
     expect($(IntroductionPage.legalBasis()).getText()).to.contain("Notice is given under section 999 of the Test Act 2000");
     expect($(IntroductionPage.introDescription()).getText()).to.contain(
       "To take part, all you need to do is check that you have the information you need to answer the survey questions."
+    );
+  });
+});
+
+describe("Introduction page", () => {
+  const introductionSchema = "test_introduction_with_guidance.json";
+
+  it("Given I start a survey with introduction guidance set, When I view the introduction page, Then I should be able to see introduction guidance", () => {
+    browser.openQuestionnaire(introductionSchema);
+    expect($("#item-guidance-business-details > div").getText()).to.contain("Coronavirus (COVID-19) guidance");
+    expect($("#item-guidance-business-details > div > p").getText()).to.contain(
+      "Explain your figures in the comment section to minimise us contacting you and to help us tell an industry story"
     );
   });
 });
