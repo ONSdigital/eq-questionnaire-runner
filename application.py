897246c551555429d9d757f3dbcf8b8a3ca75194
petechd <piotr.chudziak@ons.gov.uk>
2022-03-04 12:52:19 +0000

    Add wait

--- application.py
+++ application.py
@@ -80,11 +80,5 @@ from app.setup import (  # NOQA isort:skip # pylint: disable=wrong-import-positi
 application = create_app()
 
 if __name__ == "__main__":
-    logger = get_logger()
-    logger.error(f"hey guys {os.environ.get('SERVER_SOFTWARE', 'No value')}")
-    while ("gunicorn" not in os.environ.get("SERVER_SOFTWARE", "")):
-        logger.error(f"hey guys I am on a loop")
-        time.sleep(5)
-    logger.error(f"Server software is {os.environ.get('SERVER_SOFTWARE', 'No value')}")
     port = int(os.environ.get("PORT", 5000))
     application.run(port=port, threaded=True)
