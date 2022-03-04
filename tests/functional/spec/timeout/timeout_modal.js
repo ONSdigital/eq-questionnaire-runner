5eb3517854a08ff51970f23c36213d03315a9a92
Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>
2022-03-04 16:51:06 +0000

    Update Assert statement
    Signed-off-by: Ashish Parimoo <ashish.parimoo@ext.ons.gov.uk>

--- tests/functional/spec/timeout/timeout_modal.js
+++ tests/functional/spec/timeout/timeout_modal.js
@@ -39,7 +39,7 @@ class TestCase {
 
   checkTimeoutModal() {
     $(TimeoutModalPage.timer()).waitForDisplayed({ timeout: 70000 });
-    expect($(TimeoutModalPage.timer()).getText()).to.equal("To protect your information, your progress will be saved and you will be signed out in 1 minute.");
+    expect($(TimeoutModalPage.timer()).getText()).to.equal("To protect your information, your progress will be saved and you will be signed out in 59 seconds.");
   }
 }
 
