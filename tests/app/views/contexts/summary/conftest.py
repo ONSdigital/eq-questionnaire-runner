2d75e95e3822b3c39a28ea4625a35eadda1939e4
James Nunn <james.nunn@ext.ons.gov.uk>
2022-03-09 11:25:04 +0000

    Migrate tests/app/views/contexts/summary to pytest

---
+++ tests/app/views/contexts/summary/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+@pytest.fixture
+def dynamic_answer_options_schema():
+    return {
+        "dynamic_options": {
+            "values": {
+                "map": [
+                    {"format-date": ["self", "yyyy-MM-dd"]},
+                    {
+                        "date-range": [
+                            {
+                                "date": [
+                                    {
+                                        "source": "response_metadata",
+                                        "identifier": "started_at",
+                                    },
+                                    {"day_of_week": "MONDAY"},
+                                ]
+                            },
+                            3,
+                        ]
+                    },
+                ]
+            },
+            "transform": {"format-date": [{"date": ["self"]}, "EEEE d MMMM yyyy"]},
+        },
+    }
+
+
+@pytest.fixture
+def schema(mocker):
+    return mocker.MagicMock()
--- tests/app/views/contexts/summary/test_answer.py
+++ tests/app/views/contexts/summary/test_answer.py
@@ -1,45 +1,48 @@
+import pytest
+
 from app.views.contexts.summary.answer import Answer
-from tests.app.app_context_test_case import AppContextTestCase
 
 
-class TestAnswer(AppContextTestCase):
-    def test_create_answer(self):
-        answer_schema = {"id": "answer-id", "label": "Answer Label", "type": "date"}
-        user_answer = "An answer"
+@pytest.mark.usefixtures("app")
+def test_create_answer():
+    answer_schema = {"id": "answer-id", "label": "Answer Label", "type": "date"}
+    user_answer = "An answer"
 
-        answer = Answer(
-            answer_schema=answer_schema,
-            answer_value=user_answer,
-            block_id="house-type",
-            list_name="answer-list",
-            list_item_id="answer-item-id",
-            return_to="section-summary",
-        )
+    answer = Answer(
+        answer_schema=answer_schema,
+        answer_value=user_answer,
+        block_id="house-type",
+        list_name="answer-list",
+        list_item_id="answer-item-id",
+        return_to="section-summary",
+    )
 
-        self.assertEqual(answer.id, "answer-id")
-        self.assertEqual(answer.label, "Answer Label")
-        self.assertEqual(answer.value, "An answer")
-        self.assertEqual(answer.type, "date")
+    assert answer.id == "answer-id"
+    assert answer.label == "Answer Label"
+    assert answer.value == "An answer"
+    assert answer.type == "date"
 
-        self.assertEqual(
-            answer.link,
-            "http://test.localdomain/questionnaire/answer-list/answer-item-id/house-type/?return_to=section-summary&return_to_answer_id=answer-id#answer-id",
-        )
+    assert (
+        answer.link
+        == "/questionnaire/answer-list/answer-item-id/house-type/?return_to=section-summary&return_to_answer_id=answer-id#answer-id"
+    )
 
-    def test_date_answer_type(self):
-        # Given
-        answer_schema = {"id": "answer-id", "label": "", "type": "date"}
-        user_answer = None
 
-        # When
-        answer = Answer(
-            answer_schema=answer_schema,
-            answer_value=user_answer,
-            block_id="house-type",
-            list_name="answer-list",
-            list_item_id="answer-item-id",
-            return_to="section-summary",
-        )
+@pytest.mark.usefixtures("app")
+def test_date_answer_type():
+    # Given
+    answer_schema = {"id": "answer-id", "label": "", "type": "date"}
+    user_answer = None
 
-        # Then
-        self.assertEqual(answer.type, "date")
+    # When
+    answer = Answer(
+        answer_schema=answer_schema,
+        answer_value=user_answer,
+        block_id="house-type",
+        list_name="answer-list",
+        list_item_id="answer-item-id",
+        return_to="section-summary",
+    )
+
+    # Then
+    assert answer.type == "date"
