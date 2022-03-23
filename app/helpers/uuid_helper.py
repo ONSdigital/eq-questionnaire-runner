bc7b70375fec18cc7291543383d598e1121a5d64
Mebin Abraham <mebin95+work@gmail.com>
2022-03-23 12:35:42 +0000

    Add module level constant for default UUID

--- app/helpers/uuid_helper.py
+++ app/helpers/uuid_helper.py
@@ -1,7 +1,9 @@
 from uuid import UUID
 
+DEFAULT_UUID_VERSION = 4
 
-def is_valid_uuid(uuid_string: str, version: int = 4) -> bool:
+
+def is_valid_uuid(uuid_string: str, version: int = DEFAULT_UUID_VERSION) -> bool:
     try:
         UUID(uuid_string, version=version)
     except ValueError:
