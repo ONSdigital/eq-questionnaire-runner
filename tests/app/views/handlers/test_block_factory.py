d3c2fa74bd5e3fdb5051e30129cb0c4fbf7c4c5f
James Nunn <james.nunn@ext.ons.gov.uk>
2022-03-25 15:36:20 +0000

    Migrate tests/app/views/handlers to pytest

--- tests/app/views/handlers/test_block_factory.py
+++ tests/app/views/handlers/test_block_factory.py
@@ -1,66 +1,48 @@
-from unittest import TestCase
-
-from mock import Mock
+import pytest
 
 from app.questionnaire.location import InvalidLocationException
 from app.views.handlers.block_factory import get_block_handler
 
 
-class TestBlockFactory(TestCase):
-    def test_get_handler_invalid_block(self):
-        schema = Mock()
-        schema.get_block = Mock(return_value=None)
-        with self.assertRaises(InvalidLocationException):
-            get_block_handler(
-                schema=schema,
-                block_id="invalid-block-id",
-                list_item_id=None,
-                list_name="",
-                questionnaire_store=None,
-                language=None,
-            )
+def test_get_handler_invalid_block(mocker):
+    schema = mocker.Mock()
+    schema.get_block = mocker.Mock(return_value=None)
+    with pytest.raises(InvalidLocationException):
+        get_block_handler(
+            schema=schema,
+            block_id="invalid-block-id",
+            list_item_id=None,
+            list_name="",
+            questionnaire_store=None,
+            language=None,
+        )
 
-    def test_get_handler_invalid_block_type(self):
-        schema = Mock()
-        schema.get_block = Mock(return_value={"id": "some-block", "type": "MadeUpType"})
-        schema.is_block_in_repeating_section = Mock(return_value=False)
 
-        with self.assertRaises(ValueError):
-            get_block_handler(
-                schema=schema,
-                block_id="some-block",
-                list_item_id=None,
-                list_name="",
-                questionnaire_store=None,
-                language=None,
-            )
+@pytest.mark.parametrize(
+    "block_type, is_block_in_repeating_section, list_name, exc",
+    (
+        ("MadeUpType", False, "", ValueError),
+        ("Question", True, "", InvalidLocationException),
+        ("Question", True, "people", InvalidLocationException),
+    ),
+)
+def test_get_handler_invalid_block_raises_ValueError(
+    block_type, is_block_in_repeating_section, list_name, exc, mocker
+):
+    schema = mocker.Mock()
+    schema.get_block = mocker.Mock(
+        return_value={"id": "some-block", "type": block_type}
+    )
+    schema.is_block_in_repeating_section = mocker.Mock(
+        return_value=is_block_in_repeating_section
+    )
 
-    def test_get_handler_invalid_location_missing_list_name_for_repeat(self):
-        schema = Mock()
-        schema.get_block = Mock(return_value={"id": "some-block", "type": "Question"})
-        schema.is_block_in_repeating_section = Mock(return_value=True)
-
-        with self.assertRaises(InvalidLocationException):
-            get_block_handler(
-                schema=schema,
-                block_id="some-block",
-                list_item_id=None,
-                list_name="",
-                questionnaire_store=None,
-                language=None,
-            )
-
-    def test_get_handler_invalid_location_missing_list_item_id_for_repeat(self):
-        schema = Mock()
-        schema.get_block = Mock(return_value={"id": "some-block", "type": "Question"})
-        schema.is_block_in_repeating_section = Mock(return_value=True)
-
-        with self.assertRaises(InvalidLocationException):
-            get_block_handler(
-                schema=schema,
-                block_id="some-block",
-                list_item_id=None,
-                list_name="people",
-                questionnaire_store=None,
-                language=None,
-            )
+    with pytest.raises(exc):
+        get_block_handler(
+            schema=schema,
+            block_id="some-block",
+            list_item_id=None,
+            list_name=list_name,
+            questionnaire_store=None,
+            language=None,
+        )
