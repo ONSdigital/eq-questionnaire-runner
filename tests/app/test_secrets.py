860305b89de1090ef4dd7533f621720589f296e7
James Nunn <james.nunn@ext.ons.gov.uk>
2022-03-29 09:29:47 +0100

    Migrate tests/app to pytest

--- tests/app/test_secrets.py
+++ tests/app/test_secrets.py
@@ -1,33 +1,30 @@
-from unittest import TestCase
+import pytest
 
 from app.secrets import REQUIRED_SECRETS, validate_required_secrets
 
 
-class TestSecrets(TestCase):
-    def test_validate_required_secrets_fails_on_missing(self):
-        secrets = {"secrets": {}}
+def test_validate_required_secrets_fails_on_missing():
+    secrets = {"secrets": {}}
 
-        with self.assertRaises(Exception) as exception:
-            validate_required_secrets(secrets)
+    with pytest.raises(Exception) as exception:
+        validate_required_secrets(secrets)
 
-        self.assertIn(
-            "Missing Secret [EQ_SERVER_SIDE_STORAGE_USER_ID_SALT]",
-            str(exception.exception),
+        assert "Missing Secret [EQ_SERVER_SIDE_STORAGE_USER_ID_SALT]" in str(
+            exception.exception
         )
 
-    def test_validate_required_secrets_passes(self):
-        secrets = {"secrets": {secret: "abc" for secret in REQUIRED_SECRETS}}
-        self.assertIsNone(validate_required_secrets(secrets))
 
-    def test_validate_required_secrets_fails_on_conditional_secret(self):
-        secrets = {"secrets": {secret: "abc" for secret in REQUIRED_SECRETS}}
+def test_validate_required_secrets_passes():
+    secrets = {"secrets": {secret: "abc" for secret in REQUIRED_SECRETS}}
+    assert validate_required_secrets(secrets) is None
 
-        with self.assertRaises(Exception) as exception:
-            validate_required_secrets(
-                secrets, additional_required_secrets=["MY_REQUIRED_SECRET"]
-            )
 
-            self.assertIn(
-                "Missing Secret [MY_REQUIRED_SECRET]",
-                str(exception.exception),
-            )
+def test_validate_required_secrets_fails_on_conditional_secret():
+    secrets = {"secrets": {secret: "abc" for secret in REQUIRED_SECRETS}}
+
+    with pytest.raises(Exception) as exception:
+        validate_required_secrets(
+            secrets, additional_required_secrets=["MY_REQUIRED_SECRET"]
+        )
+
+        assert "Missing Secret [MY_REQUIRED_SECRET]" in str(exception.exception)
