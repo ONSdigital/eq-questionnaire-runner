a73859b221e7e24b4a451c455a67c3cf13ebbe41
petechd <piotr.chudziak@ons.gov.uk>
2022-03-31 13:14:48 +0100

    Fix comparison

--- app/routes/session.py
+++ app/routes/session.py
@@ -132,7 +132,7 @@ def get_sign_out():
     """
     Signs the user out of eQ and redirects to the log out url.
     """
-    todo = request.args.get("todo", False) == True
+    todo = request.args.get("todo", "") == "True"
     survey_config = get_survey_config()
     if todo:
         log_out_url = (
