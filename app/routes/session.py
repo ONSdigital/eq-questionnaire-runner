bdc5d64901771933056d6b9aae1b643927fadd0d
petechd <piotr.chudziak@ons.gov.uk>
2022-03-31 12:36:58 +0100

    Remove case sensitivity

--- app/routes/session.py
+++ app/routes/session.py
@@ -132,7 +132,7 @@ def get_sign_out():
     """
     Signs the user out of eQ and redirects to the log out url.
     """
-    todo = request.args.get("todo", "FALSE").upper() == "TRUE"
+    todo = request.args.get("todo", False) == True
     survey_config = get_survey_config()
     if todo:
         log_out_url = (
