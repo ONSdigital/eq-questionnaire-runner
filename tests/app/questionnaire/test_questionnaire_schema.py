467ee2806f439b6bfffdd384bb7cd08b123e61a9
Rhys Berrow <47635349+berroar@users.noreply.github.com>
2022-06-22 12:11:51 +0100

    Add calculated summary section dependencies (#868)

--- tests/app/questionnaire/test_questionnaire_schema.py
+++ tests/app/questionnaire/test_questionnaire_schema.py
@@ -705,3 +705,16 @@ def test_when_rules_section_dependencies_by_answer(
             "primary-person",
         },
     } == schema.when_rules_section_dependencies_by_answer
+
+
+def test_when_rules_section_dependencies_calculated_summary(
+    section_dependencies_calculated_summary_schema,
+):
+    schema = section_dependencies_calculated_summary_schema
+
+    assert {
+        "milk-answer": {"dependent-enabled-section", "dependent-question-section"},
+        "eggs-answer": {"dependent-enabled-section", "dependent-question-section"},
+        "bread-answer": {"dependent-enabled-section", "dependent-question-section"},
+        "cheese-answer": {"dependent-enabled-section", "dependent-question-section"},
+    } == schema.when_rules_section_dependencies_by_answer
