f5de443e979a0fd006e74943e8d1674afc4ce26d
petechd <piotr.chudziak@ons.gov.uk>
2022-06-28 10:26:57 +0100

    Sort imports

--- tests/app/forms/test_questionnaire_form.py
+++ tests/app/forms/test_questionnaire_form.py
@@ -14,7 +14,7 @@ from app.forms.validators import (
     ResponseRequired,
     format_message_with_title,
 )
-from app.questionnaire import QuestionnaireSchema, Location
+from app.questionnaire import Location, QuestionnaireSchema
 from app.questionnaire.placeholder_renderer import PlaceholderRenderer
 from app.utilities.schema import load_schema_from_name
 
