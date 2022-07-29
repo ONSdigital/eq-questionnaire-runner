880c4782b0e51604e94c42c89379ae441f879a74
Rhys Berrow <rhys.berrow@ons.gov.uk>
2022-07-29 10:14:15 +0100

    Remive type hint

--- app/routes/questionnaire.py
+++ app/routes/questionnaire.py
@@ -122,7 +122,6 @@ def before_post_submission_request():
 
     handle_language(metadata)
 
-    # pylint: disable=assigning-non-slot
     g.schema = load_schema_from_metadata(
         metadata=metadata, language_code=get_locale().language
     )
