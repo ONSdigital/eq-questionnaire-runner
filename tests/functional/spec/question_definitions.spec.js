be80dd79d56d051bd14f5ec8e698520a8e321536
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-08-18 12:54:00 +0100

    fix test

--- tests/functional/spec/question_definitions.spec.js
+++ tests/functional/spec/question_definitions.spec.js
@@ -7,24 +7,24 @@ describe("Component: Definition", () => {
     });
 
     it("When I click the title link, then the description should be visible", () => {
-      expect($(DefinitionPage.definitionContent(1)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionContent(1)).getText()).to.contain("");
 
       // When
       $(DefinitionPage.definitionTitle("1")).click();
 
       // Then
-      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300 });
+      expect($(DefinitionPage.definitionContent(1)).getText()).to.contain("A typical photovoltaic system employs solar panels, each comprising a number of solar cells, which generate electrical power.");
     });
 
     it("When I click the title link twice, then the description should not be visible", () => {
-      expect($(DefinitionPage.definitionContent(1)).isDisplayed()).to.be.false;
+      expect($(DefinitionPage.definitionContent(1)).getText()).to.contain("");
 
       // When
       $(DefinitionPage.definitionTitle("1")).click();
       $(DefinitionPage.definitionTitle("1")).click();
 
       // Then
-      $(DefinitionPage.definitionContent(1)).waitForDisplayed({ timeout: 300, reverse: true });
+      expect($(DefinitionPage.definitionContent(1)).getText()).to.contain("");
     });
   });
 });
