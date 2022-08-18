8530f333f4a945dfc8c76c7d4957747a54e13aae
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-08-18 10:43:19 +0100

    run format

--- tests/integration/questionnaire/test_questionnaire_section_summary.py
+++ tests/integration/questionnaire/test_questionnaire_section_summary.py
@@ -2,14 +2,15 @@ from tests.integration.integration_test_case import IntegrationTestCase
 
 
 class TestQuestionnaireCalculatedSummary(IntegrationTestCase):
-
     def test_calculated_summary_headers_with_change_link(self):
         self.launchSurvey("test_section_summary")
         self.post()
         self.post()
         self.post()
         self.post()
-        self.assertInBody("<tr><th>Question</th><th>Answer given</th><th>Change answer</th></tr>")
+        self.assertInBody(
+            "<tr><th>Question</th><th>Answer given</th><th>Change answer</th></tr>"
+        )
 
     def test_calculated_summary_headers_without_change_link(self):
         self.launchSurvey("test_view_submitted_response")
