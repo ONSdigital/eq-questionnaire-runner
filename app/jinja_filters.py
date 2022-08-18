4e910a18a1220c7b5c14815a64a795249476923f
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-08-18 15:56:57 +0100

    change summary config to resolve dac row title issue

--- app/jinja_filters.py
+++ app/jinja_filters.py
@@ -541,7 +541,6 @@ class SummaryRow:
         edit_link_text: str,
         edit_link_aria_label: str,
     ) -> None:
-        self.rowTitle = strip_tags(question["title"])
         self.id = question["id"]
         self.rowItems = []
 
