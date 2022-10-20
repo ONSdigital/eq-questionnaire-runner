215d5e1053058800ccbbe663ae64aca3209b516d
Rhys Berrow <rhys.berrow@ons.gov.uk>
2022-10-20 11:08:20 +0100

    Remove metadata from method

--- app/views/handlers/content.py
+++ app/views/handlers/content.py
@@ -29,7 +29,6 @@ class Content(BlockHandler):
     def get_context(self):
         return {
             "block": self.rendered_block,
-            "metadata": self._questionnaire_store.metadata,
             "individual_response_url": individual_response_url(
                 self._schema.get_individual_response_list(),
                 self._current_location.list_item_id,
