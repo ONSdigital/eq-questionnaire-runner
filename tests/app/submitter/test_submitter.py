a6322764dc7ea590b02af49b8ab4912604c96284
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2022-11-11 11:43:46 +0000

    updated test_double_submission

--- tests/app/submitter/test_submitter.py
+++ tests/app/submitter/test_submitter.py
@@ -1,9 +1,8 @@
 import uuid
 
 import pytest
-from google.api_core.exceptions import Forbidden
 from pika.exceptions import AMQPError, NackError
-
+import logging
 from app.submitter import GCSFeedbackSubmitter, GCSSubmitter, RabbitMQSubmitter
 from app.utilities.json import json_dumps
 
@@ -298,9 +297,10 @@ def test_gcs_feedback_submitter_uploads_feedback(patch_gcs_client):
     assert feedback_upload is True
 
 
-def test_double_submission():
+def test_double_submission(caplog):
     gcs_submitter = GCSSubmitter(bucket_name="test_bucket")
 
-    with pytest.raises(Forbidden):
+    with caplog.at_level(logging.INFO):
         gcs_submitter.send_message(message={"test_data"}, tx_id="123", case_id="456")
-        gcs_submitter.send_message(message={"test_data"}, tx_id="123", case_id="457")
+        gcs_submitter.send_message(message={"other_test_data"}, tx_id="123", case_id="456")
+    assert "Questionnaire submission exists, ignoring delete operation error" in caplog.text
