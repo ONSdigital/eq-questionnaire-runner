cbce71b1427d9245c5152f9f9c38d57233369ca6
petechd <piotr.chudziak@ons.gov.uk>
2022-12-06 19:09:27 +0000

    Add multiple list collector elements test

--- tests/functional/spec/list_collector_section_summary_items.spec.js
+++ tests/functional/spec/list_collector_section_summary_items.spec.js
@@ -12,7 +12,7 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I get to the section summary, Then the driving question should be visible.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       expect(browser.getUrl()).to.contain(SectionSummaryPage.url());
@@ -21,20 +21,39 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I add my own item, Then the item should be visible on the section summary and have correct values", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       expect($(SectionSummaryPage.companiesListLabel(1)).getText()).to.contain("Name of UK company or branch");
-      expect($(companiesListRowItem(1)).getText()).to.contain("Company A");
-      expect($(companiesListRowItem(2)).getText()).to.contain("123,456,789");
-      expect($(companiesListRowItem(3)).getText()).to.contain("Yes");
+      expect($(companiesListRowItem(1, 1)).getText()).to.contain("Company A");
+      expect($(companiesListRowItem(1, 2)).getText()).to.contain("123");
+      expect($(companiesListRowItem(1, 3)).getText()).to.contain("Yes");
       expect($(companiesListRowItemAnchor(1)).getHTML()).to.contain("return_to=section-summary#company-or-branch-name");
       expect($(companiesListRowItemAnchor(2)).getHTML()).to.contain("return_to_answer_id=registration-number#registration-number");
       expect($(companiesListRowItemAnchor(3)).getHTML()).to.contain("return_to_answer_id=authorised-insurer-radio#authorised-insurer-radio");
     });
+    it("When I add multiple items, Then all the items should be visible on the section summary and have correct values", () => {
+      drivingQuestionYes();
+      addCompany("Company A", "123", true);
+      anyMoreCompaniesYes();
+      addCompany("Company B", "456", false);
+      anyMoreCompaniesYes();
+      addCompany("Company C", "789", true);
+      anyMoreCompaniesNo();
+      $(EstimatePage.submit()).click();
+      expect($(companiesListRowItem(1, 1)).getText()).to.contain("Company A");
+      expect($(companiesListRowItem(1, 2)).getText()).to.contain("123");
+      expect($(companiesListRowItem(1, 3)).getText()).to.contain("Yes");
+      expect($(companiesListRowItem(2, 1)).getText()).to.contain("Company B");
+      expect($(companiesListRowItem(2, 2)).getText()).to.contain("456");
+      expect($(companiesListRowItem(2, 3)).getText()).to.contain("No");
+      expect($(companiesListRowItem(3, 1)).getText()).to.contain("Company C");
+      expect($(companiesListRowItem(3, 2)).getText()).to.contain("789");
+      expect($(companiesListRowItem(3, 3)).getText()).to.contain("Yes");
+    });
     it("When I add my own item, Then I should be able to remove that item from the section summary page.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       $(SectionSummaryPage.companiesListRemoveLink(1)).click();
@@ -42,7 +61,7 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I add my own item and make it not on the path, Then the list of answers should not be visible on the section summary.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       removeFirstCompany();
@@ -52,7 +71,7 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I remove my own item but list collector is still on the path, Then the placeholder text should be visible on the section summary.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       removeFirstCompany();
@@ -61,14 +80,14 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I add my own item and relevant data, Then after I answer no on additional items page I should get to the section summary page.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       expect(browser.getUrl()).to.contain(SectionSummaryPage.url());
     });
     it("When I add my own item and relevant data, Then after I answer yes on additional items page I should be able to choose an item from the items list and add relevant data about it.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesYes();
       expect($(AnyCompaniesOrBranchesAddPage.companyOrBranchName()).isExisting()).to.be.true;
       expect($(AnyCompaniesOrBranchesAddPage.registrationNumber()).isExisting()).to.be.true;
@@ -79,7 +98,7 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I add my own item and relevant data, Then I should be able to edit that item from the section summary page.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       $(SectionSummaryPage.companiesListEditLink(1)).click();
@@ -94,7 +113,7 @@ describe("List Collector Section Summary Items", () => {
       expect($(SectionSummaryPage.companiesListAddLink()).isExisting()).to.be.false;
       $(SectionSummaryPage.anyCompaniesOrBranchesAnswerEdit()).click();
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       expect(browser.getUrl()).to.contain(SectionSummaryPage.url());
       expect($(SectionSummaryPage.companiesListEditLink(1)).isExisting()).to.be.true;
@@ -103,7 +122,7 @@ describe("List Collector Section Summary Items", () => {
     });
     it("When I decide to add an item and relevant data and I decide to remove it, Then I should be able to see the item again after I decide to add more items.", () => {
       drivingQuestionYes();
-      addCompany("Company A", "0123456789", true);
+      addCompany("Company A", "123", true);
       anyMoreCompaniesNo();
       $(EstimatePage.submit()).click();
       $(SectionSummaryPage.anyCompaniesOrBranchesAnswerEdit()).click();
@@ -157,8 +176,8 @@ function removeFirstCompany() {
   $(AnyCompaniesOrBranchesRemovePage.submit()).click();
 }
 
-function companiesListRowItem(index) {
-  return `#group-companies-1 .ons-summary__items .ons-summary__item .ons-summary__row:nth-of-type(${index})`;
+function companiesListRowItem(row, index) {
+  return `#group-companies-1 .ons-summary__items .ons-summary__item:nth-of-type(${row}) .ons-summary__row:nth-of-type(${index})`;
 }
 
 function companiesListRowItemAnchor(index) {
