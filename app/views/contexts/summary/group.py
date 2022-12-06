349d33b133fb35b1fde552b5c48362f0beebdef0
petechd <piotr.chudziak@ons.gov.uk>
2022-12-06 13:12:09 +0000

    Add method to get summary item and move list collector conditional below it

--- app/views/contexts/summary/group.py
+++ app/views/contexts/summary/group.py
@@ -50,8 +50,8 @@ class Group:
         )
 
     # pylint: disable=too-many-locals
-    @staticmethod
     def _build_blocks_and_links(
+        self,
         *,
         group_schema,
         routing_path,
@@ -93,42 +93,50 @@ class Group:
                     ]
                 )
 
-            elif block["type"] in ["ListCollector"]:
-                list_collector_block = ListCollectorBlock(
-                    routing_path=routing_path,
-                    answer_store=answer_store,
-                    list_store=list_store,
-                    progress_store=progress_store,
-                    metadata=metadata,
-                    response_metadata=response_metadata,
-                    schema=schema,
-                    location=location,
-                    language=language,
-                )
-
+            else:
                 section = schema.get_section(location.section_id)
-                summary = section.get("summary", {})
-                if summary_items := summary.get("items"):
-                    for summary_item in summary_items:
-                        if (
-                            summary_item["type"] == "List"
-                            and summary_item["for_list"] == block["for_list"]
-                        ):
 
-                            list_summary_element = (
-                                list_collector_block.list_summary_element(summary_item)
-                            )
-                            blocks.extend([list_summary_element])
-                            links["add_link"] = SummaryLink(
-                                text=list_summary_element["add_link_text"],
-                                url=list_summary_element["add_link"],
-                                attributes={"data-qa": "add-item-link"},
-                            )
-                            links["empty_list_text"] = list_summary_element[
-                                "empty_list_text"
-                            ]
+                if summary_item := self.get_summary_item_for_list_for_section(
+                    section, block
+                ):
+                    if block["type"] in ["ListCollector"]:
+                        list_collector_block = ListCollectorBlock(
+                            routing_path=routing_path,
+                            answer_store=answer_store,
+                            list_store=list_store,
+                            progress_store=progress_store,
+                            metadata=metadata,
+                            response_metadata=response_metadata,
+                            schema=schema,
+                            location=location,
+                            language=language,
+                        )
+
+                        list_summary_element = (
+                            list_collector_block.list_summary_element(summary_item)
+                        )
+                        blocks.extend([list_summary_element])
+                        links["add_link"] = SummaryLink(
+                            text=list_summary_element["add_link_text"],
+                            url=list_summary_element["add_link"],
+                            attributes={"data-qa": "add-item-link"},
+                        )
+                        links["empty_list_text"] = list_summary_element[
+                            "empty_list_text"
+                        ]
 
         return blocks, links
+
+    @staticmethod
+    def get_summary_item_for_list_for_section(section, block):
+        summary = section.get("summary", {})
+        if summary_items := summary.get("items"):
+            for summary_item in summary_items:
+                if (
+                    summary_item["type"] == "List"
+                    and summary_item["for_list"] == block["for_list"]
+                ):
+                    return summary_item
 
     def serialize(self):
         return self.placeholder_renderer.render(
