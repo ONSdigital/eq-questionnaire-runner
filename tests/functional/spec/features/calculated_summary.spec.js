36f8ec7f70b3a15577e3a373f860669e41648365
Rhys Berrow <rhys.berrow@ons.gov.uk>
2022-12-08 15:49:46 +0000

    Update functional test

--- tests/functional/spec/features/calculated_summary.spec.js
+++ tests/functional/spec/features/calculated_summary.spec.js
@@ -3,6 +3,8 @@ import { CalculatedSummaryTestCase } from "./calculated_summary_test_case.js";
 describe("Feature: Calculated Summary", () => {
   describe("Given I have a Calculated Summary", () => {
     CalculatedSummaryTestCase.testCase("test_calculated_summary.json");
+  });
+  describe("Given I have a Calculated Summary with the new format", () => {
     CalculatedSummaryTestCase.testCase("test_new_calculated_summary.json");
   });
 });
