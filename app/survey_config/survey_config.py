c1786b713430eb2be9261a1ea80271ea8dd2c93e
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-12-13 11:40:11 +0000

    fix linting

--- app/survey_config/survey_config.py
+++ app/survey_config/survey_config.py
@@ -72,6 +72,6 @@ class SurveyConfig:
         return None
 
     def get_additional_data_layer_context(  # pylint: disable=no-self-use
-        self=None,
+        self,
     ) -> list[dict]:
         return []
