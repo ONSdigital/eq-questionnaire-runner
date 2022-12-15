9c6119a614276e99048b525eac10ca08be58ae61
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2022-12-15 16:31:24 +0000

    Adding a check for cookie session

--- app/helpers/template_helpers.py
+++ app/helpers/template_helpers.py
@@ -38,9 +38,7 @@ class ContextHelper:
         self._is_post_submission = is_post_submission
         self._include_csrf_token = include_csrf_token
         self._survey_config = survey_config
-        self._survey_title = cookie_session.get(
-            "survey_title", self._survey_config.survey_title
-        )
+        self._survey_title = get_survey_title(self)
         self._sign_out_url = url_for("session.get_sign_out")
         self._cdn_url = (
             f'{current_app.config["CDN_URL"]}{current_app.config["CDN_ASSETS_PATH"]}'
@@ -151,6 +149,11 @@ class ContextHelper:
             ).format(sign_out_url=self._sign_out_url)
 
             return footer_warning
+    
+def get_survey_title(self):
+    if cookie_session.get("survey_title", self._survey_config.survey_title):
+        return cookie_session.get("survey_title", self._survey_config.survey_title) 
+    return "ONS SOCIAL"
 
 
 @lru_cache
@@ -236,3 +239,6 @@ def render_template(template: str, **kwargs: Union[str, Mapping]) -> str:
 def get_survey_type() -> SurveyType:
     survey_type = cookie_session.get("theme", current_app.config["SURVEY_TYPE"])
     return SurveyType(survey_type)
+
+
+
