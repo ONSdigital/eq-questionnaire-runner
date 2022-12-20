5b1785a4c0073857f04df6667bbffb5b9514d813
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2022-12-20 13:55:04 +0000

    removed pylint ignore from launchsurvey
    Signed-off-by: Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>

--- tests/integration/session/test_multiple_login.py
+++ tests/integration/session/test_multiple_login.py
@@ -13,7 +13,6 @@ class MultipleClientTestCase(IntegrationTestCase):
 
         self.cache = {}
 
-    # pylint: disable=arguments-renamed
     def launchSurvey(self, schema_name, client, **payload_kwargs):
         token = self.token_generator.create_token(schema_name, **payload_kwargs)
         self.get(client, "/session?token=" + token)
