ad37f929fd693eeb60eafd695e0eecbfc7f67d3b
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-01-17 09:21:50 +0000

    fix test

--- tests/functional/spec/theme_beis_ni.spec.js
+++ tests/functional/spec/theme_beis_ni.spec.js
@@ -8,7 +8,7 @@ describe("Theme BEIS-NI", () => {
 
     it("When I navigate to the radio page, Then I should see BEIS-NI theme content", () => {
       expect(browser.getUrl()).to.contain(RadioPage.pageName);
-      expect($("#beis-logo-alt").getHTML()).to.contain("Department for Business, Energy and Industrial Strategy");
+      expect($("#beis-logo-mobile-alt").getHTML()).to.contain("Department for Business, Energy and Industrial Strategy");
       expect($("#finance-ni-logo-alt").getHTML()).to.contain("Northern Ireland Department of Finance logo");
     });
   });
