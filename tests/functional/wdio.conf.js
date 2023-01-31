d38b138b9ddc1d6d62bef21620adbb673aebcd35
petechd <piotr.chudziak@ons.gov.uk>
2023-01-31 08:31:10 +0000

    Split timeout modal tests

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -18,7 +18,8 @@ exports.config = {
   //
   specs: ["./tests/functional/spec/**/*.js"],
   suites: {
-    timeout_modal: ["./tests/functional/spec/timeout/*.spec.js"],
+    timeout_modal: ["./tests/functional/spec/timeout/timeout_modal.spec.js"],
+    timeout_modal_post_submission: ["./tests/functional/spec/timeout/timeout_modal_post_submission.spec.js"],
     components: ["./tests/functional/spec/components/**/*.js"],
     features: ["./tests/functional/spec/features/**/*.js"],
     general: ["./tests/functional/spec/*.spec.js"],
