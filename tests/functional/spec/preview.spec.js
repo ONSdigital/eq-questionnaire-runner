23c6785fe24c5422f51dcd771ee7a95ce803dbe4
petechd <piotr.chudziak@ons.gov.uk>
2023-02-20 09:59:05 +0000

    Split functional test

--- tests/functional/spec/preview.spec.js
+++ tests/functional/spec/preview.spec.js
@@ -9,13 +9,19 @@ describe("Introduction preview questions", () => {
     browser.openQuestionnaire(introductionSchema);
   });
 
-  it("Given I start a survey, When I view the preview page of hub flow schema, Then I should be able to use the twisty button", () => {
+  it("Given I start a survey, When I view the preview page of hub flow schema, Then the twisty button should read 'Show all' and answers should be invisible", () => {
     browser.openQuestionnaire(introductionSchema);
     $(HubPage.submit()).click();
     $(IntroductionPage.previewQuestions()).click();
     expect(browser.getUrl()).to.contain("questionnaire/preview");
     expect($(showButton).getText()).to.equal("Show all");
     expect($(summaryContent).isClickable()).to.be.false;
+  });
+  it("Given I start a survey, When I view the preview page and click the twisty button, Then the twisty button should read 'Hide all' and the answers should be visible", () => {
+    browser.openQuestionnaire(introductionSchema);
+    $(HubPage.submit()).click();
+    $(IntroductionPage.previewQuestions()).click();
+    expect(browser.getUrl()).to.contain("questionnaire/preview");
     $(showButton).click();
     expect($(showButton).getText()).to.equal("Hide all");
     expect($(summaryContent).isClickable()).to.be.true;
