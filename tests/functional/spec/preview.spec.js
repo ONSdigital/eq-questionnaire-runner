f58be8c2c76b7b384c061eedda13c1f5e90a9d83
petechd <piotr.chudziak@ons.gov.uk>
2023-02-24 14:42:22 +0000

    Add validation changes and fix functional after schema change

--- tests/functional/spec/preview.spec.js
+++ tests/functional/spec/preview.spec.js
@@ -1,5 +1,4 @@
 import IntroductionPage from "../generated_pages/introduction_preview_hub/introduction.page";
-import HubPage from "../base_pages/hub.page.js";
 
 describe("Introduction preview questions", () => {
   const introductionSchema = "test_introduction_preview_hub.json";
@@ -11,7 +10,6 @@ describe("Introduction preview questions", () => {
 
   it("Given I start a survey, When I view the preview page of hub flow schema, Then the twisty button should read 'Show all' and answers should be invisible", () => {
     browser.openQuestionnaire(introductionSchema);
-    $(HubPage.submit()).click();
     $(IntroductionPage.previewQuestions()).click();
     expect(browser.getUrl()).to.contain("questionnaire/preview");
     expect($(showButton).getText()).to.equal("Show all");
@@ -19,7 +17,6 @@ describe("Introduction preview questions", () => {
   });
   it("Given I start a survey, When I view the preview page and click the twisty button, Then the twisty button should read 'Hide all' and the answers should be visible", () => {
     browser.openQuestionnaire(introductionSchema);
-    $(HubPage.submit()).click();
     $(IntroductionPage.previewQuestions()).click();
     expect(browser.getUrl()).to.contain("questionnaire/preview");
     $(showButton).click();
