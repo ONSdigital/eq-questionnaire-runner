7b157b9268ead50c78bb90a2eb61e57c1c8d910c
petechd <53475968+petechd@users.noreply.github.com>
2023-02-28 14:55:42 +0000

    Update tests/functional/spec/preview.spec.js
    Co-authored-by: Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>

--- tests/functional/spec/preview.spec.js
+++ tests/functional/spec/preview.spec.js
@@ -14,7 +14,7 @@ describe("Introduction preview questions", () => {
     $(IntroductionPageLinear.previewQuestions()).click();
     expect(browser.getUrl()).to.contain("questionnaire/preview");
     expect($(previewSectionTitle).getText()).to.equal("Main section");
-    // TODO: Add data attributes to elements below so we don't rely on tags or classes that are subject to DS changes
+    // :TODO: Add data attributes to elements below so we don't rely on tags or classes that are subject to DS changes
     expect($(previewQuestion).$("h3").getText()).to.equal("Are you able to report for the calendar month 1 January 2017 to 1 February 2017?");
     expect($(previewQuestion).$(".ons-question__description").getText()).to.equal("Your return should relate to the calendar year 2021.");
     expect($(previewQuestion).$$(".ons-panel__body")[0].getText()).to.equal("Please provide figures for the period in which you were trading.");
