7a71d1f4aeafc82aabbb35d996929ca924a7b754
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-03-06 13:16:44 +0000

    fix tests

--- tests/functional/spec/preview.spec.js
+++ tests/functional/spec/preview.spec.js
@@ -16,11 +16,12 @@ describe("Introduction preview questions", () => {
     expect($(previewSectionTitle).getText()).to.equal("Main section");
     // :TODO: Add data attributes to elements below so we don't rely on tags or classes that are subject to DS changes
     expect($(previewQuestion).$("h3").getText()).to.equal("Are you able to report for the calendar month 1 January 2017 to 1 February 2017?");
-    expect($(previewQuestion).$(".ons-question__description").getText()).to.equal("Your return should relate to the calendar year 2021.");
+    expect($(previewQuestion).$(".ons-question__description").getText()).to.equal("Your return should relate to the calendar year 2016.");
     expect($(previewQuestion).$$(".ons-panel__body")[0].getText()).to.equal("Please provide figures for the period in which you were trading.");
-    expect($(previewQuestion).$$(".ons-panel__body")[1].getText()).to.equal("You can only pick one answer");
-    expect($(previewQuestion).$$("p")[3].getText()).to.equal("You can answer with one of the following options:");
-    expect($(previewQuestion).$$("p")[4].getText()).to.equal("Select your answer");
+    $(".ons-details__heading").click();
+    expect($(previewQuestion).$("#answer-guidance--content div p").getText()).to.equal("For example select `yes` if you can report for this period");
+    expect($(previewQuestion).$$("p")[2].getText()).to.equal("You can answer with one of the following options:");
+    expect($(previewQuestion).$$("p")[3].getText()).to.equal("Select your answer");
     expect($(previewQuestion).$$("ul")[0].getText()).to.equal("Yes\nNo");
   });
   it("Given I start a survey, When I view the preview page of hub flow schema, Then the twisty button should read 'Show all' and answers should be invisible", () => {
@@ -29,13 +30,10 @@ describe("Introduction preview questions", () => {
     expect(browser.getUrl()).to.contain("questionnaire/preview");
     expect($(showButton).getText()).to.equal("Show all");
     expect($(previewSummaryContent).isClickable()).to.be.false;
-  });
-  it("Given I start a survey, When I view the preview page and click the twisty button, Then the twisty button should read 'Hide all' and the answers should be visible", () => {
-    browser.openQuestionnaire(introductionSchema);
-    $(IntroductionPage.previewQuestions()).click();
-    expect(browser.getUrl()).to.contain("questionnaire/preview");
-    $(showButton).click();
-    expect($(showButton).getText()).to.equal("Hide all");
-    expect($(previewSummaryContent).isClickable()).to.be.true;
+    it("and click the twisty button, Then the twisty button should read 'Hide all' and the answers should be visible", () => {
+      $(showButton).click();
+      expect($(showButton).getText()).to.equal("Hide all");
+      expect($(previewSummaryContent).isClickable()).to.be.true;
+    });
   });
 });
