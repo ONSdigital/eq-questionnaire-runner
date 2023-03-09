8ed0ede82da9ec5ce5bdccd53c2815f82e091725
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-03-09 12:33:29 +0000

    add test

--- tests/integration/routes/test_view_preview_questions.py
+++ tests/integration/routes/test_view_preview_questions.py
@@ -2,7 +2,7 @@ from tests.integration.integration_test_case import IntegrationTestCase
 
 
 class TestPreviewPDF(IntegrationTestCase):
-    def test_download(self):
+    def test_download_pdf(self):
         super().setUp()
 
         # Given I launch a questionnaire and open preview of questions
@@ -19,7 +19,19 @@ class TestPreviewPDF(IntegrationTestCase):
         self.get(download_pdf_url)
 
         # Then I get 200 status code
-        self.assertStatusCode(200)
+        self.assertStatusOK()
+
+    def test_download_pdf_no_preview(self):
+        super().setUp()
+
+        # Given I launch a questionnaire without preview enabled
+        self.launchSurvey("test_checkbox")
+
+        # When I try to download preview of questions
+        self.get("/questionnaire/preview/download-pdf")
+
+        # Then I get 404 status code
+        self.assertStatusNotFound()
 
     def test_print_button(self):
         super().setUp()
