2e7989c4f254326506fde244e3faf6df685d766c
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-03-09 12:47:26 +0000

    change error returned from pdf endpoint

--- app/views/contexts/preview_context.py
+++ app/views/contexts/preview_context.py
@@ -7,6 +7,7 @@ from app.data_models.metadata_proxy import MetadataProxy
 from app.questionnaire import QuestionnaireSchema
 from app.views.contexts import Context
 from app.views.contexts.section_preview_context import SectionPreviewContext
+from werkzeug.exceptions import NotFound
 
 
 class PreviewNotEnabledException(Exception):
@@ -25,7 +26,7 @@ class PreviewContext(Context):
         response_metadata: Mapping[str, Union[str, int, list]],
     ):
         if not schema.preview_enabled:
-            raise PreviewNotEnabledException
+            raise NotFound
 
         super().__init__(
             language,
