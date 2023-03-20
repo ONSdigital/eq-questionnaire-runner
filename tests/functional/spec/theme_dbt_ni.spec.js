6a3fab8f39ec4ba11e845b9903dc2d0dbaa9ab02
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-03-20 16:14:20 +0000

    fix test

--- tests/functional/spec/theme_dbt_ni.spec.js
+++ tests/functional/spec/theme_dbt_ni.spec.js
@@ -8,7 +8,7 @@ describe("Theme DBT-NI", () => {
 
     it("When I navigate to the radio page, Then I should see DBT-NI theme content", async () => {
       await expect(await browser.getUrl()).to.contain(RadioPage.pageName);
-      await expect(await $("#dbt-logo-mobile-alt").getHTML()).to.contain("Department for Business and Trade");
+      await expect(await $("#dbt-logo-alt").getHTML()).to.contain("Department for Business and Trade");
       await expect(await $("#finance-ni-logo-alt").getHTML()).to.contain("Northern Ireland Department of Finance logo");
     });
   });
