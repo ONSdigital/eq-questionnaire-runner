2a33f81dda9a8f46fc99227513083475c5f8c737
rmccar <42928680+rmccar@users.noreply.github.com>
2023-03-20 14:55:35 +0000

    Replace BEIS with DBT-DSIT theme (#1062)

--- app/helpers/template_helpers.py
+++ app/helpers/template_helpers.py
@@ -14,11 +14,11 @@ from app.helpers.language_helper import get_languages_context
 from app.questionnaire import QuestionnaireSchema
 from app.settings import ACCOUNT_SERVICE_BASE_URL
 from app.survey_config import (
-    BEISBusinessSurveyConfig,
-    BEISNIBusinessSurveyConfig,
     BusinessSurveyConfig,
     CensusNISRASurveyConfig,
     CensusSurveyConfig,
+    DBTDSITBusinessSurveyConfig,
+    DBTDSITNIBusinessSurveyConfig,
     NIBusinessSurveyConfig,
     ORRBusinessSurveyConfig,
     SocialSurveyConfig,
@@ -182,8 +182,8 @@ def survey_config_mapping(
         SurveyType.HEALTH: SocialSurveyConfig,
         SurveyType.SOCIAL: SocialSurveyConfig,
         SurveyType.NORTHERN_IRELAND: NIBusinessSurveyConfig,
-        SurveyType.BEIS: BEISBusinessSurveyConfig,
-        SurveyType.BEIS_NI: BEISNIBusinessSurveyConfig,
+        SurveyType.DBT_DSIT: DBTDSITBusinessSurveyConfig,
+        SurveyType.DBT_DSIT_NI: DBTDSITNIBusinessSurveyConfig,
         SurveyType.ORR: ORRBusinessSurveyConfig,
         SurveyType.CENSUS: (
             WelshCensusSurveyConfig if language == "cy" else CensusSurveyConfig
