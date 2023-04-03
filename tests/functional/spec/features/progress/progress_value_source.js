5b0044d86a2596f39f0131cb4c0c376496d91b88
Rhys Berrow <rhys.berrow@ons.gov.uk>
2023-04-03 11:13:06 +0100

    Add progress functional test

---
+++ tests/functional/spec/features/progress/progress_value_source.js
@@ -0,0 +1,71 @@
+import FirstQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b1.page";
+import SecondQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b2.page";
+import ThirdQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b3.page";
+import FourthQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b4.page";
+import FifthQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b5.page";
+import SixthQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b6.page";
+import SeventhQuestionPage from "../../../generated_pages/progress_value_source_blocks/s1-b7.page";
+import SubmitPage from "../../../generated_pages/progress_value_source_blocks/submit.page";
+
+describe("Feature: Routing rules based on progress value sources", () => {
+  beforeEach(async () => {
+    await browser.openQuestionnaire("test_progress_value_source_blocks.json");
+  });
+
+  describe("Given I have routing based on the completeness of a block", () => {
+    it("When the block being evaluated is incomplete (Q2), Then the dependent question (Q4) should not be displayed", async () => {
+      await $(FirstQuestionPage.q1A1()).setValue("0");
+      await $(FirstQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(ThirdQuestionPage.pageName);
+      await $(ThirdQuestionPage.q1A1()).setValue("1");
+      await $(ThirdQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(FifthQuestionPage.pageName);
+      await $(FifthQuestionPage.q1A1()).setValue("2");
+      await $(FifthQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SeventhQuestionPage.pageName);
+      await $(SeventhQuestionPage.q1A1()).setValue("3");
+      await $(SeventhQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SubmitPage.pageName);
+      await expect(await $("body").getText()).to.not.have.string("Section 1 Question 2");
+    });
+  });
+
+    describe("Given I have routing based on the completeness of a block", () => {
+    it("When the blocks being evaluated are complete (Q2 + Q5), Then the dependent questions (Q4 + Q6) should be displayed", async () => {
+      await $(FirstQuestionPage.q1A1()).setValue("1");
+      await $(FirstQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SecondQuestionPage.pageName);
+      await $(SecondQuestionPage.q1A1()).setValue("1");
+      await $(SecondQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(ThirdQuestionPage.pageName);
+      await $(ThirdQuestionPage.q1A1()).setValue("2");
+      await $(ThirdQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(FourthQuestionPage.pageName);
+      await $(FourthQuestionPage.q1A1()).setValue("3");
+      await $(FourthQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(FifthQuestionPage.pageName);
+      await $(FifthQuestionPage.q1A1()).setValue("4");
+      await $(FifthQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SixthQuestionPage.pageName);
+      await $(SixthQuestionPage.q1A1()).setValue("5");
+      await $(SixthQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SeventhQuestionPage.pageName);
+      await $(SeventhQuestionPage.q1A1()).setValue("6");
+      await $(SeventhQuestionPage.submit()).click();
+
+      await expect(await browser.getUrl()).to.contain(SubmitPage.pageName);
+      await expect(await $("body").getText()).to.have.string("Section 1 Question 2");
+      await expect(await $("body").getText()).to.have.string("Section 1 Question 5");
+    });
+  });
+});
