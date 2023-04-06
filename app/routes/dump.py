f77cb8c27015c23e28a4d4c0fad091893a910fe5
Kyle Lawson <kyle.lawson@ext.ons.gov.uk>
2023-04-06 09:40:27 +0100

    Removing duplicate decorator and unnecessary call for QuestionnaireStore in dump.py

--- app/routes/dump.py
+++ app/routes/dump.py
@@ -1,38 +1,23 @@
-from functools import wraps
-
-from flask import Blueprint, g
-from flask_babel import get_locale
+from flask import Blueprint
 from flask_login import current_user, login_required
 
 from app.authentication.roles import role_required
-from app.globals import get_metadata, get_questionnaire_store
+from app.data_models import QuestionnaireStore
+from app.globals import get_questionnaire_store
+from app.helpers.schema_helpers import with_schema
 from app.helpers.session_helpers import with_questionnaire_store
+from app.questionnaire import QuestionnaireSchema
 from app.questionnaire.router import Router
 from app.utilities.json import json_dumps
-from app.utilities.schema import load_schema_from_metadata
 from app.views.handlers.submission import SubmissionHandler
 
 dump_blueprint = Blueprint("dump", __name__)
 
 
-def requires_schema(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        # pylint: disable=assigning-non-slot
-        metadata = get_metadata(current_user)
-        g.schema = load_schema_from_metadata(
-            metadata=metadata, language_code=get_locale().language
-        )
-        result = func(g.schema, *args, **kwargs)
-        return result
-
-    return wrapper
-
-
 @dump_blueprint.route("/dump/debug", methods=["GET"])
 @login_required
 @role_required("dumper")
-def dump_debug():
+def dump_debug() -> str:
     questionnaire_store = get_questionnaire_store(
         current_user.user_id, current_user.user_ik
     )
@@ -43,8 +28,8 @@ def dump_debug():
 @login_required
 @role_required("dumper")
 @with_questionnaire_store
-@requires_schema
-def dump_routing(schema, questionnaire_store):
+@with_schema
+def dump_routing(schema: QuestionnaireSchema, questionnaire_store: QuestionnaireStore) -> tuple[str, int]:
     router = Router(
         schema,
         questionnaire_store.answer_store,
@@ -70,8 +55,8 @@ def dump_routing(schema, questionnaire_store):
 @login_required
 @role_required("dumper")
 @with_questionnaire_store
-@requires_schema
-def dump_submission(schema, questionnaire_store):
+@with_schema
+def dump_submission(schema: QuestionnaireSchema, questionnaire_store: QuestionnaireStore) -> tuple[str, int]:
     router = Router(
         schema,
         questionnaire_store.answer_store,
@@ -82,9 +67,6 @@ def dump_submission(schema, questionnaire_store):
     )
 
     routing_path = router.full_routing_path()
-    questionnaire_store = get_questionnaire_store(
-        current_user.user_id, current_user.user_ik
-    )
 
     submission_handler = SubmissionHandler(schema, questionnaire_store, routing_path)
 
