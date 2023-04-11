e3e89c704a58ec8af2710e71bf8781e81c35b265
Kyle Lawson <kyle.lawson@ext.ons.gov.uk>
2023-04-11 11:39:59 +0100

    Type hinting added to schema.py

--- app/routes/schema.py
+++ app/routes/schema.py
@@ -8,7 +8,8 @@ schema_blueprint = Blueprint("schema", __name__)
 @schema_blueprint.route("/schemas/<schema_name>", methods=["GET"])
 def get_schema_json_from_name(schema_name: str) -> Response | tuple[str, int]:
     try:
-        schema = load_schema_from_name(schema_name)
+        # Type ignore: load_schema_from_name will default the language_code param to None despite not being defaulted in method signature
+        schema = load_schema_from_name(schema_name)  # type: ignore
         return jsonify(schema.json)
     except FileNotFoundError:
         return "Schema Not Found", 404
