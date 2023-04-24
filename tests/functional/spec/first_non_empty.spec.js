90d2ed0368a5cd120ad92ae60cf5ce7717705fc7
petechd <piotr.chudziak@ons.gov.uk>
2023-04-24 15:22:27 +0100

    Fix test and schema name

--- tests/functional/spec/first_non_empty.spec.js
+++ tests/functional/spec/first_non_empty.spec.js
@@ -1,27 +1,27 @@
-import DateEntryBlockPage from "../generated_pages/first_non_empy_item/date-entry-block.page";
-import DateQuestionBlockPage from "../generated_pages/first_non_empy_item/date-question-block.page";
-import TotalTurnoverBlockPage from "../generated_pages/first_non_empy_item/total-turnover-block.page";
+import DateEntryBlockPage from "../generated_pages/first_non_empty_item/date-entry-block.page";
+import DateQuestionBlockPage from "../generated_pages/first_non_empty_item/date-question-block.page";
+import TotalTurnoverBlockPage from "../generated_pages/first_non_empty_item/total-turnover-block.page";
 
 describe("First Non Empty Item Transform", () => {
-  describe('', () => {
-    before("Launch survey", async () => {
-      await browser.openQuestionnaire("test_first_non_empty_item.json");
-    });
+  before("Launch survey", async () => {
+    await browser.openQuestionnaire("test_first_non_empty_item.json");
+  });
 
-    it('When the custom date range is entered and the answer is changed back to metadata date range, Then metadata date should be displayed', async () => {
-        await $(DateQuestionBlockPage.noINeedToReportForADifferentPeriod()).click();
-        await $(DateEntryBlockPage.dateEntryFromday()).setValue("1");
-        await $(DateEntryBlockPage.dateEntryFrommonth()).setValue("06");
-        await $(DateEntryBlockPage.dateEntryFromyear()).setValue("2016");
-        await $(DateEntryBlockPage.dateEntryToday()).setValue("11");
-        await $(DateEntryBlockPage.dateEntryTomonth()).setValue("06");
-        await $(DateEntryBlockPage.dateEntryToyear()).setValue("2016");
-        await $(DateEntryBlockPage.submit());
-        await $(TotalTurnoverBlockPage.previous());
-        await $(DateEntryBlockPage.previous());
-        await $(DateQuestionBlockPage.yesICanReportForThisPeriod()).click();
-        await expect(await browser.getUrl()).to.contain(TotalTurnoverBlockPage.pageName);
-        await expect(await $(TotalTurnoverBlockPage.questionTitle()).getText()).to.contain("1 June 2016 to 11 June 2016");
-    });
+  it("When the custom date range is entered and the answer is changed back to metadata date range, Then metadata date should be displayed", async () => {
+    await $(DateQuestionBlockPage.noINeedToReportForADifferentPeriod()).click();
+    await $(DateQuestionBlockPage.submit()).click();
+    await $(DateEntryBlockPage.dateEntryFromday()).setValue("5");
+    await $(DateEntryBlockPage.dateEntryFrommonth()).setValue("01");
+    await $(DateEntryBlockPage.dateEntryFromyear()).setValue("2017");
+    await $(DateEntryBlockPage.dateEntryToday()).setValue("25");
+    await $(DateEntryBlockPage.dateEntryTomonth()).setValue("01");
+    await $(DateEntryBlockPage.dateEntryToyear()).setValue("2017");
+    await $(DateEntryBlockPage.submit()).click();
+    await $(TotalTurnoverBlockPage.previous()).click();
+    await $(DateEntryBlockPage.previous()).click();
+    await $(DateQuestionBlockPage.yesICanReportForThisPeriod()).click();
+    await $(DateQuestionBlockPage.submit()).click();
+    await expect(await browser.getUrl()).to.contain(TotalTurnoverBlockPage.pageName);
+    await expect(await $(TotalTurnoverBlockPage.questionTitle()).getText()).to.contain("5 January 2017 to 25 January 2017");
   });
 });
