08fed5547baafa80dfdbd7b1682189a89444a889
Kyle Lawson <kyle.lawson@ext.ons.gov.uk>
2023-04-27 17:56:58 +0100

    Schema for repeating blocks and section summary incomplete + ListCollector Question handling many add types by enum.

--- app/views/handlers/list_collector.py
+++ app/views/handlers/list_collector.py
@@ -1,16 +1,24 @@
+from enum import Enum
+from typing import Any
+
 from flask import url_for
 
 from app.views.contexts import ListContext
 from app.views.handlers.question import Question
 
 
+class ListCollectorAddType(Enum):
+    SINGLE = "RedirectToListAddBlock"
+    MULTI = "RedirectToListRepeatingBlocks"
+
+
 class ListCollector(Question):
-    def __init__(self, *args):
-        self._is_adding = False
+    def __init__(self, *args: Any):
+        self._add_type: ListCollectorAddType | None = None
         super().__init__(*args)
 
     def get_next_location_url(self):
-        if self._is_adding:
+        if self._add_type == ListCollectorAddType.SINGLE:
             add_url = url_for(
                 "questionnaire.block",
                 list_name=self.rendered_block["for_list"],
@@ -20,6 +28,17 @@ class ListCollector(Question):
                 return_to_block_id=self._return_to_block_id,
             )
             return add_url
+
+        # TODO Build URL to the block of type ListRepeatingQuestion
+        if self._add_type == ListCollectorAddType.MULTI:
+            add_url = url_for(
+                "questionnaire.block",
+                list_name=self.rendered_block["for_list"],
+                block_id=self.rendered_block["repeating_blocks"]["id"],
+                return_to=self._return_to,
+                return_to_answer_id=self._return_to_answer_id,
+                return_to_block_id=self._return_to_block_id,
+            )
 
         return super().get_next_location_url()
 
@@ -47,10 +66,11 @@ class ListCollector(Question):
         }
 
     def handle_post(self):
-        answer_action = self._get_answer_action()
+        if answer_action := self._get_answer_action():
+            if answer_action["type"] in set(add_type.value for add_type in ListCollectorAddType):
+                self._add_type = ListCollectorAddType(answer_action["type"])
 
-        if answer_action and answer_action["type"] == "RedirectToListAddBlock":
-            self._is_adding = True
+        if self._add_type:
             # pylint: disable=no-member
             # wtforms Form parents are not discoverable in the 2.3.3 implementation
             self.questionnaire_store_updater.update_answers(self.form.data)
