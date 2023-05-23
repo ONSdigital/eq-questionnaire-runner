189ae7255572a2b6f49580372ba99453b965cad4
Rhys Berrow <rhys.berrow@ons.gov.uk>
2023-05-23 10:31:08 +0100

    Update cross section schema

--- tests/integration/questionnaire/test_questionnaire_progress_value_source_blocks.py
+++ tests/integration/questionnaire/test_questionnaire_progress_value_source_blocks.py
@@ -3,6 +3,9 @@ from tests.integration.questionnaire import SUBMIT_URL_PATH
 
 
 class TestQuestionnaireProgressValueSourceBlocks(IntegrationTestCase):
+    def go_to_section(self, section_id):
+        self.get(f"/questionnaire/sections/{section_id}/")
+
     def test_skip_condition_block_not_complete(self):
         """
         Test that a block is skipped if the progress value source is not complete
@@ -161,6 +164,8 @@ class TestQuestionnaireProgressValueSourceBlocks(IntegrationTestCase):
 
         self.launchSurvey("test_progress_value_source_blocks_cross_section")
 
+        self.post()
+
         self.assertInBody("Section 1 Question 1")
         self.post({"s1-b1-q1-a1": 1})
 
@@ -173,6 +178,9 @@ class TestQuestionnaireProgressValueSourceBlocks(IntegrationTestCase):
         self.assertInBody("Section 1 Question 4")
         self.post({"s1-b4-q1-a1": 1})
 
+        self.post()
+        self.post()
+
         self.assertInBody("Section 2 Question 5")
         self.post({"s2-b5-q1-a1": 1})
 
@@ -182,19 +190,24 @@ class TestQuestionnaireProgressValueSourceBlocks(IntegrationTestCase):
         self.assertInBody("Section 2 Question 7")
         self.post({"s2-b7-q1-a1": 1})
 
-        self.assertInBody("Check your answers and submit")
+        self.post()
+        self.assertEqualUrl("/questionnaire/")
 
         # Change block 1 answer to 0
-        self.get(
-            "/questionnaire/s1-b1/?return_to=final-summary&return_to_answer_id=s1-b1-q1-a1#s1-b1-q1-a1"
-        )
+        self.get("/questionnaire/s1-b1/")
         self.assertInBody("Section 1 Question 1")
         self.post({"s1-b1-q1-a1": 0})
 
-        # Redirects to the hub
-        self.assertInUrl(SUBMIT_URL_PATH)
-
-        # Questions 2, 4 and 6 are not visible because they aren't on the path anymore although they've been answered earlier
+        # Questions 2, 4 in Section 1 are not visible because they aren't on the path anymore
+        self.assertEqualUrl("/questionnaire/sections/section-1/")
         self.assertNotInBody("Section 1 Question 2")
         self.assertNotInBody("Section 1 Question 4")
+        self.post()
+
+        # Redirects to the hub
+        self.assertEqualUrl("/questionnaire/")
+
+        # Question 6 in Section 2 is not visible because it is not on the path anymore
+        self.go_to_section("section-2")
+        self.assertEqualUrl("/questionnaire/sections/section-2/")
         self.assertNotInBody("Section 2 Question 6")
