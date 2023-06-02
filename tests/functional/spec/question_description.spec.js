701ad6d9b212783323e589404134cad5a71f663c
Rhys Berrow <rhys.berrow@ons.gov.uk>
2023-06-02 12:55:39 +0100

    Update schema and test

--- tests/functional/spec/question_description.spec.js
+++ tests/functional/spec/question_description.spec.js
@@ -1,5 +1,7 @@
 import NameBlockPage from "../generated_pages/question_description/name-block.page.js";
-import MandatoryCheckboxPage from "../generated_pages/optional_guidance_and_description/mandatory-checkbox.page";
+import DescriptionBlockPage from "../generated_pages/optional_guidance_and_description/description-block.page";
+import RadioPage from "../generated_pages/optional_guidance_and_description/mandatory-radio.page";
+import RadioPageTwo from "../generated_pages/optional_guidance_and_description/mandatory-radio-two.page";
 
 describe("Question description", () => {
   it("Given a question description has been set in the schema as an array, When it is rendered, Then it is displayed correctly as multiple paragraph attributes", async () => {
@@ -9,8 +11,14 @@ describe("Question description", () => {
 });
 
 describe("Optional question description and guidance", () => {
-  it("Given a question description has been set in the schema as an array, When the value to be displayed is None, Then it is not rendered on the page", async () => {
+  it("Given a question description has been set in the schema, When the value to be displayed is None, Then it is not rendered on the page", async () => {
     await browser.openQuestionnaire("test_optional_guidance_and_description.json");
-    await expect(await $(MandatoryCheckboxPage.questionTitle()).getHTML()).to.contain("<p>''</p>");
+    await $(DescriptionBlockPage.submit()).click();
+    await expect(await $(RadioPage.questionTitle()).getHTML()).to.not.contain("<p>''</p>");
+    await expect(await $(RadioPage.guidance()).isExisting()).to.be.false;
+    await $(RadioPage.no()).click();
+    await $(RadioPage.submit()).click();
+    await expect(await $(RadioPageTwo.questionTitle()).getHTML()).to.contain("<li>List item one</li>");
+    await expect(await $(RadioPageTwo.questionTitle()).getHTML()).to.not.contain("<li></li>");
   });
 });
