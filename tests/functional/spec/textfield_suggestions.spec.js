86021f44fe652c9de0ca0e6a8a196aecaee813bb
petechd <piotr.chudziak@ons.gov.uk>
2023-06-05 09:16:12 +0100

    Increase timeout

--- tests/functional/spec/textfield_suggestions.spec.js
+++ tests/functional/spec/textfield_suggestions.spec.js
@@ -31,7 +31,7 @@ describe("Suggestions", () => {
     await browser.keys(" United");
     await suggestionsList.waitForExist();
     await expect(await $$(".ons-js-autosuggest-listbox li").length).to.not.equal(0);
-    await suggestionsOption.waitForExist({ timeout: 2000 });
+    await suggestionsOption.waitForExist({ timeout: 3000 });
     await suggestionsOption.click();
     await $(MultipleSuggestionsPage.submit()).click();
     await expect(await browser.getUrl()).to.contain(SubmitPage.url());
