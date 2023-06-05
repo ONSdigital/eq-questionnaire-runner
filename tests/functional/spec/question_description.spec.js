390297921f3b181e12f5e9632f923f4e1a885e48
Rhys Berrow <rhys.berrow@ons.gov.uk>
2023-06-05 15:17:23 +0100

    Add test

--- tests/functional/spec/question_description.spec.js
+++ tests/functional/spec/question_description.spec.js
@@ -2,6 +2,8 @@ import NameBlockPage from "../generated_pages/question_description/name-block.pa
 import DescriptionBlockPage from "../generated_pages/optional_guidance_and_description/description-block.page";
 import RadioPage from "../generated_pages/optional_guidance_and_description/mandatory-radio.page";
 import RadioPageTwo from "../generated_pages/optional_guidance_and_description/mandatory-radio-two.page";
+import IntroductionPage from "../generated_pages/question_guidance/introduction.page";
+import GuidancePage from "../generated_pages/question_guidance/block-test-guidance-title.page";
 
 describe("Question description", () => {
   it("Given a question description has been set in the schema as an array, When it is rendered, Then it is displayed correctly as multiple paragraph attributes", async () => {
@@ -20,5 +22,14 @@ describe("Optional question description and guidance", () => {
     await $(RadioPage.submit()).click();
     await expect(await $(RadioPageTwo.questionTitle()).getHTML()).to.contain("<li>List item one</li>");
     await expect(await $(RadioPageTwo.questionTitle()).getHTML()).to.not.contain("<li></li>");
+  });
+});
+
+describe("Question guidance", () => {
+  it("Given a question guidance with multiple content items, When it is rendered, Then there should only be one guidance box", async () => {
+    await browser.openQuestionnaire("test_question_guidance.json");
+    await $(IntroductionPage.submit()).click();
+    await expect(await browser.getUrl()).to.contain(GuidancePage.pageName);
+    await expect(await $$("#question-guidance-question-test-guidance-title").length).to.equal(1);
   });
 });
