ddcc76b63c40c77c7f428d58f824424692bec802
petechd <piotr.chudziak@ons.gov.uk>
2023-06-07 14:52:28 +0100

    Revert dbt-ni change

--- tests/functional/spec/theme_dbt_ni.spec.js
+++ tests/functional/spec/theme_dbt_ni.spec.js
@@ -1,14 +1,14 @@
-import RadioPage from "../generated_pages/theme_desnz_ni/radio.page";
+import RadioPage from "../generated_pages/theme_dbt_ni/radio.page";
 
-describe("Theme DESNZ-NI", () => {
-  describe("Given I launch a DESNZ-NI themed questionnaire", () => {
+describe("Theme DBT-NI", () => {
+  describe("Given I launch a DBT-NI themed questionnaire", () => {
     before(async () => {
-      await browser.openQuestionnaire("test_theme_desnz_ni.json");
+      await browser.openQuestionnaire("test_theme_dbt_ni.json");
     });
 
     it("When I navigate to the radio page, Then I should see DBT-NI theme content", async () => {
       await expect(await browser.getUrl()).to.contain(RadioPage.pageName);
-      await expect(await $("#desnz-logo-alt").getHTML()).to.contain("Department for Business and Trade");
+      await expect(await $("#dbt-logo-alt").getHTML()).to.contain("Department for Business and Trade");
       await expect(await $("#finance-ni-logo-alt").getHTML()).to.contain("Northern Ireland Department of Finance logo");
     });
   });
