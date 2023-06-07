0aaef9dd7d3910ee8da5382a3a6b9cad8fe003c6
petechd <piotr.chudziak@ons.gov.uk>
2023-06-07 14:25:56 +0100

    Fix functional test

--- tests/functional/spec/theme_desnz_ni.spec.js
+++ tests/functional/spec/theme_desnz_ni.spec.js
@@ -1,9 +1,9 @@
-import RadioPage from "../generated_pages/theme_dbt_ni/radio.page";
+import RadioPage from "../generated_pages/theme_desnz_ni/radio.page";
 
 describe("Theme DESNZ-NI", () => {
   describe("Given I launch a DESNZ-NI themed questionnaire", () => {
     before(async () => {
-      await browser.openQuestionnaire("test_theme_dbt_ni.json");
+      await browser.openQuestionnaire("test_theme_desnz_ni.json");
     });
 
     it("When I navigate to the radio page, Then I should see DESNZ-NI theme content", async () => {
