a6d3a0e46d661206275348309cf56dd185e96b5c
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-06-16 09:24:22 +0100

    run format

--- app/questionnaire/questionnaire_schema.py
+++ app/questionnaire/questionnaire_schema.py
@@ -106,24 +106,35 @@ class QuestionnaireSchema:  # pylint: disable=too-many-public-methods
         for block in self.get_blocks():
             if block["type"] == "Question":
                 if questions := self.get_all_questions_for_block(block):
-                    for answer in get_answers_from_question(
-                        questions[0]
-                    ):
+                    for answer in get_answers_from_question(questions[0]):
                         answer_id = answer["id"]
-                        if (answer_type := answer.get("type")) and answer_type not in ["Date", "MonthYearDate", "YearDate"]:
-                            answer_to_search_for_min_max = self.get_answers_by_answer_id(
-                                answer_id
-                            )[0]
+                        if (answer_type := answer.get("type")) and answer_type not in [
+                            "Date",
+                            "MonthYearDate",
+                            "YearDate",
+                        ]:
+                            answer_to_search_for_min_max = (
+                                self.get_answers_by_answer_id(answer_id)[0]
+                            )
                             for item in ["minimum", "maximum"]:
                                 if item in answer and not isinstance(
                                     answer_to_search_for_min_max[item]["value"], int
                                 ):
-                                    if answer_to_search_for_min_max[item]["value"]["source"] == "answers":
-                                        min_max_answer_id = answer_to_search_for_min_max[item][
-                                            "value"
-                                        ]["identifier"]
+                                    if (
+                                        answer_to_search_for_min_max[item]["value"][
+                                            "source"
+                                        ]
+                                        == "answers"
+                                    ):
+                                        min_max_answer_id = (
+                                            answer_to_search_for_min_max[item]["value"][
+                                                "identifier"
+                                            ]
+                                        )
                                         self.min_and_max_map[min_max_answer_id] = (
-                                            self.get_answers_by_answer_id(min_max_answer_id)[0]
+                                            self.get_answers_by_answer_id(
+                                                min_max_answer_id
+                                            )[0]
                                             .get(item, {})
                                             .get("value", {})
                                         )
