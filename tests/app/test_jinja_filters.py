9189c1bd8552274150a795e249e18a5260d1f373
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-06-21 09:22:51 +0100

    add test coverage

--- tests/app/test_jinja_filters.py
+++ tests/app/test_jinja_filters.py
@@ -255,6 +255,8 @@ def test_format_duration(duration, formatted_duration, app):
             {"maximum": {"value": 123456789012345678901123456789012345678901234567890}},
             None,
         ),
+        ({"maximum": {"value": {"identifier": "set-maximum","source": "answers"}}}, 5),
+        ({"minimum": {"value": {"identifier": "set-minimum","source": "answers"}}}, 15),
     ),
 )
 def test_get_width_for_number(answer, width, app):
@@ -339,24 +341,6 @@ def test_get_width_for_number(answer, width, app):
                             "value": {"identifier": "set-maximum", "source": "answers"}
                         },
                     }
-                ],
-            },
-            False,
-        ),
-        (
-            {
-                "type": "General",
-                "answers": [
-                    {"type": "Currency", "minimum": {"value": {"identifier": "1"}}}
-                ],
-            },
-            False,
-        ),
-        (
-            {
-                "type": "General",
-                "answers": [
-                    {"type": "Currency", "maximum": {"value": {"identifier": "1"}}}
                 ],
             },
             False,
