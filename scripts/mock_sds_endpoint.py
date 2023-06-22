29601ac0520e91d37aca3b72a38331689a3d7f90
katie-gardner <katharine.gardner@and.digital>
2023-06-22 11:53:01 +0100

    Initial changes

--- scripts/mock_sds_endpoint.py
+++ scripts/mock_sds_endpoint.py
@@ -17,9 +17,31 @@ def get_sds_data():
     return Response(status=404)
 
 
+@app.route("/v1/dataset_metadata")
+def get_sds_dataset_ids():
+    survey_id = request.args.get("survey_id")
+    period_id = request.args.get("period_id")
+
+    return load_mock_sds_dataset_metadata(survey_id, period_id)
+
+
 def load_mock_data(filename):
     with open(filename, encoding="utf-8") as mock_data_file:
         return json.load(mock_data_file)
+
+
+def load_mock_sds_dataset_metadata(survey_id: str, period_id: str):
+    del period_id  # not required for mock
+    if survey_id == "101":
+        return load_mock_data(
+            "scripts/mock_data/supplementary_dataset_multiple_item_response.json"
+        )
+    if survey_id == "202":
+        return load_mock_data(
+            "scripts/mock_data/supplementary_dataset_single_item_response.json"
+        )
+
+    return Response(status=404)
 
 
 if __name__ == "__main__":
