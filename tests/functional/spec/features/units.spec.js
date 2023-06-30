deb64a7ad74100df961c687ff8359954950070a6
petechd <53475968+petechd@users.noreply.github.com>
2023-06-30 13:10:35 +0100

    Fix unit label hover title for metric ton (#1150)

--- tests/functional/spec/features/units.spec.js
+++ tests/functional/spec/features/units.spec.js
@@ -2,6 +2,7 @@ import SetLengthUnitsBlockPage from "../../generated_pages/unit_patterns/set-len
 import SetDurationUnitsBlockPage from "../../generated_pages/unit_patterns/set-duration-units-block.page.js";
 import SetAreaUnitsBlockPage from "../../generated_pages/unit_patterns/set-area-units-block.page.js";
 import SetVolumeUnitsBlockPage from "../../generated_pages/unit_patterns/set-volume-units-block.page.js";
+import SetWeightUnitsBlockPage from "../../generated_pages/unit_patterns/set-weight-units-block.page.js";
 import SubmitPage from "../../generated_pages/unit_patterns/submit.page.js";
 
 describe("Units", () => {
@@ -15,6 +16,7 @@ describe("Units", () => {
     await $(SetDurationUnitsBlockPage.submit()).click();
     await $(SetAreaUnitsBlockPage.submit()).click();
     await $(SetVolumeUnitsBlockPage.submit()).click();
+    await $(SetWeightUnitsBlockPage.submit()).click();
     await expect(await $(SubmitPage.durationHour()).getText()).to.equal("6 hours");
     await expect(await $(SubmitPage.durationYear()).getText()).to.equal("20 years");
   });
@@ -31,15 +33,26 @@ describe("Units", () => {
     await $(SetDurationUnitsBlockPage.submit()).click();
     await $(SetAreaUnitsBlockPage.submit()).click();
     await $(SetVolumeUnitsBlockPage.submit()).click();
+    await $(SetWeightUnitsBlockPage.submit()).click();
     await expect(await $(SubmitPage.durationHour()).getText()).to.equal("6 awr");
     await expect(await $(SubmitPage.durationYear()).getText()).to.equal("20 mlynedd");
   });
 
   it("Given we open a questionnaire with unit labels, when the label is highlighted by the tooltip, then the long unit label should be displayed.", async () => {
     await browser.openQuestionnaire("test_unit_patterns.json", { language: "en" });
-    await expect(await $(SetLengthUnitsBlockPage.centimetresUnit()).getAttribute("title")).to.equal("centimeters");
-    await expect(await $(SetLengthUnitsBlockPage.metresUnit()).getAttribute("title")).to.equal("meters");
-    await expect(await $(SetLengthUnitsBlockPage.kilometresUnit()).getAttribute("title")).to.equal("kilometers");
+    await expect(await $(SetLengthUnitsBlockPage.centimetresUnit()).getAttribute("title")).to.equal("centimetres");
+    await expect(await $(SetLengthUnitsBlockPage.metresUnit()).getAttribute("title")).to.equal("metres");
+    await expect(await $(SetLengthUnitsBlockPage.kilometresUnit()).getAttribute("title")).to.equal("kilometres");
     await expect(await $(SetLengthUnitsBlockPage.milesUnit()).getAttribute("title")).to.equal("miles");
+  });
+
+  it("Given we open a questionnaire with unit labels, when the weight unit label is highlighted by the tooltip, then the correct unit label should be displayed.", async () => {
+    await browser.openQuestionnaire("test_unit_patterns.json", { language: "en" });
+    await $(SetLengthUnitsBlockPage.submit()).click();
+    await $(SetDurationUnitsBlockPage.submit()).click();
+    await $(SetAreaUnitsBlockPage.submit()).click();
+    await $(SetVolumeUnitsBlockPage.submit()).click();
+    await expect(await $("body").getText()).to.have.string("tonnes");
+    await expect(await $("body").getText()).to.not.have.string("metric tons");
   });
 });
