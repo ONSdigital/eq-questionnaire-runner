9eee605518c529b6a9d286e81cbb1c2cd6e98970
petechd <piotr.chudziak@ons.gov.uk>
2023-07-17 12:05:51 +0100

    Remove duplicated is collector complete method

--- app/views/handlers/list_collector_content.py
+++ app/views/handlers/list_collector_content.py
@@ -8,17 +8,7 @@ class ListCollectorContent(ListCollector):
     def handle_post(self):
         self._set_started_at_metadata()
         self.questionnaire_store_updater.add_completed_location()
-        if self._is_list_collector_content_complete():
+        if self._is_list_collector_complete():
             self._update_section_completeness()
         self.questionnaire_store_updater.update_progress_for_dependent_sections()
         self.questionnaire_store_updater.save()
-
-    def _is_list_collector_content_complete(self):
-        list_name = self._schema.get_repeating_blocks_list_name_for_section(
-            self.current_location.section_id
-        )
-        return not self.get_first_incomplete_repeating_block_location(
-            repeating_block_ids=self.repeating_block_ids,
-            section_id=self.current_location.section_id,
-            list_name=list_name,
-        )
