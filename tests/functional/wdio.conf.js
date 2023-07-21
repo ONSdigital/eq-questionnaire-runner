b69be78e056c87a8030753c2d572069a88eda57a
Mebin Abraham <mebin95+work@gmail.com>
2023-07-21 14:53:43 +0100

    Revert trace level

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -75,7 +75,7 @@ export const config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "trace",
+  logLevel: "error",
   outputDir: "tests/functional/logs/",
   //
   // Set specific log levels per logger
