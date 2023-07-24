9c0409676f0ae6dbfbd630118cc8f13759423903
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2023-07-24 10:48:44 +0100

    Retry flaky functional tests (#1170)

--- tests/functional/spec/features/validation/sum/dynamic.spec.js
+++ tests/functional/spec/features/validation/sum/dynamic.spec.js
@@ -10,7 +10,11 @@ import ListCollectorEditPage from "../../../../generated_pages/validation_sum_ag
 import HubPage from "../../../../base_pages/hub.page";
 import TotalBlockOtherPage from "../../../../generated_pages/validation_sum_against_total_dynamic_answers/total-block-other.page";
 
-describe("Feature: Sum of dynamic answers based on list and optional static answers equal to validation against total ", () => {
+describe("Feature: Sum of dynamic answers based on list and optional static answers equal to validation against total ", function () {
+  // These tests are flaky therefore we add a retry. The cause is unknown.
+  // :TODO: Revert this in future when we have a fix for this.
+  this.retries(5);
+
   const summaryTitles = 'dt[class="ons-summary__item-title"]';
   beforeEach(async () => {
     await browser.openQuestionnaire("test_validation_sum_against_total_dynamic_answers.json");
