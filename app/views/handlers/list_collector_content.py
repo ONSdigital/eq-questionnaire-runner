f68f7bd4ae474039ad84283d44ecd5641ffd004b
petechd <piotr.chudziak@ons.gov.uk>
2023-07-31 15:50:31 +0100

    Remove duplication in handle post

--- app/views/handlers/list_collector_content.py
+++ app/views/handlers/list_collector_content.py
@@ -9,14 +9,12 @@ class ListCollectorContent(ListCollector):
         return self.rendered_block["content"]
 
     def handle_post(self) -> None:
-        self._set_started_at_metadata()
-        self.questionnaire_store_updater.add_completed_location()
         if self._is_list_collector_complete():
-            self.questionnaire_store_updater.add_completed_location()
             self._routing_path = self.router.routing_path(
                 section_id=self._current_location.section_id,
                 list_item_id=self._current_location.list_item_id,
             )
             return super(Question, self).handle_post()
+        self.questionnaire_store_updater.add_completed_location()
         self.questionnaire_store_updater.update_progress_for_dependent_sections()
         self.questionnaire_store_updater.save()
