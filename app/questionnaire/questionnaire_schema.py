3026fe44b665d1e5b25033e3470d63805f2e2321
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-08-01 13:37:03 +0100

    run make format

--- app/questionnaire/questionnaire_schema.py
+++ app/questionnaire/questionnaire_schema.py
@@ -141,7 +141,10 @@ class QuestionnaireSchema:  # pylint: disable=too-many-public-methods
 
             elif isinstance(value, dict) and value:
                 if value.get("source") == "answers":
-                    longest_value_length = max(longest_value_length, int(self._min_and_max_map[value["identifier"]][min_max]))
+                    longest_value_length = max(
+                        longest_value_length,
+                        int(self._min_and_max_map[value["identifier"]][min_max]),
+                    )
 
         self._min_and_max_map[answer_id][min_max] = longest_value_length or len(
             default_min_max
