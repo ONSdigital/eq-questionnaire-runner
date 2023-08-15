5abafb2150324e1e48cbf8b6b9a0b96e3253e9d8
petechd <piotr.chudziak@ons.gov.uk>
2023-08-15 10:57:59 +0100

    Add EQ_MAX_INSTANCES env var

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -45,7 +45,7 @@ export const config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 2,
+  maxInstances: process.env.MAX_INSTANCES || 2,
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://docs.saucelabs.com/reference/platforms-configurator
