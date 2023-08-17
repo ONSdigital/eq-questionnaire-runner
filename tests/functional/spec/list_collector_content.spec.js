c582e80d0ee748a1618425ce7d8cb353eef3d402
petechd <piotr.chudziak@ons.gov.uk>
2023-08-17 09:20:19 +0100

    Fix Continue button

--- tests/functional/spec/list_collector_content.spec.js
+++ tests/functional/spec/list_collector_content.spec.js
@@ -8,6 +8,7 @@ import ListCollectorFirstRepeatingBlockPage from "../generated_pages/list_collec
 import ListCollectorSecondRepeatingBlockPage from "../generated_pages/list_collector_content_page/companies-repeating-block-2-repeating-block.page";
 import ListCollectorContentPage from "../generated_pages/list_collector_content_page/list-collector-content.page";
 import ListCollectorContentSectionSummaryPage from "../generated_pages/list_collector_content_page/section-list-collector-contents-summary.page";
+import PropertyDetailsSummaryPage from "../generated_pages/section_summary/property-details-section-summary.page";
 
 describe("List Collector Section Summary and Summary Items", () => {
   describe("Given I launch the test list collector section summary items survey", () => {
@@ -23,7 +24,7 @@ describe("List Collector Section Summary and Summary Items", () => {
       await $(ResponsiblePartyQuestionPage.submit()).click();
       await expect(await browser.getUrl()).to.contain(ListCollectorContentPage.url());
     });
-    it("When I get to the list collector content page, Then the relevant content is displayed.", async () => {
+    it("When I get to the list collector content page, Then the relevant content and button is displayed.", async () => {
       await fillInListCollectorSection();
       await $(HubPage.submit()).click();
       await $(ResponsiblePartyQuestionPage.yes()).click();
@@ -32,6 +33,7 @@ describe("List Collector Section Summary and Summary Items", () => {
         "You have previously reported the following companies. Press continue to updated registration and trading information.",
       );
       await expect(await $(ListCollectorContentPage.questionText()).getHTML()).to.contain("Companies");
+      await expect(await $(ListCollectorContentPage.submit()).getText()).to.equal("Continue");
     });
     it("When I get to list collector content block section, Then I should be able to complete repeating blocks and get to the summary.", async () => {
       await fillInListCollectorSection();
