3290d18c1754810e12832d95e6205a60dc7ffd5e
petechd <piotr.chudziak@ons.gov.uk>
2023-08-31 09:06:38 +0100

    Add back X-Xss protection

--- tests/integration/test_app_create.py
+++ tests/integration/test_app_create.py
@@ -116,6 +116,7 @@ class TestCreateApp(unittest.TestCase):  # pylint: disable=too-many-public-metho
                 headers["Strict-Transport-Security"],
             )
             self.assertEqual("DENY", headers["X-Frame-Options"])
+            self.assertEqual("1; mode=block", headers["X-Xss-Protection"])
             self.assertEqual("nosniff", headers["X-Content-Type-Options"])
 
     def test_csp_policy_headers(self):
