00df9ec3be9e19a915356c34e0555bde76f1408a
Liam Toozer <liam.toozer@gmail.com>
2023-09-26 09:25:35 +0100

    Add type hints to year_date_field.py

--- app/forms/fields/year_date_field.py
+++ app/forms/fields/year_date_field.py
@@ -1,18 +1,20 @@
 import logging
 from functools import cached_property
+from typing import Sequence, Callable, Any
 
+from werkzeug.datastructures import MultiDict
 from wtforms import Form, FormField, StringField
-from wtforms.utils import unset_value
+from wtforms.utils import unset_value, UnsetValue
 
 logger = logging.getLogger(__name__)
 
 
-def get_form_class(validators):
+def get_form_class(validators: Sequence[Callable]) -> Form:
     class YearDateForm(Form):
         year = StringField(validators=validators)
 
         @cached_property
-        def data(self):
+        def data(self) -> str | None:
             # pylint: disable=no-member
             # wtforms Form parents are not discoverable in the 2.3.3 implementation
             data = super().data
@@ -26,11 +28,28 @@ def get_form_class(validators):
 
 
 class YearDateField(FormField):
-    def __init__(self, validators, **kwargs):
+    def __init__(
+        self,
+        *,
+        validators: Sequence[Callable],
+        label: str | None = None,
+        description: str,
+        **kwargs: Any
+    ):
         form_class = get_form_class(validators)
-        super().__init__(form_class, **kwargs)
+        super().__init__(
+            form_class,
+            label=label,
+            description=description,
+            **kwargs,
+        )
 
-    def process(self, formdata, data=None, extra_filters=None):
+    def process(
+        self,
+        formdata: MultiDict,
+        data: UnsetValue = unset_value,
+        extra_filters: None = None
+    ) -> None:
         if data is not unset_value:
             substrings = data.split("-")
             data = {"year": substrings[0]}
