74d76b63c2c33f64ed31b3e062af45bc183efe30
VirajP1002 <Viraj.Patel@ons.gov.uk>
2023-09-29 09:08:22 +0100

    add tests that were temporarily removed

--- tests/functional/spec/features/calculated_summary/calculated_summary.spec.js
+++ tests/functional/spec/features/calculated_summary/calculated_summary.spec.js
@@ -1,20 +1,20 @@
 import { CalculatedSummaryTestCase } from "./calculated_summary_test_case.js";
-//
-// describe("Feature: Calculated Summary", () => {
-//   describe("Given I have a Calculated Summary", () => {
-//     CalculatedSummaryTestCase.testCase("test_calculated_summary.json");
-//   });
-//   describe("Given I have a Calculated Summary with the new format", () => {
-//     CalculatedSummaryTestCase.testCase("test_new_calculated_summary.json");
-//   });
-//
-//   describe("Given I have a Calculated Summary", () => {
-//     CalculatedSummaryTestCase.testCrossSectionDependencies("test_calculated_summary_cross_section_dependencies.json");
-//   });
-//   describe("Given I have a Calculated Summary with the new format", () => {
-//     CalculatedSummaryTestCase.testCrossSectionDependencies("test_new_calculated_summary_cross_section_dependencies.json");
-//   });
-// });
+
+describe("Feature: Calculated Summary", () => {
+  describe("Given I have a Calculated Summary", () => {
+    CalculatedSummaryTestCase.testCase("test_calculated_summary.json");
+  });
+  describe("Given I have a Calculated Summary with the new format", () => {
+    CalculatedSummaryTestCase.testCase("test_new_calculated_summary.json");
+  });
+
+  describe("Given I have a Calculated Summary", () => {
+    CalculatedSummaryTestCase.testCrossSectionDependencies("test_calculated_summary_cross_section_dependencies.json");
+  });
+  describe("Given I have a Calculated Summary with the new format", () => {
+    CalculatedSummaryTestCase.testCrossSectionDependencies("test_new_calculated_summary_cross_section_dependencies.json");
+  });
+});
 
 describe("Feature: Calculated Summary with negative values", () => {
   describe("Given I enter a negative value in the first section", () => {
