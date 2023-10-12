e4cc07a4e1c21006e24dbacbc5e09877c5278118
VirajP1002 <Viraj.Patel@ons.gov.uk>
2023-10-12 10:46:34 +0100

    Remove venv from being ignored

--- scripts/run_lint_python.sh
+++ scripts/run_lint_python.sh
@@ -34,5 +34,5 @@ display_result $? 1 "isort linting check"
 ./scripts/run_mypy.sh
 display_result $? 1 "Mypy type check"
 
-black --check . --exclude node_modules --exclude venv
+black --check . --exclude node_modules
 display_result $? 1 "Python code formatting check"
