98a06901bfe2ba3c8b2864f29386ea3f03bb7ec2
Liam Toozer <liam.toozer@gmail.com>
2023-10-23 11:28:30 +0100

    Add reused ReturnLocation in test

--- tests/app/questionnaire/test_return_location.py
+++ tests/app/questionnaire/test_return_location.py
@@ -18,14 +18,7 @@ def test_return_location_to_dict():
 
 
 def test_return_location_to_dict_with_anchor_return_to_answer_id():
-    return_location = ReturnLocation(
-        return_to="test-return-to",
-        return_to_block_id="test-return-to-block-id",
-        return_to_answer_id="test-return-to-answer-id",
-        return_to_list_item_id="return-to-list-item-id",
-    )
-
-    assert return_location.to_dict(answer_id_is_anchor=True) == {
+    assert TEST_RETURN_LOCATION_OBJECT.to_dict(answer_id_is_anchor=True) == {
         "return_to": "test-return-to",
         "return_to_block_id": "test-return-to-block-id",
         "_anchor": "test-return-to-answer-id",
