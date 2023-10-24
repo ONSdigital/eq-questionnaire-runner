7fb217a56a610bc4f3b92768e4c40554cf6334ad
petechd <piotr.chudziak@ons.gov.uk>
2023-10-24 07:56:04 +0100

    Fix progress store import

--- app/questionnaire/dependencies.py
+++ app/questionnaire/dependencies.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Mapping, Sequence
 from ordered_set import OrderedSet
 from werkzeug.datastructures import MultiDict
 
-from app.data_models import progress_store as ps
+from app.data_models import ProgressStore
 from app.questionnaire import QuestionnaireSchema
 from app.questionnaire.questionnaire_schema import get_sources_for_type_from_data
 from app.utilities.mappings import get_flattened_mapping_values
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
 def get_routing_path_block_ids_by_section_for_dependent_sections(
     *,
     location: LocationType,
-    progress_store: ps.ProgressStore,
+    progress_store: ProgressStore,
     path_finder: PathFinder,
     source_type: str,
     data: MultiDict | Mapping | Sequence,
@@ -58,7 +58,7 @@ def get_routing_path_block_ids_by_section_for_dependent_sections(
 def get_routing_path_block_ids_by_section_for_calculated_summary_dependencies(
     *,
     location: LocationType,
-    progress_store: ps.ProgressStore,
+    progress_store: ProgressStore,
     path_finder: PathFinder,
     data: MultiDict | Mapping | Sequence,
     sections_to_ignore: list | None = None,
