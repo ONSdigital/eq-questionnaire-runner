c50f6a10dffb85e245f877eef5e63d78d1618fab
VirajP1002 <Viraj.Patel@ons.gov.uk>
2023-11-20 14:13:00 +0000

    Refactor more type hints

--- app/utilities/json.py
+++ app/utilities/json.py
@@ -3,12 +3,12 @@ from typing import IO, Any
 import simplejson as json
 
 
-def json_load(file: IO[str], **kwargs: Any) -> dict:
-    return json.load(file, use_decimal=True, **kwargs)  # type: ignore
+def json_load(file: IO[str], **kwargs: Any) -> Any:
+    return json.load(file, use_decimal=True, **kwargs)
 
 
-def json_loads(data: str, **kwargs: Any) -> dict:
-    return json.loads(data, use_decimal=True, **kwargs)  # type: ignore
+def json_loads(data: str, **kwargs: Any) -> Any:
+    return json.loads(data, use_decimal=True, **kwargs)
 
 
 def json_dumps(data: Any, **kwargs: Any) -> str:
