7e52462febbbb6ca07368ec3039203c62f54c1d4
katie-gardner <katharine.gardner@and.digital>
2023-11-21 15:54:48 +0000

    Linting error

--- app/routes/questionnaire.py
+++ app/routes/questionnaire.py
@@ -17,7 +17,6 @@ from app.authentication.no_questionnaire_state_exception import (
     NoQuestionnaireStateException,
 )
 from app.data_models import QuestionnaireStore, SessionStore
-from app.data_models.metadata_proxy import MetadataProxy
 from app.globals import (
     get_metadata,
     get_questionnaire_store,
