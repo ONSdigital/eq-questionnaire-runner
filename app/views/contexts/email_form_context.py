99c73cbca291a2f1b233d42b90e70f2915782270
petechd <53475968+petechd@users.noreply.github.com>
2023-11-30 08:29:46 +0000

    Add confirmation email support to non census thank-you page (#1253)

--- app/views/contexts/email_form_context.py
+++ app/views/contexts/email_form_context.py
@@ -8,19 +8,16 @@ from app.forms.email_form import EmailForm
 def build_confirmation_email_form_context(
     email_confirmation_form: EmailForm,
 ) -> dict[str, Union[bool, str, Any]]:
-    context = {
+    return {
         "hide_sign_out_button": False,
         "sign_out_url": url_for("session.get_sign_out"),
+        "form": build_email_form_context(email_confirmation_form),
     }
-    context.update(build_email_form_context(email_confirmation_form))
-    return context
 
 
 def build_email_form_context(email_confirmation_form: EmailForm) -> dict[str, Any]:
     return {
-        "form": {
-            "mapped_errors": email_confirmation_form.map_errors(),
-            "email_field": email_confirmation_form.email,
-            "errors": email_confirmation_form.errors,
-        }
+        "mapped_errors": email_confirmation_form.map_errors(),
+        "email_field": email_confirmation_form.email,
+        "errors": email_confirmation_form.errors,
     }
