2669e87759d72f2f46cde3f6c5161f7185f78a93
VirajP1002 <Viraj.Patel@ons.gov.uk>
2024-01-31 14:31:12 +0000

    Update tests to use correct app context in order to prevent tests having to be manually stopped to pass

--- tests/integration/test_create_token.py
+++ tests/integration/test_create_token.py
@@ -1,30 +1,14 @@
 import uuid
 
-from sdc.crypto.key_store import KeyStore
-
 from app.authentication.auth_payload_versions import AuthPayloadVersion
 from app.authentication.authenticator import decrypt_token
-from profile_application import app
+from tests.integration.app_context_test_case import AppContextTestCase
 from tests.integration.create_token import (
     ACCOUNT_SERVICE_URL,
     PAYLOAD_V2_SOCIAL,
     PAYLOAD_V2_SUPPLEMENTARY_DATA,
-    TokenGenerator,
 )
-from tests.integration.integration_test_case import (
-    EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY_KID,
-    KEYS_DICT,
-    SR_USER_AUTHENTICATION_PUBLIC_KEY_KID,
-    IntegrationTestCase,
-)
-
-key_store = KeyStore(KEYS_DICT)
-
-token_generator = TokenGenerator(
-    key_store,
-    EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY_KID,
-    SR_USER_AUTHENTICATION_PUBLIC_KEY_KID,
-)
+from tests.integration.integration_test_case import IntegrationTestCase
 
 test_parameters = [
     {
@@ -90,17 +74,17 @@ as v1 methods are intended to be dropped and the other tests only use methods th
 """
 
 
-class TestCreateToken(IntegrationTestCase):
+class TestCreateToken(IntegrationTestCase, AppContextTestCase):
     def test_payload_from_token(self):
         for value in test_parameters:
             with self.subTest():
                 additional_payload = value.get("additional_payload")
-                token = token_generator.create_token_v2(
+                token = self.token_generator.create_token_v2(
                     schema_name=value.get("schema"),
                     theme=value.get("theme"),
                     **additional_payload,
                 )
-                with app.app_context():
+                with self.test_app.app_context():
                     decrypted_token = decrypt_token(token)
                     self.assertEqual(
                         decrypted_token, value.get("payload") | decrypted_token
@@ -114,7 +98,7 @@ class TestCreateToken(IntegrationTestCase):
         token = self.token_generator.create_token_v2(
             "social_demo.json", theme="social", value="Dummy Text"
         )
-        with app.app_context():
+        with self.test_app.app_context():
             decrypted_token = decrypt_token(token)
             assert decrypted_token.get("survey_metadata") == {
                 "data": {
@@ -131,7 +115,7 @@ class TestCreateToken(IntegrationTestCase):
         token = self.token_generator.create_supplementary_data_token(
             "test_checkbox.json", flag_1=True
         )
-        with app.app_context():
+        with self.test_app.app_context():
             decrypted_token = decrypt_token(token)
             self.assertEqual(
                 decrypted_token, PAYLOAD_V2_SUPPLEMENTARY_DATA | decrypted_token
@@ -142,12 +126,12 @@ class TestCreateToken(IntegrationTestCase):
 
     def test_metadata_is_removed_from_token(self):
         for value in [
-            [token_generator.create_token_without_jti, "jti"],
-            [token_generator.create_token_without_case_id, "case_id"],
-            [token_generator.create_token_without_trad_as, "trad_as"],
+            [self.token_generator.create_token_without_jti, "jti"],
+            [self.token_generator.create_token_without_case_id, "case_id"],
+            [self.token_generator.create_token_without_trad_as, "trad_as"],
         ]:
             with self.subTest():
                 token = value[0]("test_numbers.json")
-                with app.app_context():
+                with self.test_app.app_context():
                     decrypted_token = decrypt_token(token)
                     self.assertNotIn(value[1], decrypted_token)
