983f43e6569402fcf740ee9644914decc2f120e6
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2024-01-31 12:59:41 +0000

    add application secrety key check and add type ingore at Url_safe_serializer

--- app/helpers/url_safe_serializer.py
+++ app/helpers/url_safe_serializer.py
@@ -6,7 +6,5 @@ from app.settings import EQ_SESSION_ID
 
 
 def url_safe_serializer() -> URLSafeSerializer:
-    secret_key = current_app.secret_key
-    if secret_key is None:
-        raise ValueError("Flask application secret key is not set.")
-    return URLSafeSerializer(secret_key, salt=cookie_session[EQ_SESSION_ID])
+    # Type Ignore: Changing secret key at this point would be too late.
+    return URLSafeSerializer(current_app.secret_key, salt=cookie_session[EQ_SESSION_ID]) # type: ignore
