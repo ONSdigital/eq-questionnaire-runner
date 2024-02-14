2d736bdc8271b06e6df11f5086538e613891ad8f
Liam Toozer <liam.toozer@gmail.com>
2024-02-14 15:05:31 +0000

    Address PR comments and small tidy up

--- scripts/generate_integration_test.py
+++ scripts/generate_integration_test.py
@@ -1,4 +1,5 @@
-from urllib.parse import parse_qs
+from typing import TextIO
+from urllib.parse import parse_qs, urlparse
 
 from playwright.sync_api import Playwright, Request, sync_playwright
 from structlog import get_logger
@@ -10,15 +11,16 @@ previous_request_method = ""
 survey_start = True
 schema_name = ""
 
-LAUNCHER_ROOT_URL = "http://localhost:8000/"
+LAUNCHER_ROOT_URL = "http://localhost:8000"
 RUNNER_ROOT_URL = "http://localhost:5000"
 
 
 # pylint: disable=global-variable-not-assigned
-def process_request(request):
+def process_request(request: Request) -> None:
     global schema_name
 
     with open(f"{schema_name}.py", "a", encoding="utf-8") as file:
+
         if request.method == "POST":
             process_post(request, file)
 
@@ -27,23 +29,18 @@ def process_request(request):
 
 
 # pylint: disable=global-statement
-def process_post(request, file):
+def process_post(request: Request, file: TextIO) -> None:
     global previous_request_method
-    global survey_start
-    global schema_name
 
     previous_request_method = "POST"
 
-    query_string = parse_qs(request.post_data, keep_blank_values=True)
-    del query_string["csrf_token"]
+    # Playwright Request.post_data comes formatted like a URL query string, so can be parsed
+    post_data = parse_qs(request.post_data)
+    del post_data["csrf_token"]
 
-    if not query_string.items():
-        file.write("\n        self.post()")
-        logger.info("self.post()")
-        return
+    items: dict[str, str | list[str]] = {}
+    for answer_id, answer_values in post_data.items():
 
-    items = {}
-    for answer_id, answer_values in query_string.items():
         if any(
             answer_values
         ):  # Only populate items if non-empty answers dict in request
@@ -52,30 +49,28 @@ def process_post(request, file):
             elif len(answer_values) == 1:
                 items[answer_id] = answer_values[0]  # A single item in list
 
-    if len(items) > 0:
+    if items:
         file.write(f"\n        self.post({items})")
         logger.info(f"self.post({items})")
-        return
-
-    if len(items) == 0:  # Empty post for no answers and non question pages
+    else:
+        # Empty post for no answers and non question pages
         file.write("\n        self.post()")
         logger.info("self.post()")
 
 
 # pylint: disable=global-statement
-def process_get(request, file):
+def process_get(request: Request, file: TextIO) -> None:
     global previous_request_method
     global survey_start
-    global schema_name
 
     if (  # Logic to filter out the session and root questionnaire GET requests at the start
         previous_request_method == "GET"
         and "session?token" not in request.url
-        and request.url != "http://localhost:5000/questionnaire/"
+        and request.url != f"{RUNNER_ROOT_URL}/questionnaire/"
     ):
-        path = request.url.lstrip(RUNNER_ROOT_URL)
-        file.write(f'\n        self.get("/{path}")')
-        logger.info(f'self.get("/{path}")')
+        path = urlparse(request.url).path
+        file.write(f'\n        self.get("{path}")')
+        logger.info(f'self.get("{path}")')
 
     if not survey_start:
         previous_request_method = request.method
@@ -83,38 +78,46 @@ def process_get(request, file):
     if (
         previous_request_method == ""
         and survey_start
-        and request.url == "http://localhost:5000/questionnaire/"
+        and request.url == f"{RUNNER_ROOT_URL}/questionnaire/"
     ):
         survey_start = False
 
 
-def request_handler(request: Request):
+def create_file_skeleton(request: Request):
     global schema_name
-    matching_url_found = RUNNER_ROOT_URL in request.url
 
-    if matching_url_found:
+    schema_name = parse_qs(request.url)["schema_name"][0]
+    with open(f"{schema_name}.py", "w", encoding="utf-8") as file:
+        file.write(
+            f'from tests.integration.integration_test_case import IntegrationTestCase\n\n\nclass {schema_name.title().replace("_", "")}'
+            f'(IntegrationTestCase):\n    def {schema_name}(self):\n        self.launchSurvey("{request.url.split("=")[-1]}")'
+        )
+        logger.info(f"self.launchSurvey('{schema_name}')")
+
+
+def request_handler(request: Request) -> None:
+    is_schema_launch_url = LAUNCHER_ROOT_URL in request.url and request.method == "GET"
+    is_runner_url = RUNNER_ROOT_URL in request.url
+
+    if is_schema_launch_url:
+        create_file_skeleton(request)
+    if is_runner_url:
         process_request(request)
-    elif request.url != LAUNCHER_ROOT_URL and LAUNCHER_ROOT_URL in request.url:
-        schema_name = request.url.split("=")[-1]
-        with open(f"{schema_name}.py", "w", encoding="utf-8") as file:
-            file.write(
-                f'from tests.integration.integration_test_case import IntegrationTestCase\n\n\nclass {schema_name.title().replace("_", "")}'
-                f'(IntegrationTestCase):\n    def {schema_name}(self):\n        self.launchSurvey("{request.url.split("=")[-1]}")'
-            )
-            logger.info(f'self.launchSurvey("{request.url.split("=")[-1]}")')
 
 
-def run(pl: Playwright):
-    chromium = pl.chromium
+def run(pw: Playwright):
+    chromium = pw.chromium
     browser = chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto("http://localhost:8000/")
+    page.goto(LAUNCHER_ROOT_URL)
 
     page.on("request", request_handler)
 
     input(
-        "Pause script. Start navigating through the browser for journey & press enter when finished to capture the output and add into a test\n"
+        ""
+        "Script is paused. Start navigating through the browser for the journey & press Enter when finished to capture the output and add into a test file\n"
+        ""
     )
     browser.close()
 
