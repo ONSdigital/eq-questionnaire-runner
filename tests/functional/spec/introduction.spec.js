22b734aa2c5b82cd6a53a61979c6541e1b26995b
petechd <petechadwick1944@gmail.com>
2024-03-13 16:09:58 +0000

    Add correct heading level on introduction questions

--- tests/functional/spec/introduction.spec.js
+++ tests/functional/spec/introduction.spec.js
@@ -6,7 +6,7 @@ describe("Introduction page", () => {
     await browser.openQuestionnaire(introductionSchema);
   });
 
-  it("Given I start a survey, When I view the introduction page, Then I should be able to see introduction information", async () => {
+  it("Given I start a survey, When I view the introduction page, Then I should be able to see introduction information displayed correctly", async () => {
     await browser.openQuestionnaire(introductionSchema);
     await expect(await $(IntroductionPage.useOfData()).getText()).toContain("How we use your data");
     await expect(await $(IntroductionPage.useOfInformation()).getText()).toContain(
@@ -17,6 +17,7 @@ describe("Introduction page", () => {
     await expect(await $(IntroductionPage.introDescription()).getText()).toBe(
       "To take part, all you need to do is check that you have the information you need to answer the survey questions.",
     );
+    await expect(await $(IntroductionPage.introQuestion()).getHTML()).toContain("h3");
   });
   it("Given I start a survey with introduction guidance set, When I view the introduction page, Then I should be able to see introduction guidance", async () => {
     await browser.openQuestionnaire(introductionSchema);
