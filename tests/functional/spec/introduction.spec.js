5cd999c26fa1423ffc9c8fc7ff37687384c4f861
petechd <petechadwick1944@gmail.com>
2024-03-19 09:29:16 +0000

    Rework functional test

--- tests/functional/spec/introduction.spec.js
+++ tests/functional/spec/introduction.spec.js
@@ -20,8 +20,9 @@ describe("Introduction page", () => {
   });
   it("Given I start a survey, When preview content is set on the introduction page, Then the content headings should be displayed at the correct level", async () => {
     await browser.openQuestionnaire(introductionSchema);
-    await expect(await $(IntroductionPage.previewQuestions()).isDisplayed()).toBe(true);
-    await expect(await $(IntroductionPage.introQuestion()).getHTML()).toContain("h3");
+    const introQuestionH3Selector = `${IntroductionPage.introQuestion()} h3`;
+    const h3Exists = await $(introQuestionH3Selector).isExisting();
+    await expect(h3Exists).toBe(true);
   });
   it("Given I start a survey with introduction guidance set, When I view the introduction page, Then I should be able to see introduction guidance", async () => {
     await browser.openQuestionnaire(introductionSchema);
