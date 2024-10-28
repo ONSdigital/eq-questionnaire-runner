99269ed9766957cfccf1c0acb8b3ff53dbb8601b
Rhys Berrow <rhys.berrow@ons.gov.uk>
2024-10-28 09:21:54 +0000

    Add wait

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -79,7 +79,7 @@ export const config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "debug",
+  logLevel: "error",
   outputDir: "tests/functional/logs/",
   //
   // Set specific log levels per logger
