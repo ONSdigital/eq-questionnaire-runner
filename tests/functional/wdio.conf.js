710ffb0b78c80227784b8d539d58129fade1d9fc
Rhys Berrow <rhys.berrow@ons.gov.uk>
2024-10-30 12:58:05 +0000

    All suites

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -115,7 +115,7 @@ export const config = {
   connectionRetryTimeout: 120000,
   //
   // Default request retries count
-  connectionRetryCount: 3,
+  connectionRetryCount: 5,
   //
   // Test runner services
   // Services take over a specific job you don't want to take care of. They enhance
