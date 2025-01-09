7e79b082de095b6ad5be0038a3c197c4915bfeb9
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-01-09 11:55:40 +0000

    fix formatting in settings

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ def get_env_or_fail(key):
 
     return value
 
+
 DATASTORE_USE_GRPC = parse_mode(os.getenv("DATASTORE_USE_GRPC", "True"))
 CDN_URL = os.getenv("CDN_URL", "https://cdn.eq.gcp.onsdigital.uk")
 CDN_ASSETS_PATH = os.getenv("CDN_ASSETS_PATH", "/design-system")
