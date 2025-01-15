a725c64e795387c6f29c6d75366da1335fbc141d
petechd <petechadwick1944@gmail.com>
2025-01-15 23:53:27 +0000

    Add retries and linting

--- scripts/validate_test_schemas.py
+++ scripts/validate_test_schemas.py
@@ -1,13 +1,15 @@
 import json
-import subprocess
 import os
 import re
+import subprocess
 import sys
+import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 error = False
 passed = 0
 failed = 0
+
 
 def validate_schema(schema_path):
     try:
@@ -16,7 +18,7 @@ def validate_schema(schema_path):
              'http://localhost:5001/validate'],
             capture_output=True,
             text=True,
-            check=True
+            check=True,
         )
         return schema_path, result.stdout
     except subprocess.CalledProcessError as e:
@@ -25,23 +27,55 @@ def validate_schema(schema_path):
 
 
 def main():
-    file_path = './schemas/test/en'
-    schemas = [os.path.join(file_path, f) for f in os.listdir(file_path) if f.endswith('.json')]
+    checks = 4
+
+    while checks > 0:
+        response = subprocess.run(
+            ['curl', '-so', '/dev/null', '-w', '%{http_code}', 'http://localhost:5002/status'],
+            capture_output=True,
+            text=True
+        ).stdout.strip()
+
+        if response != "200":
+            print(f"\033[31m---Error: Schema Validator Not Reachable---\033[0m")
+            print(f"\033[31mHTTP Status: {response}\033[0m")
+            if checks != 1:
+                print(f"Retrying...\n")
+                time.sleep(5)
+            else:
+                print(f"Exiting...\n")
+                sys.exit(1)
+            checks -= 1
+        else:
+            checks = 0
+
+    if len(sys.argv) == 1 or sys.argv[1] == "--local":
+        file_path = "./schemas/test/en"
+    else:
+        file_path = sys.argv[1]
+
+    print(f"--- Testing Schemas in {file_path} ---")
+
+    schemas = [
+        os.path.join(file_path, f) for f in os.listdir(file_path) if f.endswith(".json")
+    ]
 
     with ThreadPoolExecutor(max_workers=20) as executor:
-        future_to_schema = {executor.submit(validate_schema, schema): schema for schema in schemas}
+        future_to_schema = {
+            executor.submit(validate_schema, schema): schema for schema in schemas
+        }
         for future in as_completed(future_to_schema):
             schema = future_to_schema[future]
             try:
                 schema_path, result = future.result()
                 # Extract HTTP body
-                http_body = re.sub(r'HTTPSTATUS:.*', '', result)
+                http_body = re.sub(r"HTTPSTATUS:.*", "", result)
 
                 # Convert HTTP body to JSON
                 http_body_json = json.loads(http_body)
 
                 # Extract HTTP status code
-                result_response = re.search(r'HTTPSTATUS:(\d+)', result)[1]
+                result_response = re.search(r"HTTPSTATUS:(\d+)", result)[1]
 
                 if result_response == "200" and http_body_json == {}:
                     print(f"\033[32m{schema_path}: PASSED\033[0m")
@@ -51,9 +85,8 @@ def main():
                     print(f"\033[31m{schema_path}: FAILED\033[0m")
                     print(f"\033[31mHTTP Status @ /validate: {result_response}\033[0m")
                     print(f"\033[31mHTTP Status: {http_body_json}\033[0m")
-                    global error
+                    global error, failed
                     error = True
-                    global failed
                     failed += 1
             except Exception as e:
                 print(f"\033[31mError processing {schema}: {e}\033[0m")
