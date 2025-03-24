2298aa8c193518174c0496a7e558ff8c0ba2d781
VirajP1002 <Viraj.Patel@ons.gov.uk>
2025-03-24 11:58:28 +0000

    Upgrade WTForms to version 3.2.1 and update lock

--- tests/app/conftest.py
+++ tests/app/conftest.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta, timezone
 from http.client import HTTPMessage
 
 import fakeredis
-import google.pubsub_v1.services.publisher.client
 import pytest
 from mock import MagicMock
 from mock.mock import Mock
