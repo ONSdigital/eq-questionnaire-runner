3b58c0f125a1dd49eb153c354cf13fd59da12e1b
petechd <petechadwick1944@gmail.com>
2025-07-15 12:56:59 +0100

    Seperate timeout modal

--- tests/functional/wdio.conf.cjs
+++ tests/functional/wdio.conf.cjs
@@ -18,8 +18,9 @@ exports.config = {
   //
   specs: ["./spec/**/*.js"],
   suites: {
-    components: ["./spec/timeout/timeout_modal_extended_new_window/*.js", "./spec/timeout/timeout_modal_expired/*.js", "./spec/timeout/timeout_modal_extended/*.js", "./spec/components/**/*.js", "./spec/*.spec.js", "./spec/summaries/**/*.js"],
-    features: ["./spec/features/**/*.js", "./spec/list_collector/**/*.js", "./spec/hub_and_spoke/**/*.js", "./spec/supplementary_data/**/*.js", "./spec/journeys/**/*.js"],
+    components: ["./spec/components/**/*.js", "./spec/*.spec.js", "./spec/summaries/**/*.js", "./spec/journeys/**/*.js"],
+    timeouts_modal: ["./spec/timeout/timeout_modal_extended_new_window/*.js", "./spec/timeout/timeout_modal_expired/*.js", "./spec/timeout/timeout_modal_extended/*.js"],
+    features: ["./spec/features/**/*.js", "./spec/list_collector/**/*.js", "./spec/hub_and_spoke/**/*.js", "./spec/supplementary_data/**/*.js"],
   },
   // Patterns to exclude.
   exclude: [
