ca7e11020df8d93c77e008fe52cc5e43a7c4da96
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-11-05 13:37:29 +0000

    add a wait function before checking for elements

--- tests/functional/spec/supplementary_data/supplementary_data_calculated_summary_and_repeating_block.spec.js
+++ tests/functional/spec/supplementary_data/supplementary_data_calculated_summary_and_repeating_block.spec.js
@@ -45,6 +45,7 @@ describe("Using supplementary data", () => {
   });
 
   it("Given I have repeating blocks with supplementary data, When I start the first repeating block, Then I see the supplementary data for the first list item", async () => {
+    await waitForQuestionnaireToLoad();
     await expect(await $("body").getHTML({ prettify: false })).toContain("<h2>Include</h2>");
     await expect(await $("body").getHTML({ prettify: false })).toContain("<li>for children's playgrounds</li>");
     await expect(await $("body").getHTML({ prettify: false })).toContain("<li>swimming pools and paddling pools</li>");
