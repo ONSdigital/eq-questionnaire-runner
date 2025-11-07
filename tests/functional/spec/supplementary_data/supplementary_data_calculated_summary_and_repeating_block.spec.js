f9d61c7adb2db95fa924ef80ec37a171e0f7fb6d
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-11-07 09:06:15 +0000

    add wait for page to loads

--- tests/functional/spec/supplementary_data/supplementary_data_calculated_summary_and_repeating_block.spec.js
+++ tests/functional/spec/supplementary_data/supplementary_data_calculated_summary_and_repeating_block.spec.js
@@ -30,6 +30,7 @@ describe("Using supplementary data", () => {
   });
   it("Given I have some repeating blocks with supplementary data, When I begin the section, Then I see the supplementary names rendered correctly", async () => {
     await click(HubPage.submit());
+    await waitForPageToLoad();
     await $(ListCollectorProductsPage.listLabel(1)).waitForExist({ timeout: 15000 });
     await $(ListCollectorProductsPage.listLabel(2)).waitForExist({ timeout: 15000 });
     await expect(await $(ListCollectorProductsPage.listLabel(1)).getText()).toBe("Articles and equipment for sports or outdoor games");
