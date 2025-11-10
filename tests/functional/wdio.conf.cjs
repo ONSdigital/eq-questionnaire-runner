e838ae330e35ca9578d797382759453ec9ddb829
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-11-10 10:13:49 +0000

    update debug log level

--- tests/functional/wdio.conf.cjs
+++ tests/functional/wdio.conf.cjs
@@ -76,8 +76,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "debug",
-  // outputDir: "tests/functional/logs/",
+  logLevel: 'debug',
   //
   // Set specific log levels per logger
   // loggers:
