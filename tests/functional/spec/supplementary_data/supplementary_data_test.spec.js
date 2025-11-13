e47218e12894c0aecedc8f596b57e511b2b3f618
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2025-11-13 10:46:06 +0000

    update tests for more specifc use

--- tests/functional/spec/supplementary_data/supplementary_data_test.spec.js
+++ tests/functional/spec/supplementary_data/supplementary_data_test.spec.js
@@ -1,6 +1,10 @@
 import { expect } from "@wdio/globals";
 import { getRandomString } from "../../jwt_helper";
-import { waitForPageToLoad } from "../../helpers";
+import { clickSyncMode, click, waitForPageToLoad } from "../../helpers";
+import LoadedSuccessfullyBlockPage from "../../generated_pages/hub_section_required_with_repeat_supplementary/loaded-successfully-block.page";
+import IntroductionBlockPage from "../../generated_pages/hub_section_required_with_repeat_supplementary/introduction-block.page";
+import HubPage from "../../base_pages/hub.page";
+import ListCollectorProductsPage from "../../generated_pages/supplementary_data_repeating_block_and_calculated_summary/list-collector-products.page.js";
 
 const DATASETS = ["203b2f9d-c500-8175-98db-86ffcfdccfa3", "3bb41d29-4daa-9520-82f0-cae365f390c6"];
 
@@ -10,19 +14,40 @@ const SCHEMAS = [
   "test_hub_section_required_with_repeat_supplementary.json",
   "test_supplementary_data.json",
 ];
+const EXPECTED_SURNAMES_BY_DATASET = {
+  "203b2f9d-c500-8175-98db-86ffcfdccfa3": "Potter, Kent",
+  "3bb41d29-4daa-9520-82f0-cae365f390c6": "Potter, Wayne",
+};
 
-async function assertIntroAndCalcSummaryInterstitial() {
+async function assertIntroAndCalcSummaryInterstitial(_schema, sdsDatasetId) {
   await waitForPageToLoad(15000);
+
   const bodyText = await $("body").getText();
   await expect(bodyText).toContain("You have successfully loaded Supplementary data");
-  await expect(await $$('[data-qa="btn-submit"]').length).toBeGreaterThan(0);
-  await expect(await $("#main-content #guidance-1").getText()).toContain("The surnames of the employees are: Potter, Kent.")
+  await expect((await $$('[data-qa="btn-submit"]')).length).toBeGreaterThan(0);
+
+  const expectedSurnames = EXPECTED_SURNAMES_BY_DATASET[sdsDatasetId];
+  const guidance = await $("#main-content #guidance-1").getText();
+  await expect(guidance).toContain(`The surnames of the employees are: ${expectedSurnames}.`);
+
+  const items = await $$("#main-content li");
+  await expect(await items[0].getText()).toBe("Articles and equipment for sports or outdoor games");
+  await expect(await items[1].getText()).toBe("Kitchen Equipment");
+}
+
+async function assertHubAndSpoke() {
+  await waitForPageToLoad(15000);
+  clickSyncMode(LoadedSuccessfullyBlockPage.submit());
+  clickSyncMode(IntroductionBlockPage.submit());
 }
 
 async function assertRepeatingBlockHubLanding() {
   await waitForPageToLoad(15000);
   const bodyText = await $("body").getText();
   await expect(bodyText).toContain("Product details");
+  await click(HubPage.submit());
+  await expect(await $(ListCollectorProductsPage.listLabel(1)).getText()).toBe("Articles and equipment for sports or outdoor games");
+  await expect(await $(ListCollectorProductsPage.listLabel(2)).getText()).toBe("Kitchen Equipment");
 }
 
 async function assertGenericTestSupplementaryDataTitle() {
@@ -31,18 +56,17 @@ async function assertGenericTestSupplementaryDataTitle() {
   await expect(bodyText).toContain("Test Supplementary Data");
 }
 
-function getValidatorForSchema(schema) {
+function getValidatorForSchema(schema, sdsDatasetId) {
   switch (schema) {
     case "test_supplementary_data_with_introduction_and_calculated_summary.json":
       return assertIntroAndCalcSummaryInterstitial;
 
     case "test_hub_section_required_with_repeat_supplementary.json":
-      return assertIntroAndCalcSummaryInterstitial;
+      return assertHubAndSpoke;
 
     case "test_supplementary_data_repeating_block_and_calculated_summary.json":
       return assertRepeatingBlockHubLanding;
 
-    // If you later add a specific check for test_supplementary_data.json, swap it here
     case "test_supplementary_data.json":
       return assertGenericTestSupplementaryDataTitle;
 
@@ -63,7 +87,7 @@ describe("Each schema validates its expected first page with each SDS dataset", 
         });
 
         const validate = getValidatorForSchema(schema);
-        await validate();
+        await validate(schema, sdsDatasetId);
       });
     }
   }
