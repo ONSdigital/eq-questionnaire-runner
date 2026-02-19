de438bcf937018a94d05bce49df66b12a4b8f86c
Yuyutsu Rai <Yuyutsu.Rai@ons.gov.uk>
2026-02-19 15:04:15 +0000

    change click behavior to instant

--- tests/functional/helpers.js
+++ tests/functional/helpers.js
@@ -45,7 +45,7 @@ export const click = async (selector) => {
   // but clicks down on the very top of the button which moves down and just below the mouse. When the mouse click is released
   // it's no longer over the button and the click silently fails. This means that when the test comes to do assertions on the following page
   // they fail, as we never navigated to that page.
-  await $(selector).scrollIntoView({ block: "center", inline: "center", behavior: "smooth" });
+  await $(selector).scrollIntoView({ block: "center", inline: "center", behavior: "instant" });
   await $(selector).click();
 
   // Allow time in case the click loads a new page.
