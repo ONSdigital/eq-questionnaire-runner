e27b566c357be76402d893eb53e202cd985ba316
petechd <piotr.chudziak@ons.gov.uk>
2026-03-24 11:23:35 +0000

    Change global timeout

--- tests/functional/wdio.conf.cjs
+++ tests/functional/wdio.conf.cjs
@@ -104,7 +104,7 @@ exports.config = {
   baseUrl: process.env.EQ_FUNCTIONAL_TEST_ENV || "http://localhost:5000",
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 10000,
+  waitforTimeout: 100000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
