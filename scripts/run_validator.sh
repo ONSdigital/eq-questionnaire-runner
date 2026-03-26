50a6b8185062402157974f8c2b89969cfa822ce2
petechd <piotr.chudziak@ons.gov.uk>
2026-03-26 08:57:06 +0000

    Revert validator version change

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=348-merge
+tag=latest
 TAG=${tag} docker compose -f docker-compose-schema-validator.yml pull
 TAG=${tag} docker compose -f docker-compose-schema-validator.yml up -d
