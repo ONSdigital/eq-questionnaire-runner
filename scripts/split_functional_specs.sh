3fc33fe6aaf90c5499b2780bf0bd4ce020aa205a
petechd <piotr.chudziak@ons.gov.uk>
2026-04-13 15:30:34 +0100

    Split specs to different shards

---
+++ scripts/split_functional_specs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Usage: ./scripts/split_functional_specs.sh <total_shards> <shard_number>
+set -e
+
+TOTAL_SHARDS=$1
+SHARD_NUMBER=$2
+
+SPECS=($(ls tests/functional/spec/**/*.spec.js | sort))
+NUM_SPECS=${#SPECS[@]}
+
+for i in "${!SPECS[@]}"; do
+  if (( i % TOTAL_SHARDS + 1 == SHARD_NUMBER )); then
+    echo "${SPECS[$i]}"
+  fi
+done
