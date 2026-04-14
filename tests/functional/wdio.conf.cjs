58cba555b68a14cd46d048558c77842987b13713
petechd <piotr.chudziak@ons.gov.uk>
2026-04-14 08:47:26 +0100

    Add specs back into conf file

--- tests/functional/wdio.conf.cjs
+++ tests/functional/wdio.conf.cjs
@@ -16,7 +16,7 @@ exports.config = {
   // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
-  specs: ["./spec/features/confirmation_question.spec.js", "./spec/features/units.spec.js", "./spec/list_collector/list_collector_content.spec.js", "./spec/list_collector/list_collector_section_summary.spec.js", "./spec/list_collector/relationships.spec.js", "./spec/supplementary_data/supplementary_data_with_list_collector.spec.js"],
+  specs: process.env.SPECS || ["./spec/**/*.js"],
   suites: {
     components: ["./spec/components/**/*.js", "./spec/*.spec.js", "./spec/summaries/**/*.js"],
     timeout_modal: ["./spec/timeout/timeout_modal_extended_new_window/*.js", "./spec/timeout/timeout_modal_expired/*.js", "./spec/timeout/timeout_modal_extended/*.js"],
