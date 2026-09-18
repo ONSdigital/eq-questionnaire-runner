3fea9cac15f109b0fc8cc54a8abcc311f01c9eaa
Piotr Chudziak <piotr.chudziak@ons.gov.uk>
2026-09-18 09:42:42 +0100

    Revert summary item change

--- tests/functional/base_pages/calculated-summary.page.ts
+++ tests/functional/base_pages/calculated-summary.page.ts
@@ -14,4 +14,7 @@ export default class CalculatedSummaryBasePage extends BasePage {
     return this.locator('[data-qa=calculated-summary-answer]')
   }
 
+  summaryItems (): Locator {
+    return this.locator('dl.ons-summary__items')
+  }
 }
